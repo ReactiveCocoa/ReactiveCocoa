8f5f727dcbf87c5aa769992b47fad9ac68efe296
joshaber <joshaber@gmail.com>
2012-03-02 17:01:11 -0500

    wall werror guys

--- ReactiveCocoa/RACAppDelegate.m
+++ ReactiveCocoa/RACAppDelegate.m
@@ -15,7 +15,7 @@
 @interface RACAppDelegate ()
 @property (nonatomic, strong) RACObservableValue *textField1Value;
 @property (nonatomic, strong) RACObservableValue *isMagicValue;
-@property (nonatomic, strong) RACObservableValue *doMatchObservable;
+@property (nonatomic, strong) RACObservableValue *dontMatchObservable;
 @property (nonatomic, strong) RACObservableValue *textField2Value;
 @end
 
@@ -29,18 +29,18 @@
 	self.textField1Value = [RACObservableValue valueWithValue:@""];
 	self.textField2Value = [RACObservableValue valueWithValue:@""];
 	self.isMagicValue = [RACObservableValue valueWithValue:[NSNumber numberWithBool:NO]];
-	self.doMatchObservable = [RACObservableValue valueWithValue:[NSNumber numberWithBool:YES]];
+	self.dontMatchObservable = [RACObservableValue valueWithValue:[NSNumber numberWithBool:YES]];
 	
 	// UI elements should *always* be backed by the model.
 	[self.doMagicButton bind:NSEnabledBinding toObject:self withKeyPath:RACKVO(self.isMagicValue.value)];
 	[self.textField1 bind:NSValueBinding toObject:self withKeyPath:RACKVO(self.textField1Value.value)];
-	[self.matchesLabel bind:NSHiddenBinding toObject:self withKeyPath:RACKVO(self.doMatchObservable.value)];
+	[self.matchesLabel bind:NSHiddenBinding toObject:self withKeyPath:RACKVO(self.dontMatchObservable.value)];
 	[self.textField2 bind:NSValueBinding toObject:self withKeyPath:RACKVO(self.textField2Value.value)];
 	
 	[[[RACObservableSequence 
 	   whenAny:self.textField1Value, self.textField2Value, nil] 
-	  select:^(id value) { return [NSNumber numberWithBool:![self.textField1Value.value isEqualToString:self.textField2Value.value]]; }] 
-	 toProperty:self.doMatchObservable];
+	  select:^(id value) { return [NSNumber numberWithBool:[self.textField1Value.value isEqualToString:self.textField2Value.value]]; }] 
+	 toProperty:self.dontMatchObservable];
 	
 	[[self.textField1Value 
 	  select:^(id x) { return [NSNumber numberWithBool:[x hasPrefix:@"magic"]]; }] 
@@ -63,7 +63,7 @@
 @synthesize textField1Value;
 @synthesize textField2;
 @synthesize matchesLabel;
-@synthesize doMatchObservable;
+@synthesize dontMatchObservable;
 @synthesize textField2Value;
 	 
 @end

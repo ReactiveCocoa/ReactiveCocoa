6fa36fe93a5f4a683b6273bc82fb639c79d4b1ce
joshaber <joshaber@gmail.com>
2012-03-04 22:54:34 -0500

    work on RACAsyncCommand

---
+++ ReactiveCocoa/RACAsyncCommand.h
@@ -0,0 +1,14 @@
+//
+//  RACAsyncCommand.h
+//  ReactiveCocoa
+//
+//  Created by Josh Abernathy on 3/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RACCommand.h"
+
+
+@interface RACAsyncCommand : RACCommand
+
+@end
---
+++ ReactiveCocoa/RACAsyncCommand.m
@@ -0,0 +1,14 @@
+//
+//  RACAsyncCommand.m
+//  ReactiveCocoa
+//
+//  Created by Josh Abernathy on 3/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RACAsyncCommand.h"
+
+
+@implementation RACAsyncCommand
+
+@end

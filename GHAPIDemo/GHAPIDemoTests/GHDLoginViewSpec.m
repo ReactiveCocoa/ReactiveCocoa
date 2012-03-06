66396589d21b6fe25223e22d023ca9a03ac6af51
joshaber <joshaber@gmail.com>
2012-03-06 10:43:16 -0500

    changed which herders are exposed

---
+++ GHAPIDemo/GHAPIDemoTests/GHDLoginViewSpec.m
@@ -0,0 +1,13 @@
+//
+//  GHDLoginViewSpec.m
+//  GHAPIDemo
+//
+//  Created by Josh Abernathy on 3/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GHDLoginViewSpec.h"
+
+@implementation GHDLoginViewSpec
+
+@end

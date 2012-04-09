7d5f3cad877e7dfd54e87fec41645359d15f79ac
joshaber <joshaber@gmail.com>
2012-04-08 22:26:50 -0400

    a better -whenAny:

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACOperations.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACOperations.m
@@ -12,6 +12,7 @@
 #import "RACSubscribable.h"
 #import "RACSubscribable+Operations.h"
 #import "EXTNil.h"
+#import "RACSubscriber.h"
 
 
 @implementation NSObject (RACOperations)
@@ -20,20 +21,33 @@
 	NSParameterAssert(keyPaths != nil);
 	NSParameterAssert(reduceBlock != NULL);
 	
-	NSMutableArray *subscribables = [NSMutableArray arrayWithCapacity:keyPaths.count];
-	for(NSString *keyPath in keyPaths) {
-		[subscribables addObject:[[self RACSubscribableForKeyPath:keyPath onObject:self] distinctUntilChanged]];
-	}
-	
 	__block __unsafe_unretained id weakSelf = self;
-	return [[RACSubscribable merge:subscribables] select:^(id x) {
+	return [RACSubscribable createSubscribable:^RACDisposable *(id<RACSubscriber> observer) {
 		NSObject *strongSelf = weakSelf;
-		NSMutableArray *values = [NSMutableArray arrayWithCapacity:keyPaths.count];
+		
+		NSArray * (^currentValues)(void) = ^{
+			NSMutableArray *values = [NSMutableArray arrayWithCapacity:keyPaths.count];
+			for(NSString *keyPath in keyPaths) {
+				[values addObject:[strongSelf valueForKeyPath:keyPath] ? : [EXTNil null]];
+			}
+			
+			return values;
+		};
+		
+		NSMutableArray *subscribables = [NSMutableArray arrayWithCapacity:keyPaths.count];
 		for(NSString *keyPath in keyPaths) {
-			[values addObject:[strongSelf valueForKeyPath:keyPath] ? : [EXTNil null]];
+			[subscribables addObject:[self RACSubscribableForKeyPath:keyPath onObject:self]];
 		}
 		
-		return reduceBlock(values);
+		[observer sendNext:reduceBlock(currentValues())];
+		
+		return [[RACSubscribable merge:subscribables] subscribeNext:^(id x) {
+			[observer sendNext:reduceBlock(currentValues())];
+		} error:^(NSError *error) {
+			[observer sendError:error];
+		} completed:^{
+			[observer sendCompleted];
+		}];
 	}];
 }
 

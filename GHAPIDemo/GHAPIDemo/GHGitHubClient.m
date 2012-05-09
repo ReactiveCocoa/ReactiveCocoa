74f50d9182f3cc9c3011db9cd007e4399eea6ebe
joshaber <joshaber@gmail.com>
2012-05-09 11:03:11 -0400

    multicast to an async subject

--- GHAPIDemo/GHAPIDemo/GHGitHubClient.m
+++ GHAPIDemo/GHAPIDemo/GHGitHubClient.m
@@ -91,7 +91,7 @@
 
 - (RACSubscribable *)enqueueRequestWithMethod:(NSString *)method path:(NSString *)path parameters:(NSDictionary *)parameters {
 	return [[[RACSubscribable defer:^{
-		RACAsyncSubject *subject = [RACAsyncSubject subject];
+		RACSubject *subject = [RACSubject subject];
 		NSURLRequest *request = [self requestWithMethod:method path:path parameters:parameters];
 		AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
 			[subject sendNext:responseObject];
@@ -103,7 +103,7 @@
 		[self enqueueHTTPRequestOperation:operation];
 		
 		return subject;
-	}] publish] autoconnect];
+	}] multicast:[RACAsyncSubject subject]] autoconnect];
 }
 
 - (void)setUser:(GHGitHubUser *)u {

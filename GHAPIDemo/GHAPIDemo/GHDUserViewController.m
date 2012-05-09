d90c1a98df377388ea2a1403209c316e1d3edf3d
joshaber <joshaber@gmail.com>
2012-05-09 10:48:54 -0400

    RAC-ier

--- GHAPIDemo/GHAPIDemo/GHDUserViewController.m
+++ GHAPIDemo/GHAPIDemo/GHDUserViewController.m
@@ -17,6 +17,7 @@
 @property (nonatomic, strong) GHGitHubUser *userAccount;
 @property (nonatomic, strong) NSImage *avatar;
 @property (nonatomic, assign) BOOL loading;
+@property (nonatomic, strong) GHGitHubClient *client;
 @end
 
 
@@ -44,55 +45,56 @@
 @dynamic view;
 @synthesize loading;
 @synthesize avatar;
+@synthesize client;
 
 - (id)initWithUserAccount:(GHGitHubUser *)user {
+	NSParameterAssert(user != nil);
+	
 	self = [super initWithNibName:nil bundle:nil];
 	if(self == nil) return nil;
-		
-	RACSubscribable *userAccountIsntNil = [RACAbleSelf(self.userAccount) where:^BOOL(id x) {
-		return x != nil;
-	}];
 	
-	[[[userAccountIsntNil 
-		injectObjectWeakly:self]
-		select:^id(id x) {
-			return [NSNumber numberWithBool:YES];
-		}]
-		toProperty:RAC_KEYPATH_SELF(self.loading) onObject:self];
+	self.userAccount = user;
+	self.client = [GHGitHubClient clientForUser:self.userAccount];
+	self.loading = YES;
 	
 	__block __unsafe_unretained id weakSelf = self;
-	// We're using -selectMany: to chain userAccountIsntNil so that we fetch
-	// the new user account's info whenever it changes.
-	[[[userAccountIsntNil 
-		selectMany:^(GHGitHubUser *x) {
-			return [[GHGitHubClient clientForUser:x] fetchUserInfo];
-		}]
-		injectObjectWeakly:self]
-		subscribeNext:^(RACTuple *t) {
-			GHDUserViewController *self = t.last;
-			[self.userAccount setValuesForKeysWithDictionary:t.first];
-			self.loading = NO;
-		}
-		error:^(NSError *error) {
-			NSLog(@"error: %@", error);
+	// We're using -merge: so that -fetchUser, -fetchRepos, and -fetchOrgs are 
+	// all executed independently. We're then told when they've all completed.
+	// -finally: lets us share logic regardless of whether we get an error or 
+	// complete successfully.
+	[[[RACSubscribable 
+		merge:[NSArray arrayWithObjects:[self fetchUser], [self fetchRepos], [self fetchOrgs], nil]] 
+		finally:^{
 			GHDUserViewController *strongSelf = weakSelf;
 			strongSelf.loading = NO;
+		}]
+		subscribeNext:^(id x) {
+			// nothing
+		} error:^(NSError *error) {
+			NSLog(@"error: %@", error);
+		} completed:^{
+			NSLog(@"done");
 		}];
 	
-	// Note that we're using -deliverOn: to load the image in a background 
-	// queue and then finish with another -deliverOn: so that subscribers get 
-	// the result on the main queue.
+	// We're using -deliverOn: to load the image in a background queue and then 
+	// finish with another -deliverOn: so that subscribers get the result on the 
+	// main queue.
 	RACSubscribable *loadedAvatar = [[[[[RACAbleSelf(self.userAccount.avatarURL) 
 		where:^BOOL(id x) {
 			return x != nil;
 		}] 
 		deliverOn:[RACScheduler sharedOperationQueueScheduler]] 
-		select:^(id x) {
-			return [[NSImage alloc] initWithContentsOfURL:x];
+		injectObjectWeakly:self]
+		selectMany:^(RACTuple *t) {
+			GHDUserViewController *self = t.last;
+			// We retry the image request 1 time if an error occurs. We're also 
+			// using -catchTo: to send the default user image if an error occurs 
+			// and the retry fails.
+			return [[[self 
+						loadImageAtURL:t.first] 
+						retry:1] 
+						catchTo:[RACSubscribable return:[NSImage imageNamed:NSImageNameUser]]];
 		}] 
-		where:^BOOL(id x) {
-			return x != nil;
-		}]
 		deliverOn:[RACScheduler mainQueueScheduler]];
 	
 	// -merge: takes the latest value from the subscribables. In this case, 
@@ -103,9 +105,66 @@
 		merge:[NSArray arrayWithObjects:[RACSubscribable return:[NSImage imageNamed:NSImageNameUser]], loadedAvatar, nil]] 
 		toProperty:RAC_KEYPATH_SELF(self.avatar) onObject:self];
 	
-	self.userAccount = user;
-	
 	return self;
+}
+
+- (RACSubscribable *)fetchUser {	
+	return [[[self.client 
+				fetchUserInfo] 
+				injectObjectWeakly:self]
+				select:^(RACTuple *t) {
+					GHDUserViewController *self = t.last;
+					[self.userAccount setValuesForKeysWithDictionary:t.first];
+					return [RACUnit defaultUnit];
+				}];
+}
+
+- (RACSubscribable *)fetchRepos {	
+	return [[[self.client 
+				fetchUserRepos] 
+				injectObjectWeakly:self]
+				select:^(RACTuple *t) {
+					NSLog(@"repos: %@", t.first);
+					return [RACUnit defaultUnit];
+				}];
+}
+
+- (RACSubscribable *)fetchOrgs {	
+	return [[[self.client 
+				fetchUserRepos] 
+				injectObjectWeakly:self]
+				select:^(RACTuple *t) {
+					NSLog(@"orgs: %@", t.first);
+					return [RACUnit defaultUnit];
+				}];
+}
+
+- (RACSubscribable *)loadImageAtURL:(NSURL *)URL {
+	// This -defer, -publish, -autoconnect dance might seem a little odd, so 
+	// let's talk through it.
+	//
+	// We're using -defer because -startWithScheduler:block: returns us a hot 
+	// subscribable but we really want a cold one. Why do we want a cold one? 
+	// It lets us defer the actual work of loading the image until someone 
+	// actually cares enough about it to subscribe. But even more than that, 
+	// cold subscribables let us use operations like -retry: or -repeat:.
+	//
+	// But the downside to cold subscribables is that subsequent subscribers
+	// will cause the subscribable to fire again, which we don't really want.
+	// So we use -publish to share the subscriptions to the underlying subscribable.
+	// -autoconnect means the connectable subscribable from -publish will connect
+	// automatically when it receives its first subscriber.
+	return [[[RACSubscribable defer:^{
+		return [RACSubscribable startWithScheduler:[RACScheduler immediateScheduler] block:^id(BOOL *success, NSError **error) {
+			NSImage *image = [[NSImage alloc] initWithContentsOfURL:URL];
+			if(image == nil) {
+				*success = NO;
+				return nil;
+			}
+			
+			return image;
+		}];
+	}] publish] autoconnect];
 }
 
 @end

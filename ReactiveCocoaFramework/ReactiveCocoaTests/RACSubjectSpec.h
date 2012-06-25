441b6ddfe16aceaba071f0a21b45f9f2d64bcb12
joshaber <joshaber@gmail.com>
2012-06-24 20:57:09 -0400

    project organization

---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSubjectSpec.h
@@ -0,0 +1,13 @@
+//
+//  RACSubjectSpec.h
+//  ReactiveCocoa
+//
+//  Created by Josh Abernathy on 6/24/12.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RACSubjectSpec : NSObject
+
+@end
---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSubjectSpec.m
@@ -0,0 +1,13 @@
+//
+//  RACSubjectSpec.m
+//  ReactiveCocoa
+//
+//  Created by Josh Abernathy on 6/24/12.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "RACSubjectSpec.h"
+
+@implementation RACSubjectSpec
+
+@end

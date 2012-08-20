9388a7c744e00b7f351d560a2f816cd4cdb50e2f
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-08-20 14:37:41 -0700

    Exposed a bit of an interface to RACBacktrace
    This module won't be visible to client code, but maybe it could be useful elsewhere within RAC.

--- ReactiveCocoaFramework/ReactiveCocoa/RACBacktrace.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACBacktrace.h
@@ -8,6 +8,23 @@
 
 // Preserves backtraces across asynchronous calls.
 @interface RACBacktrace : NSObject
+
+// The backtrace from any previous thread.
 @property (nonatomic, strong, readonly) RACBacktrace *previousThreadBacktrace;
+
+// The call stack of this backtrace's thread.
 @property (nonatomic, copy, readonly) NSArray *callStackSymbols;
+
+// Captures the current thread's backtrace, appending it to any backtrace from
+// a previous thread.
++ (instancetype)captureBacktrace;
+
+// Same as +captureBacktrace, but omits the specified number of frames at the
+// top of the stack (in addition to this method itself).
++ (instancetype)captureBacktraceIgnoringFrames:(NSUInteger)ignoreCount;
+
+// Prints the backtrace of the current thread, appended to that of any previous
+// threads.
++ (void)printBacktrace;
+
 @end
--- ReactiveCocoaFramework/ReactiveCocoa/RACBacktrace.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACBacktrace.m
@@ -11,25 +11,8 @@
 #import "RACBacktrace.h"
 
 @interface RACBacktrace ()
-
-// The backtrace from any previous thread.
 @property (nonatomic, strong, readwrite) RACBacktrace *previousThreadBacktrace;
-
-// The call stack of this backtrace's thread.
 @property (nonatomic, copy, readwrite) NSArray *callStackSymbols;
-
-// Captures the current thread's backtrace, appending it to any backtrace from
-// a previous thread.
-+ (instancetype)captureBacktrace;
-
-// Same as +captureBacktrace, but omits the specified number of frames at the
-// top of the stack (in addition to this method itself).
-+ (instancetype)captureBacktraceIgnoringFrames:(NSUInteger)ignoreCount;
-
-// Prints the backtrace of the current thread, appended to that of any previous
-// threads.
-+ (void)printBacktrace;
-
 @end
 
 @interface RACDispatchInfo : NSObject

82cae0a7de17fa74752560fc48626fde8761d614
joshaber <joshaber@gmail.com>
2012-09-18 23:02:03 -0400

    moved from NSFileHandle => NSFileManager, added tests

--- RACExtensions/NSFileHandle+RACSupport.h
+++ RACExtensions/NSFileHandle+RACSupport.h
@@ -9,37 +9,10 @@
 #import <Foundation/Foundation.h>
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
-extern NSString * const RACNSFileHandleErrorDomain;
-
-// The file handle does not have a valid file descriptor and so it cannot be
-// watched for events.
-extern const NSInteger RACNSFileHandleErrorInvalidFileDescriptor;
-
-// An error occurred while trying to create an event source with the receiver's
-// file descriptor.
-extern const NSInteger RACNSFileHandleErrorCouldNotCreateEventSource;
-
 @interface NSFileHandle (RACSupport)
 
 // Read any available data in the background and send it. Completes when data
 // length is <= 0.
 - (RACSubscribable *)rac_readInBackground;
-
-// Creates a new file handle which opens the URL for event notification only.
-// This is meant to be used in combination with `-rac_watchForEventsWithQueue:`.
-+ (instancetype)rac_fileHandleForEventsWithFileAtURL:(NSURL *)URL error:(NSError **)error;
-
-// Creates a new subscribable that sends `self` when the file represented by the
-// receiver's `fileDescriptor` changes. The receiver must have a valid file
-// descriptor or an error will be sent with the domain
-// `RACNSFileHandleErrorDomain` and code
-// `RACNSFileHandleErrorInvalidFileDescriptor`.
-//
-// Note: The file handle is kept alive until the subscription is disposed of or
-// the file handle is closed.
-//
-// queue - the dispatch queue to which the event blocks are submitted. If NULL,
-// the default priority global queue is used.
-- (RACSubscribable *)rac_watchForEventsWithQueue:(dispatch_queue_t)queue;
 
 @end
---
+++ ReactiveCocoaFramework/ReactiveCocoa/NSFileManager+RACSupport.h
@@ -0,0 +1,34 @@
+//
+//  NSFileManager+RACSupport.h
+//  ReactiveCocoa
+//
+//  Created by Josh Abernathy on 9/18/12.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+extern NSString * const RACNSFileManagerErrorDomain;
+
+// An error occurred while trying to open the file at the URL.
+extern const NSInteger RACNSFileManagerErrorCouldNotOpenFile;
+
+// An error occurred while trying to create an event source for the file at the
+// URL.
+extern const NSInteger RACNSFileManagerErrorCouldNotCreateEventSource;
+
+@interface NSFileManager (RACSupport)
+
+// Creates a new subscribable that sends `URL` when the file changes. It sends
+// an error if a problem occurred trying to watch the file, and sends completed
+// if the file was deleted.
+//
+// Take care when watching bundles. Events can be delivered before bundle is
+// done being written. The FSEvents API might be a better alternative for that case.
+//
+// queue - the dispatch queue to which the event blocks are submitted. If NULL,
+// the default priority global queue is used.
++ (RACSubscribable *)rac_watchForEventsForFileAtURL:(NSURL *)URL queue:(dispatch_queue_t)queue;
+
+@end

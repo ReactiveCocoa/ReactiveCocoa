90570c6a0f2f7955c463e64a7d2c98f96bc6522d
joshaber <joshaber@gmail.com>
2012-09-20 00:10:55 -0400

    more documentation about the racey risks inherent in the API

--- ReactiveCocoaFramework/ReactiveCocoa/NSFileManager+RACSupport.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSFileManager+RACSupport.h
@@ -27,6 +27,15 @@ extern const NSInteger RACNSFileManagerErrorCouldNotCreateEventSource;
 // If the file at the URL is overwritten, it will re-register for events for the
 // new file and continue watching.
 //
+// Note that the distinction between deleted and overwritten is pretty weak and
+// prone to race conditions. Depending on how the file is written, there could
+// be a time between when a file is deleted and when the new file is written.
+// In that case, the subscribable could complete as it thinks the file was
+// deleted.
+//
+// Depending on how the subscribable's being used, callers could mitigate this
+// by calling -repeat: and -retry: on the returned subscribable.
+//
 // Take care when watching bundles. Events can be delivered before the bundle is
 // done being written. The FSEvents API might be a better alternative for that
 // case.

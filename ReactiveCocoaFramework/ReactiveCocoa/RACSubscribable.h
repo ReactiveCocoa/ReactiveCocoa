65f0d2a8d8a30810eae0ec4a511d73fdb1b51076
joshaber <joshaber@gmail.com>
2012-10-05 12:16:48 -0400

    lolgenerators

--- ReactiveCocoaFramework/ReactiveCocoa/RACSubscribable.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACSubscribable.h
@@ -38,34 +38,6 @@
 // *Note* that the `didSubscribe` block is called every time a subscriber subscribes.
 + (instancetype)createSubscribable:(RACDisposable * (^)(id<RACSubscriber> subscriber))didSubscribe;
 
-// Creates a new subscribable that uses the given block to generate values to
-// send to subscribers, based on the previous value. The subscribable completes
-// when `block` returns nil.
-//
-// Subscribers are sent values on the given scheduler, synchronously with
-// respect to the generation. So subscribers will get the next value and have a
-// chance to dispose of the subscription (and thus end the generator) before the
-// next value is generated. In this way, the generator only generates enough
-// values to satisify the subscriber's demand.
-//
-// Practically, this means users should always limit the subscribable on the
-// same scheduler passed in, otherwise the generator could outrun its need as
-// the limiting subscriber and the generator race to see who can work faster.
-//
-// scheduler - The scheduler on which the returned subscribable will generate
-// and send values. If it is nil, it uses `+[RACScheduler backgroundScheduler]`.
-//
-// start - The initial value sent to subscribers and then passed into `block` to
-// generate the next value.
-//
-// block - The block that generates a new value from the previous value. When
-// the block returns nil, the subscribable completes. If the block is nil, the
-// subscribable will repeatedly send `start`.
-+ (instancetype)generatorWithScheduler:(RACScheduler *)scheduler start:(id)start next:(id (^)(id x))block;
-
-// Calls `+generateWithScheduler:start:block:` with a nil scheduler.
-+ (instancetype)generatorWithStart:(id)start next:(id (^)(id x))block;
-
 // Returns a subscribable that immediately sends the given value and then
 // completes.
 + (instancetype)return:(id)value;

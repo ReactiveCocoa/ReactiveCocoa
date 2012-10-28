12053c137f83a547f4fef590f1e7b9279ff1a0c2
joshaber <joshaber@gmail.com>
2012-10-28 12:24:37 -0700

    documentation for RACBlockTrampoline

--- ReactiveCocoaFramework/ReactiveCocoa/RACBlockTrampoline.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACBlockTrampoline.h
@@ -8,8 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+// Allows a certain subset of dynamic block invocation.
 @interface RACBlockTrampoline : NSObject
 
+// Invokes the given block with the given arguments. The block must accept all
+// object arguments.
+//
+// block - The block to invoke. Must accept all object arguments and return an
+//         object. Cannot be nil.
+// arguments - The arguments with which to invoke the block. `RACTupleNil`s will
+//             be passed as nils.
+//
+// Returns the return value of invoking the block.
 + (id)invokeBlock:(id)block withArguments:(NSArray *)arguments;
 
 @end

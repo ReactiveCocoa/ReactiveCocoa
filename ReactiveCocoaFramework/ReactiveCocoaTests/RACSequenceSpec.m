469a92f6e46b5ef3985eef4496c37df23e14cff5
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-11-01 15:50:25 -0700

    Test <NSFastEnumeration> and -array

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
@@ -11,6 +11,32 @@
 
 #import "RACSequence.h"
 
+static NSString * const RACSequenceExamples = @"RACSequenceExamples";
+static NSString * const RACSequenceSequence = @"RACSequenceSequence";
+static NSString * const RACSequenceExpectedValues = @"RACSequenceExpectedValues";
+
+SharedExampleGroupsBegin(RACSequenceExamples);
+
+sharedExamplesFor(RACSequenceExamples, ^(NSDictionary *data) {
+	RACSequence *sequence = data[RACSequenceSequence];
+	NSArray *values = data[RACSequenceExpectedValues];
+
+	it(@"should implement <NSFastEnumeration>", ^{
+		NSMutableArray *collectedValues = [NSMutableArray array];
+		for (id value in sequence) {
+			[collectedValues addObject:value];
+		}
+
+		expect(collectedValues).to.equal(values);
+	});
+
+	it(@"should return an array", ^{
+		expect(sequence.array).to.equal(values);
+	});
+});
+
+SharedExampleGroupsEnd
+
 SpecBegin(RACSequence)
 
 it(@"should implement <RACStream>", ^{
@@ -27,6 +53,17 @@ it(@"should implement <RACStream>", ^{
 	};
 
 	itShouldBehaveLike(RACStreamExamples, @{ RACStreamExamplesClass: RACSequence.class, RACStreamExamplesVerifyValuesBlock: verifyValues });
+});
+
+it(@"empty sequences", ^{
+	itShouldBehaveLike(RACSequenceExamples, @{ RACSequenceSequence: RACSequence.empty, RACSequenceExpectedValues: @[] });
+});
+
+it(@"non-empty sequences", ^{
+	RACSequence *sequence = [[[RACSequence return:@0] concat:[RACSequence return:@1]] concat:[RACSequence return:@2]];
+	NSArray *values = @[ @0, @1, @2 ];
+
+	itShouldBehaveLike(RACSequenceExamples, @{ RACSequenceSequence: sequence, RACSequenceExpectedValues: values });
 });
 
 SpecEnd

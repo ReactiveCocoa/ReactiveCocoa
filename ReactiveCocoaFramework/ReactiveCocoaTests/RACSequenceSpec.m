cd3c16f9d919e9ef55b53e72de835be7131248df
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-11-01 16:01:57 -0700

    Tests for +sequenceWithHeadBlock:tailBlock:

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
@@ -93,11 +93,50 @@ it(@"should implement <RACStream>", ^{
 	itShouldBehaveLike(RACStreamExamples, @{ RACStreamExamplesClass: RACSequence.class, RACStreamExamplesVerifyValuesBlock: verifyValues });
 });
 
-it(@"empty sequences", ^{
+describe(@"+sequenceWithHeadBlock:tailBlock:", ^{
+	it(@"should use the values from the head and tail blocks", ^{
+		RACSequence *sequence = [RACSequence sequenceWithHeadBlock:^{
+			return @0;
+		} tailBlock:^{
+			return [RACSequence return:@1];
+		}];
+
+		expect(sequence).notTo.beNil();
+		expect(sequence.head).to.equal(@0);
+		expect(sequence.tail.head).to.equal(@1);
+		expect(sequence.tail.tail).to.beNil();
+	});
+
+	it(@"should lazily invoke head and tail blocks", ^{
+		__block BOOL headInvoked = NO;
+		__block BOOL tailInvoked = NO;
+
+		RACSequence *sequence = [RACSequence sequenceWithHeadBlock:^ id {
+			headInvoked = YES;
+			return nil;
+		} tailBlock:^ id {
+			tailInvoked = YES;
+			return nil;
+		}];
+
+		expect(sequence).notTo.beNil();
+		expect(headInvoked).to.beFalsy();
+		expect(tailInvoked).to.beFalsy();
+
+		expect(sequence.head).to.beNil();
+		expect(headInvoked).to.beTruthy();
+		expect(tailInvoked).to.beFalsy();
+
+		expect(sequence.tail).to.beNil();
+		expect(tailInvoked).to.beTruthy();
+	});
+});
+
+it(@"should support empty sequences", ^{
 	itShouldBehaveLike(RACSequenceExamples, @{ RACSequenceSequence: RACSequence.empty, RACSequenceExpectedValues: @[] });
 });
 
-it(@"non-empty sequences", ^{
+it(@"should support non-empty sequences", ^{
 	RACSequence *sequence = [[[RACSequence return:@0] concat:[RACSequence return:@1]] concat:[RACSequence return:@2]];
 	NSArray *values = @[ @0, @1, @2 ];
 

c654033822d754a425e3642704c7029edc1d15dc
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-11-01 16:16:33 -0700

    Tests for NSArray.rac_sequence

---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceAdditionsSpec.m
@@ -0,0 +1,44 @@
+//
+//  RACSequenceAdditionsSpec.m
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2012-11-01.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "RACSpecs.h"
+#import "RACSequenceExamples.h"
+
+#import "RACSequence.h"
+#import "NSArray+RACSequenceAdditions.h"
+#import "NSDictionary+RACSequenceAdditions.h"
+#import "NSOrderedSet+RACSequenceAdditions.h"
+#import "NSSet+RACSequenceAdditions.h"
+#import "NSString+RACSequenceAdditions.h"
+
+SpecBegin(RACSequenceAdditions)
+
+describe(@"NSArray sequences", ^{
+	__block NSMutableArray *values;
+	__block RACSequence *sequence;
+
+	before(^{
+		values = [@[ @0, @1, @2, @3, @4, @5 ] mutableCopy];
+		sequence = values.rac_sequence;
+	});
+
+	it(@"should initialize", ^{
+		expect(sequence).notTo.beNil();
+
+		itShouldBehaveLike(RACSequenceExamples, @{ RACSequenceSequence: sequence, RACSequenceExpectedValues: values });
+	});
+
+	it(@"should not change even if the underlying array does", ^{
+		NSArray *unchangedValues = [values copy];
+		[values addObject:@6];
+
+		itShouldBehaveLike(RACSequenceExamples, @{ RACSequenceSequence: sequence, RACSequenceExpectedValues: unchangedValues });
+	});
+});
+
+SpecEnd
--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
@@ -7,73 +7,10 @@
 //
 
 #import "RACSpecs.h"
+#import "RACSequenceExamples.h"
 #import "RACStreamExamples.h"
 
 #import "RACSequence.h"
-
-static NSString * const RACSequenceExamples = @"RACSequenceExamples";
-static NSString * const RACSequenceSequence = @"RACSequenceSequence";
-static NSString * const RACSequenceExpectedValues = @"RACSequenceExpectedValues";
-
-SharedExampleGroupsBegin(RACSequenceExamples);
-
-sharedExamplesFor(RACSequenceExamples, ^(NSDictionary *data) {
-	RACSequence *sequence = data[RACSequenceSequence];
-	NSArray *values = data[RACSequenceExpectedValues];
-
-	it(@"should implement <NSFastEnumeration>", ^{
-		NSMutableArray *collectedValues = [NSMutableArray array];
-		for (id value in sequence) {
-			[collectedValues addObject:value];
-		}
-
-		expect(collectedValues).to.equal(values);
-	});
-
-	it(@"should return an array", ^{
-		expect(sequence.array).to.equal(values);
-	});
-
-	it(@"should be equal to itself", ^{
-		expect(sequence).to.equal(sequence);
-	});
-
-	it(@"should be equal to the same sequence of values", ^{
-		RACSequence *newSequence = nil;
-		for (id value in values) {
-			RACSequence *valueSeq = [RACSequence return:value];
-			expect(valueSeq).notTo.beNil();
-
-			if (newSequence == nil) {
-				newSequence = valueSeq;
-			} else {
-				newSequence = [newSequence concat:valueSeq];
-			}
-		}
-		
-		expect(sequence).to.equal(newSequence);
-		expect(sequence.hash).to.equal(newSequence.hash);
-	});
-
-	it(@"should not be equal to a different sequence of values", ^{
-		RACSequence *anotherSequence = [RACSequence return:@(-1)];
-		expect(sequence).notTo.equal(anotherSequence);
-	});
-
-	it(@"should return an identical object for -copy", ^{
-		expect([sequence copy]).to.beIdenticalTo(sequence);
-	});
-
-	it(@"should archive", ^{
-		NSData *data = [NSKeyedArchiver archivedDataWithRootObject:sequence];
-		expect(data).notTo.beNil();
-
-		RACSequence *unarchived = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-		expect(unarchived).to.equal(sequence);
-	});
-});
-
-SharedExampleGroupsEnd
 
 SpecBegin(RACSequence)
 
@@ -105,6 +42,8 @@ describe(@"+sequenceWithHeadBlock:tailBlock:", ^{
 		expect(sequence.head).to.equal(@0);
 		expect(sequence.tail.head).to.equal(@1);
 		expect(sequence.tail.tail).to.beNil();
+
+		itShouldBehaveLike(RACSequenceExamples, @{ RACSequenceSequence: sequence, RACSequenceExpectedValues: @[ @0, @1 ] });
 	});
 
 	it(@"should lazily invoke head and tail blocks", ^{

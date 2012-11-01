64279ac6783a72a35a2ec6901aa95c1d29cd90bf
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-11-01 15:55:12 -0700

    Tests for RACSequence -isEqual:, -hash, <NSCopying>, <NSCoding>

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSequenceSpec.m
@@ -33,6 +33,44 @@ sharedExamplesFor(RACSequenceExamples, ^(NSDictionary *data) {
 	it(@"should return an array", ^{
 		expect(sequence.array).to.equal(values);
 	});
+
+	it(@"should be equal to itself", ^{
+		expect(sequence).to.equal(sequence);
+	});
+
+	it(@"should be equal to the same sequence of values", ^{
+		RACSequence *newSequence = nil;
+		for (id value in values) {
+			RACSequence *valueSeq = [RACSequence return:value];
+			expect(valueSeq).notTo.beNil();
+
+			if (newSequence == nil) {
+				newSequence = valueSeq;
+			} else {
+				newSequence = [newSequence concat:valueSeq];
+			}
+		}
+		
+		expect(sequence).to.equal(newSequence);
+		expect(sequence.hash).to.equal(newSequence.hash);
+	});
+
+	it(@"should not be equal to a different sequence of values", ^{
+		RACSequence *anotherSequence = [RACSequence return:@(-1)];
+		expect(sequence).notTo.equal(anotherSequence);
+	});
+
+	it(@"should return an identical object for -copy", ^{
+		expect([sequence copy]).to.beIdenticalTo(sequence);
+	});
+
+	it(@"should archive", ^{
+		NSData *data = [NSKeyedArchiver archivedDataWithRootObject:sequence];
+		expect(data).notTo.beNil();
+
+		RACSequence *unarchived = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+		expect(unarchived).to.equal(sequence);
+	});
 });
 
 SharedExampleGroupsEnd

ca470729413e37fbb25c5c8380df36b02d63dbb0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-11-09 14:56:40 -0800

    Fix GHAPIDemo calling -flattenMap:

--- GHAPIDemo/GHAPIDemo/GHDUserViewController.m
+++ GHAPIDemo/GHAPIDemo/GHDUserViewController.m
@@ -85,7 +85,7 @@
 		}] 
 		deliverOn:[RACScheduler sharedOperationQueueScheduler]] 
 		injectObjectWeakly:self]
-		flattenMap:^(RACTuple *t) {
+		flattenMap:^(RACTuple *t, BOOL *stop) {
 			GHDUserViewController *self = t.last;
 			return [self loadImageAtURL:t.first];
 		}] 

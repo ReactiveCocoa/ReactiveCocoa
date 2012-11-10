054677cb6281c189a5aac3c12ec88d08e06532bd
Uri Baghin <skerfat@gmail.com>
2012-11-10 13:45:37 +0100

    Added <RACStream> implementation for NSArray.

---
+++ ReactiveCocoaFramework/ReactiveCocoa/NSArray+RACStream.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+RACStream.h
+//  ReactiveCocoa
+//
+//  Created by Uri Baghin on 11/10/12.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "RACStream.h"
+
+@interface NSArray (RACStream) <RACStream>
+
+@end
---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/NSArrayRACStreamSpec.m
@@ -0,0 +1,31 @@
+//
+//  NSArrayRACStreamSpec.m
+//  ReactiveCocoa
+//
+//  Created by Uri Baghin on 11/10/12.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "RACSpecs.h"
+#import "RACStreamExamples.h"
+#import "RACUnit.h"
+
+SpecBegin(NSArrayRACStream)
+
+describe(@"<RACStream>", ^{
+	id verifyValues = ^(NSArray *array, NSArray *expectedValues) {
+		expect(array).notTo.beNil();
+    expect(array).to.equal(expectedValues);
+	};
+  
+  // NSArray cannot be infinite, cheat the tests for now
+	NSArray *infiniteArray = @[[RACUnit defaultUnit], [RACUnit defaultUnit], [RACUnit defaultUnit], [RACUnit defaultUnit], [RACUnit defaultUnit], [RACUnit defaultUnit], [RACUnit defaultUnit]];
+  
+	itShouldBehaveLike(RACStreamExamples, @{
+                     RACStreamExamplesClass: NSArray.class,
+                     RACStreamExamplesVerifyValuesBlock: verifyValues,
+                     RACStreamExamplesInfiniteStream: infiniteArray
+                     });
+});
+
+SpecEnd

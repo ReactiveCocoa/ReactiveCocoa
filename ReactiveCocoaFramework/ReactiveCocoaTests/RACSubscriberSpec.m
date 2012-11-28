608e15fe6bafc2e963c6d55502ba772a1911c99e
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-11-27 18:44:09 -0800

    Test that RACSubscriber doesn't send nexts after completed/error

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSubscriberSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSubscriberSpec.m
@@ -10,11 +10,14 @@
 #import "RACSubscriberExamples.h"
 
 #import "RACSubscriber.h"
+#import <libkern/OSAtomic.h>
 
 SpecBegin(RACSubscriber)
 
 __block RACSubscriber *subscriber;
 __block NSMutableSet *values;
+
+__block volatile BOOL finished;
 
 __block BOOL success;
 __block NSError *error;
@@ -22,10 +25,13 @@ __block NSError *error;
 beforeEach(^{
 	values = [NSMutableSet set];
 
+	finished = NO;
 	success = YES;
 	error = nil;
 
 	subscriber = [RACSubscriber subscriberWithNext:^(id value) {
+		expect(finished).to.beFalsy();
+
 		[values addObject:value];
 	} error:^(NSError *e) {
 		error = e;
@@ -40,5 +46,77 @@ itShouldBehaveLike(RACSubscriberExamples, [^{ return subscriber; } copy], [^(NSS
 	expect(error).to.beNil();
 	expect(values).to.equal(expectedValues);
 } copy], nil);
+
+describe(@"finishing", ^{
+	__block void (^sendValues)(void);
+	__block BOOL expectedSuccess;
+
+	__block dispatch_group_t dispatchGroup;
+	__block dispatch_queue_t concurrentQueue;
+
+	beforeEach(^{
+		dispatchGroup = dispatch_group_create();
+		expect(dispatchGroup).notTo.beNil();
+
+		concurrentQueue = dispatch_queue_create("com.github.ReactiveCocoa.RACSubscriberSpec", DISPATCH_QUEUE_CONCURRENT);
+		expect(concurrentQueue).notTo.beNil();
+
+		dispatch_suspend(concurrentQueue);
+
+		sendValues = [^{
+			for (NSUInteger i = 0; i < 15; i++) {
+				dispatch_group_async(dispatchGroup, concurrentQueue, ^{
+					[subscriber sendNext:@(i)];
+				});
+			}
+		} copy];
+
+		sendValues();
+	});
+
+	afterEach(^{
+		sendValues();
+		dispatch_resume(concurrentQueue);
+
+		// Time out after one second.
+		dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC));
+		expect(dispatch_group_wait(dispatchGroup, time)).to.equal(0);
+		
+		dispatch_release(dispatchGroup);
+		dispatchGroup = NULL;
+
+		dispatch_release(concurrentQueue);
+		concurrentQueue = NULL;
+
+		if (expectedSuccess) {
+			expect(success).to.beTruthy();
+			expect(error).to.beNil();
+		} else {
+			expect(success).to.beFalsy();
+		}
+	});
+
+	it(@"should never invoke next after sending completed", ^{
+		expectedSuccess = YES;
+
+		dispatch_group_async(dispatchGroup, concurrentQueue, ^{
+			[subscriber sendCompleted];
+
+			finished = YES;
+			OSMemoryBarrier();
+		});
+	});
+
+	it(@"should never invoke next after sending error", ^{
+		expectedSuccess = NO;
+
+		dispatch_group_async(dispatchGroup, concurrentQueue, ^{
+			[subscriber sendError:nil];
+
+			finished = YES;
+			OSMemoryBarrier();
+		});
+	});
+});
 
 SpecEnd

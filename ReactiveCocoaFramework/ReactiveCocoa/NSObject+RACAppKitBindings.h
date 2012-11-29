7eba5002417838bbd5b99c36020d5e1d8ca7c99d
Jonathan Willing <jwilling@me.com>
2012-11-29 10:33:26 -0600

    prefix methods

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACAppKitBindings.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACAppKitBindings.h
@@ -12,17 +12,17 @@
 @interface NSObject (RACAppKitBindings)
 
 // Calls -[NSObject bind:binding toObject:object withKeyPath:keyPath options:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSContinuouslyUpdatesValueBindingOption, nil]]
-- (void)bind:(NSString *)binding toObject:(id)object withKeyPath:(NSString *)keyPath;
+- (void)rac_bind:(NSString *)binding toObject:(id)object withKeyPath:(NSString *)keyPath;
 
 // Calls -[NSObject bind:binding toObject:object withKeyPath:keyPath options:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSContinuouslyUpdatesValueBindingOption, nilValue, NSNullPlaceholderBindingOption, nil]];
-- (void)bind:(NSString *)binding toObject:(id)object withKeyPath:(NSString *)keyPath nilValue:(id)nilValue;
+- (void)rac_bind:(NSString *)binding toObject:(id)object withKeyPath:(NSString *)keyPath nilValue:(id)nilValue;
 
 // Same as `-[NSObject bind:toObject:withKeyPath:] but also transforms values
 // using the given transform block.
-- (void)bind:(NSString *)binding toObject:(id)object withKeyPath:(NSString *)keyPath transform:(id (^)(id value))transformBlock;
+- (void)rac_bind:(NSString *)binding toObject:(id)object withKeyPath:(NSString *)keyPath transform:(id (^)(id value))transformBlock;
 
 // Same as `-[NSObject bind:toObject:withKeyPath:] but the value is transformed
 // by negating it.
-- (void)bind:(NSString *)binding toObject:(id)object withNegatedKeyPath:(NSString *)keyPath;
+- (void)rac_bind:(NSString *)binding toObject:(id)object withNegatedKeyPath:(NSString *)keyPath;
 
 @end

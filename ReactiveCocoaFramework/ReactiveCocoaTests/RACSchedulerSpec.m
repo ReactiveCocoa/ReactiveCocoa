0f37c35835be7d82f43ba5242639ab14f7ad9503
joshaber <joshaber@gmail.com>
2012-11-30 11:04:59 -0500

    tests for all the things

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSchedulerSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSchedulerSpec.m
@@ -7,64 +7,154 @@
 //
 
 #import "RACScheduler.h"
+#import "RACScheduler+Private.h"
+#import "RACDisposable.h"
 
 SpecBegin(RACScheduler)
 
-it(@"should know its current scheduler", ^{
-	RACScheduler *scheduler = RACScheduler.backgroundScheduler;
+RACScheduler * (^expectNonNilScheduler)(void (^)(void (^)(void))) = ^(void (^block)(void (^)(void))) {
 	__block RACScheduler *currentScheduler;
-	[scheduler schedule:^{
+	block(^{
 		currentScheduler = RACScheduler.currentScheduler;
-	}];
+	});
 
 	expect(currentScheduler).willNot.beNil();
-	expect(currentScheduler).to.equal(scheduler);
+	return currentScheduler;
+};
 
-	scheduler = RACScheduler.backgroundScheduler;
-	currentScheduler = nil;
-	[scheduler schedule:^{
-		[RACScheduler.deferredScheduler schedule:^{
-			currentScheduler = RACScheduler.currentScheduler;
-		}];
-	}];
+it(@"should know its current scheduler", ^{
+	void (^expectScheduler)(RACScheduler *, void (^)(void (^)(void))) = ^(RACScheduler *expectedScheduler, void (^block)(void (^)(void))) {
+		RACScheduler *currentScheduler = expectNonNilScheduler(block);
+		expect(currentScheduler).to.equal(expectedScheduler);
+	};
 
-	expect(currentScheduler).willNot.beNil();
-	expect(currentScheduler).to.equal(scheduler);
-
-	scheduler = RACScheduler.sharedBackgroundScheduler;
-	currentScheduler = nil;
-	[scheduler schedule:^{
-		[RACScheduler.immediateScheduler schedule:^{
-			currentScheduler = RACScheduler.currentScheduler;
-		}];
-	}];
-
-	expect(currentScheduler).willNot.beNil();
-	expect(currentScheduler).to.equal(scheduler);
-
-	scheduler = RACScheduler.mainQueueScheduler;
-	currentScheduler = nil;
-	[RACScheduler.backgroundScheduler schedule:^{
-		[RACScheduler.mainQueueScheduler schedule:^{
-			currentScheduler = RACScheduler.currentScheduler;
-		}];
-	}];
-
-	expect(currentScheduler).willNot.beNil();
-	expect(currentScheduler).to.equal(scheduler);
-
-	scheduler = RACScheduler.backgroundScheduler;
-	currentScheduler = nil;
-	[RACScheduler.backgroundScheduler schedule:^{
-		[RACScheduler.mainQueueScheduler schedule:^{
-			[scheduler schedule:^{
-				currentScheduler = RACScheduler.currentScheduler;
+	expectScheduler(RACScheduler.sharedBackgroundScheduler, ^(void (^captureCurrentScheduler)(void)) {
+		[RACScheduler.sharedBackgroundScheduler schedule:^{
+			[RACScheduler.deferredScheduler schedule:^{
+				captureCurrentScheduler();
 			}];
 		}];
+	});
+
+	expectScheduler(RACScheduler.sharedBackgroundScheduler, ^(void (^captureCurrentScheduler)(void)) {
+		[RACScheduler.sharedBackgroundScheduler schedule:^{
+			[RACScheduler.immediateScheduler schedule:^{
+				captureCurrentScheduler();
+			}];
+		}];
+	});
+
+	expectScheduler(RACScheduler.mainQueueScheduler, ^(void (^captureCurrentScheduler)(void)) {
+		[RACScheduler.mainQueueScheduler schedule:^{
+			[RACScheduler.backgroundScheduler schedule:^{
+				[RACScheduler.mainQueueScheduler schedule:^{
+					captureCurrentScheduler();
+				}];
+			}];
+		}];
+	});
+
+	RACScheduler *backgroundScheduler = RACScheduler.backgroundScheduler;
+	expectScheduler(backgroundScheduler, ^(void (^captureCurrentScheduler)(void)) {
+		[backgroundScheduler schedule:^{
+			[RACScheduler.mainQueueScheduler schedule:^{
+				[backgroundScheduler schedule:^{
+					captureCurrentScheduler();
+				}];
+			}];
+		}];
+	});
+});
+
+it(@"shouldn't execute a disposed block", ^{
+	__block BOOL executed = NO;
+	RACDisposable *disposable = [RACScheduler.deferredScheduler schedule:^{
+		executed = YES;
 	}];
 
-	expect(currentScheduler).willNot.beNil();
-	expect(currentScheduler).to.equal(scheduler);
+	expect(executed).to.beFalsy();
+	[disposable dispose];
+	expect(executed).willNot.beTruthy();
+});
+
+describe(@"+deferredScheduler", ^{
+	it(@"shouldn't execute the block immediately", ^{
+		__block BOOL executed = NO;
+		[RACScheduler.deferredScheduler schedule:^{
+			executed = YES;
+		}];
+
+		expect(executed).to.beFalsy();
+		expect(executed).will.beTruthy();
+	});
+});
+
+describe(@"+subscriptionScheduler", ^{
+	it(@"should always have a valid +currentScheduler from within a scheduled block", ^{
+		expectNonNilScheduler(^(void (^captureCurrentScheduler)(void)) {
+			dispatch_async(dispatch_get_main_queue(), ^{
+				[RACScheduler.subscriptionScheduler schedule:^{
+					captureCurrentScheduler();
+				}];
+			});
+		});
+
+		expectNonNilScheduler(^(void (^captureCurrentScheduler)(void)) {
+			dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+				[RACScheduler.subscriptionScheduler schedule:^{
+					captureCurrentScheduler();
+				}];
+			});
+		});
+	});
+
+	it(@"should execute scheduled blocks immediately if it's in a scheduler already", ^{
+		__block BOOL done = NO;
+		[RACScheduler.sharedBackgroundScheduler schedule:^{
+			__block BOOL executedImmediately = NO;
+			[RACScheduler.subscriptionScheduler schedule:^{
+				executedImmediately = YES;
+			}];
+
+			expect(executedImmediately).to.beTruthy();
+			done = YES;
+		}];
+
+		expect(done).will.beTruthy();
+	});
+});
+
+describe(@"+currentQueueScheduler", ^{
+	it(@"should flatten any recursive scheduled blocks", ^{
+		NSMutableArray *order = [NSMutableArray array];
+		[RACScheduler.currentQueueScheduler schedule:^{
+			[order addObject:@1];
+			[RACScheduler.currentQueueScheduler schedule:^{
+				[order addObject:@3];
+
+				[RACScheduler.currentQueueScheduler schedule:^{
+					[order addObject:@5];
+				}];
+
+				[order addObject:@4];
+			}];
+			[order addObject:@2];
+		}];
+
+		NSArray *expected = @[ @1, @2, @3, @4, @5 ];
+		expect(order).to.equal(expected);
+	});
+});
+
+describe(@"+immediateScheduler", ^{
+	it(@"should immediately execute scheduled blocks", ^{
+		__block BOOL executed = NO;
+		[RACScheduler.immediateScheduler schedule:^{
+			executed = YES;
+		}];
+
+		expect(executed).to.beTruthy();
+	});
 });
 
 SpecEnd

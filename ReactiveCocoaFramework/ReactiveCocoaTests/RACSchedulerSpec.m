491acaea7632414c86e584489fd7dd3847898e1a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-12-07 07:42:15 -0800

    Test rescheduling multiple times recursively

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACSchedulerSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSchedulerSpec.m
@@ -172,44 +172,46 @@ describe(@"+immediateScheduler", ^{
 });
 
 describe(@"-scheduleRecursiveBlock:", ^{
-	it(@"should behave like a normal block when it doesn't invoke itself", ^{
-		__block BOOL executed = NO;
-		[RACScheduler.mainThreadScheduler scheduleRecursiveBlock:^(void (^recurse)(void)) {
-			expect(executed).to.beFalsy();
-			executed = YES;
-		}];
+	describe(@"with an asynchronous scheduler", ^{
+		it(@"should behave like a normal block when it doesn't invoke itself", ^{
+			__block BOOL executed = NO;
+			[RACScheduler.mainThreadScheduler scheduleRecursiveBlock:^(void (^recurse)(void)) {
+				expect(executed).to.beFalsy();
+				executed = YES;
+			}];
 
-		expect(executed).will.beTruthy();
-	});
+			expect(executed).will.beTruthy();
+		});
 
-	it(@"should reschedule itself after the caller completes", ^{
-		__block NSUInteger count = 0;
-		[RACScheduler.mainThreadScheduler scheduleRecursiveBlock:^(void (^recurse)(void)) {
-			NSUInteger thisCount = ++count;
-			if (thisCount < 2) {
+		it(@"should reschedule itself after the caller completes", ^{
+			__block NSUInteger count = 0;
+			[RACScheduler.mainThreadScheduler scheduleRecursiveBlock:^(void (^recurse)(void)) {
+				NSUInteger thisCount = ++count;
+				if (thisCount < 3) {
+					recurse();
+
+					// The block shouldn't have been invoked again yet, only
+					// scheduled.
+					expect(count).to.equal(thisCount);
+				}
+			}];
+
+			expect(count).will.equal(3);
+		});
+
+		it(@"shouldn't reschedule itself when disposed", ^{
+			__block NSUInteger count = 0;
+			__block RACDisposable *disposable = [RACScheduler.mainThreadScheduler scheduleRecursiveBlock:^(void (^recurse)(void)) {
+				++count;
+
+				expect(disposable).notTo.beNil();
+				[disposable dispose];
+
 				recurse();
+			}];
 
-				// The block shouldn't have been invoked again yet, only
-				// scheduled.
-				expect(count).to.equal(thisCount);
-			}
-		}];
-
-		expect(count).will.equal(2);
-	});
-
-	it(@"shouldn't reschedule itself when disposed", ^{
-		__block NSUInteger count = 0;
-		__block RACDisposable *disposable = [RACScheduler.mainThreadScheduler scheduleRecursiveBlock:^(void (^recurse)(void)) {
-			++count;
-
-			expect(disposable).notTo.beNil();
-			[disposable dispose];
-
-			recurse();
-		}];
-
-		expect(count).will.equal(1);
+			expect(count).will.equal(1);
+		});
 	});
 });
 

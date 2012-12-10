8565df4a02eacfa959f879d5dcc33849a5c4e14c
Uri Baghin <skerfat@gmail.com>
2012-12-10 22:40:27 +0100

    Updated docs.

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACBindings.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACBindings.h
@@ -19,20 +19,14 @@
 // `otherKeyPath` on `otherObject`. After that, the two properties will be kept
 // in sync by forwarding changes to one onto the other.
 //
-// WARNING: for this method to work, both properties must return a value equal
-// to the last one that has been set, for each value that is set. This excludes
-// setting `nil` on scalar properties, even if `-setNilValueForKey:` is
-// overridden. In this context, a equals b if both are `nil` or if
-// `[a isEqual:b]` returns `YES`.
-//
 // receiverKeyPath     - The key path of the receiver to bind.
 // receiverScheduler   - An optional scheduler on which all accesses to the
 //                       receiver will be scheduled. If not specified, the
 //                       receiver will be accessed from wherever the binding
 //                       target is modified.
 // receiverTransformer - An optional block with which to transform values from
-//                       the receiver to the binding target. Must be
-//                       thread-safe.
+//                       the receiver to the binding target. Will be scheduled
+//                       on `receiverScheduler`.
 // otherObject         - The object with which to bind the receiver.
 // otherKeyPath        - The key path of the binding target to bind.
 // otherScheduler      - An optional scheduler on which all accesses to the
@@ -40,8 +34,8 @@
 //                       binding target will be accessed from wherever the
 //                       receiver is modified.
 // otherTransformer    - An optional block with which to transform values from
-//                       the binding target to the receiver. Must be
-//                       thread-safe.
+//                       the binding target to the receiver. Will be scheduled
+//                       on `otherScheduler`.
 // 
 // Returns a disposable that can be used to sever the binding.
 - (RACDisposable *)rac_bind:(NSString *)receiverKeyPath transformer:(id(^)(id value))receiverTransformer onScheduler:(RACScheduler *)receiverScheduler toObject:(id)otherObject withKeyPath:(NSString *)otherKeyPath transformer:(id(^)(id value))otherTransformer onScheduler:(RACScheduler *)otherScheduler;

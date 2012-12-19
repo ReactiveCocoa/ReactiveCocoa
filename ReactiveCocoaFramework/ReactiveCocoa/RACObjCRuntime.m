4f64cf0a6c798a129efe19628c72da6a3da57d15
Jonathan Sterling <jonsterling@me.com>
2012-12-19 11:54:33 -0800

    Change order of method lookup cases; add explanatory comment

--- ReactiveCocoaFramework/ReactiveCocoa/RACObjCRuntime.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACObjCRuntime.m
@@ -11,9 +11,11 @@
 @implementation RACObjCRuntime
 
 + (void)findMethod:(SEL)method inProtocol:(Protocol *)protocol outMethod:(struct objc_method_description *)outMethod {
-    *outMethod = protocol_getMethodDescription(protocol, method, NO, YES);
+    // First, we look for a @required method. If none is found, we look for an
+    // @optional method.
+    *outMethod = protocol_getMethodDescription(protocol, method, YES, YES);
     if (outMethod->name == NULL) {
-        *outMethod = protocol_getMethodDescription(protocol, method, YES, YES);
+        *outMethod = protocol_getMethodDescription(protocol, method, NO, YES);
     }
 }
 

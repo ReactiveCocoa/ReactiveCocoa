6534da34bb82e41f60c78a7d35e2a7de98f6f83f
joshaber <joshaber@gmail.com>
2012-12-21 15:42:28 -0500

    fix a retain cycle caused by lift

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACLifting.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACLifting.m
@@ -32,10 +32,8 @@
 	NSMethodSignature *methodSignature = [self methodSignatureForSelector:selector];
 	NSAssert(methodSignature != nil, @"%@ does not respond to %@", self, NSStringFromSelector(selector));
 
-	NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
-	invocation.selector = selector;
-
 	NSMutableArray *signals = [NSMutableArray arrayWithCapacity:methodSignature.numberOfArguments - 2];
+	NSMutableArray *arguments = [NSMutableArray arrayWithCapacity:methodSignature.numberOfArguments - 2];
 	NSMutableDictionary *argIndexesBySignal = [NSMutableDictionary dictionaryWithCapacity:methodSignature.numberOfArguments - 2];
 
 	va_list args;
@@ -45,35 +43,41 @@
 	for (NSUInteger i = 2; i < methodSignature.numberOfArguments; i++) {
 		currentObject = (i == 2 ? arg : va_arg(args, id));
 
+		[arguments addObject:currentObject ?: RACTupleNil.tupleNil];
+
 		if ([currentObject isKindOfClass:RACSignal.class]) {
-			[invocation rac_setArgument:nil atIndex:i];
-			argIndexesBySignal[[NSValue valueWithNonretainedObject:currentObject]] = @(i);
+			argIndexesBySignal[[NSValue valueWithNonretainedObject:currentObject]] = @(i - 2);
 			[signals addObject:currentObject];
-		} else {
-			[invocation rac_setArgument:currentObject atIndex:i];
 		}
 	}
 	va_end(args);
 
-	[invocation retainArguments];
+	id (^invokeWithTarget)(id) = [^(id target) {
+		NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSignature];
+		invocation.selector = selector;
+
+		for (NSUInteger i = 0; i < arguments.count; i++) {
+			[invocation rac_setArgument:[arguments[i] isKindOfClass:RACTupleNil.class] ? nil : arguments[i] atIndex:i + 2];
+		}
+
+		[invocation invokeWithTarget:target];
+		return [invocation rac_returnValue];
+	} copy];
 
 	if (signals.count < 1) {
-		[invocation invokeWithTarget:self];
-		return [invocation rac_returnValue];
+		return invokeWithTarget(self);
 	} else {
 		@unsafeify(self);
-		return [self rac_liftSignals:signals withReducingInvocation:^(RACTuple *xs) {
+		return [self rac_liftSignals:signals withReducingInvocation:^ id (RACTuple *xs) {
 			@strongify(self);
+
 			for (NSUInteger i = 0; i < xs.count; i++) {
 				RACSignal *signal = signals[i];
 				NSUInteger argIndex = [argIndexesBySignal[[NSValue valueWithNonretainedObject:signal]] unsignedIntegerValue];
-				[invocation rac_setArgument:xs[i] atIndex:argIndex];
-				[invocation retainArguments];
+				[arguments replaceObjectAtIndex:argIndex withObject:xs[i] ?: RACTupleNil.tupleNil];
 			}
 
-			[invocation invokeWithTarget:self];
-
-			return [invocation rac_returnValue];
+			return invokeWithTarget(self);
 		}];
 	}
 }

5bc87586f36e9f4ff67ebe88208367c33edd984e
Uri Baghin <skerfat@gmail.com>
2012-12-30 22:23:17 +0100

    Added RACLazyProperty docs.

--- ReactiveCocoaFramework/ReactiveCocoa/RACLazyProperty.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACLazyProperty.h
@@ -8,8 +8,11 @@
 
 #import <ReactiveCocoa/ReactiveCocoa.h>
 
+// A property with a lazily generated default value.
 @interface RACLazyProperty : RACProperty
 
+// Creates a new lazy property. The property's default value is the first value
+// sent by `start`. `start` must send at least one value.
 + (instancetype)lazyPropertyWithStart:(RACSignal *)start;
 
 @end

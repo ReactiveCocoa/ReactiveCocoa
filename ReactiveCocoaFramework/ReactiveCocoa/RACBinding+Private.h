010d2e499cddb1a1c8f93422af7acb8eebe23310
Uri Baghin <skerfat@gmail.com>
2013-01-02 10:10:41 +0100

    Refer to instances of RACBinding in docs.

--- ReactiveCocoaFramework/ReactiveCocoa/RACBinding+Private.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACBinding+Private.h
@@ -14,15 +14,15 @@
 //
 // signal     - A signal of `RACTuple`s where the first element is the value of
 //              the binding's property as it changes, and the second element is
-//              the binding that triggered the change, or `nil` if the change
-//              was triggered by other means. The signal must also send a
+//              the `RACBinding` that triggered the change, or `nil` if the
+//              change was triggered by other means. The signal must also send a
 //              `RACTuple` with the current value and it's originator on
 //              subscription.
 // subscriber - A subscriber that will be sent a `RACTuple` every time the
 //              binding's property is changed. The first element will be the new
-//              value, the second element will be the binding that triggered the
-//              change or nil if the change was triggered by the property
-//              itself.
+//              value, the second element will be the `RACBinding` that
+//              triggered the change or nil if the change was triggered by the
+//              property itself.
 - (instancetype)initWithSignal:(RACSignal *)signal subscriber:(id<RACSubscriber>)subscriber;
 
 @end

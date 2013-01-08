6d33f6d38d8b93d486f6cd84dcb2865a5be49864
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-01-08 15:50:30 -0800

    Use compare-and-swap instead of a mutex in RACDisposable

--- ReactiveCocoaFramework/ReactiveCocoa/RACDisposable.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACDisposable.m
@@ -8,33 +8,58 @@
 
 #import "RACDisposable.h"
 #import "RACScopedDisposable.h"
+#import <libkern/OSAtomic.h>
 
-@interface RACDisposable ()
-@property (nonatomic, copy) void (^disposeBlock)(void);
+@interface RACDisposable () {
+	// A copied block of type void (^)(void) containing the logic for disposal,
+	// or NULL if the receiver is already disposed.
+	//
+	// This should only be used atomically.
+	void * volatile _disposeBlock;
+}
+
 @end
-
 
 @implementation RACDisposable
 
-
-#pragma mark API
-
-@synthesize disposeBlock;
+#pragma mark Lifecycle
 
 + (instancetype)disposableWithBlock:(void (^)(void))block {
 	RACDisposable *disposable = [[self alloc] init];
-	disposable.disposeBlock = block;
+
+	id copiedBlock = [block copy];
+	disposable->_disposeBlock = (void *)CFBridgingRetain(copiedBlock);
+
+	// Force the store to _disposeBlock to complete.
+	OSMemoryBarrier();
+
 	return disposable;
 }
 
-- (void)dispose {
-	@synchronized(self) {
-		if(self.disposeBlock != NULL) {
-			self.disposeBlock();
-			self.disposeBlock = NULL;
-		}
+- (void)dealloc {
+	if (_disposeBlock != NULL) {
+		CFRelease(_disposeBlock);
+		_disposeBlock = NULL;
 	}
 }
+
+#pragma mark Disposal
+
+- (void)dispose {
+	void (^disposeBlock)(void) = NULL;
+
+	while (YES) {
+		void *blockPtr = _disposeBlock;
+		if (OSAtomicCompareAndSwapPtrBarrier(blockPtr, NULL, &_disposeBlock)) {
+			disposeBlock = CFBridgingRelease(blockPtr);
+			break;
+		}
+	}
+
+	if (disposeBlock != nil) disposeBlock();
+}
+
+#pragma mark Scoped Disposables
 
 - (RACScopedDisposable *)asScopedDisposable {
 	return [RACScopedDisposable scopedDisposableWithDisposable:self];

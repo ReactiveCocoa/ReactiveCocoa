9f77bb7d99118ca52c1758da161542cfa2a0f4a5
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-01-08 19:20:14 -0800

    s/more correct/safer

--- ReactiveCocoaFramework/ReactiveCocoa/RACDynamicSequence.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACDynamicSequence.m
@@ -84,7 +84,7 @@
 - (void)dealloc {
 	// Prevents overflowing the stack when deallocating a long chain of dynamic
 	// sequences (which results in a lot of nested calls to -dealloc). This is
-	// obviously not ideal performance-wise, but is more correct.
+	// obviously not ideal performance-wise, but is safer.
 	__autoreleasing RACSequence *tail __attribute__((unused)) = _tail;
 }
 

464587741ce22b0e3fa117cffb9579284a0c2135
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-02-03 11:26:49 -0800

    Factor out RACCommand tests into a shared example group

---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACCommandExamples.h
@@ -0,0 +1,13 @@
+//
+//  RACCommandExamples.h
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2013-02-03.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+// The name of the shared examples for a RACCommand subclass.
+extern NSString * const RACCommandExamples;
+
+// The RACCommand subclass to test.
+extern NSString * const RACCommandExamplesClass;
---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACSignalCommandSpec.m
@@ -0,0 +1,9 @@
+//
+//  RACSignalCommandSpec.m
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2013-02-03.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#include <stdio.h>

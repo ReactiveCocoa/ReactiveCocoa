95c43592ba7f5561b8b496a9c7677b15954fe985
joshaber <joshaber@gmail.com>
2013-03-12 12:15:01 -0400

    remove the disposable after the scheduled block has been executed

--- ReactiveCocoaFramework/ReactiveCocoa/RACSignal+Operations.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACSignal+Operations.m
@@ -1100,8 +1100,11 @@ static RACDisposable *concatPopNextSignal(NSMutableArray *signals, BOOL *outerDo
 	return [[RACSignal createSignal:^(id<RACSubscriber> subscriber) {
 		RACCompoundDisposable *disposable = [RACCompoundDisposable compoundDisposable];
 
-		void (^schedule)(id) = [^(id block) {
-			RACDisposable *schedulingDisposable = [scheduler schedule:block];
+		void (^schedule)(dispatch_block_t) = [^(dispatch_block_t block) {
+			__block RACDisposable *schedulingDisposable = [scheduler schedule:^{
+				[disposable removeDisposable:schedulingDisposable];
+				block();
+			}];
 			if (schedulingDisposable != nil) [disposable addDisposable:schedulingDisposable];
 		} copy];
 

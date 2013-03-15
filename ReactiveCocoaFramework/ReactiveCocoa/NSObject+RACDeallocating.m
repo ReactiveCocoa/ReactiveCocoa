67b1ad1e3e40e6440e301401f1ee88d5ade5c497
Kazuo Koga <obiwanko@me.com>
2013-03-15 21:50:36 +0900

    rewrite with rac_addDeallocDisposable:

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACDeallocating.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACDeallocating.m
@@ -7,32 +7,18 @@
 //
 
 #import "NSObject+RACDeallocating.h"
+#import "NSObject+RACPropertySubscribing.h"
+#import "RACDisposable.h"
 #import "RACSubject.h"
-#import <objc/runtime.h>
-
-@interface rac_RACDeallocTracker : NSObject
-@property (nonatomic) RACSubject *subject;
-@end
-
-@implementation rac_RACDeallocTracker
-
-- (void)dealloc {
-	[_subject sendCompleted];
-}
-
-@end
 
 @implementation NSObject (RACDeallocating)
 
 - (RACSignal *)rac_didDeallocSignal {
 	RACSubject *subject = [RACSubject subject];
 
-	rac_RACDeallocTracker *tracker = [[rac_RACDeallocTracker alloc] init];
-	tracker.subject = subject;
-
-	// make retain chain: self -> tracker -> subject.
-	objc_setAssociatedObject(self, (__bridge CFTypeRef)tracker, tracker,
-		OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	[self rac_addDeallocDisposable:[RACDisposable disposableWithBlock:^{
+		[subject sendCompleted];
+	}]];
 
 	return subject;
 }

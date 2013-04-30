8942bd9c4db5adc0e57d6af875d8d4a39fe1a4f8
Uri Baghin <skerfat@gmail.com>
2013-04-30 13:00:32 +0200

    Added note about RACObservablePropertySubject's thread-safety.

--- ReactiveCocoaFramework/ReactiveCocoa/RACObservablePropertySubject.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACObservablePropertySubject.h
@@ -37,6 +37,10 @@
 // bindings' subscribers. `keyPath` will be updated with values sent to the
 // wrapper or it's bindings. Subscribers of the wrapper or it's bindings will be
 // sent the current value of `keyPath`.
+//
+// Note: RACObservablePropertySubject and the RACBinding it returns are not
+// thread-safe and should not observe atomic properties, or be bound to a
+// RACProperty whose value can be changed from a different thread.
 @interface RACObservablePropertySubject : RACPropertySubject
 
 // Returns a new RACPropertySubject wrapper for `keyPath` on `target` with a

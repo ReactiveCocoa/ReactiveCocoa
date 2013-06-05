9e15492679c7e8582fd1dd0720c586154bbc5d49
Dave Lee <davelee.com@gmail.com>
2013-06-05 17:31:02 -0600

    Refactor -rac_signalForSelector: to adopt a runtime proxy subclass
    With this change, calling -rac_signalForSelector: will cause the receiver to
    adopt a proxy subclass, creating one if none exists for its class. This proxy
    subclass is where changes are made to the dispatch table. The proxy subclass
    will always have an implementation of -forwardInvocation: which is defined by
    RACSignalForSelectorForwardingIMP. Additionally, the proxy subclass will have
    one or more of its parent methods redefined to point to _objc_msgForward to
    allow -forwardInvocation: to intercede and send nexts to the appropriate
    subjects before forwarding to the original implementation.

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACSelectorSignal.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACSelectorSignal.m
@@ -19,8 +19,26 @@ static const void *RACObjectSelectorSignals = &RACObjectSelectorSignals;
 
 @implementation NSObject (RACSelectorSignal)
 
-static RACSignal *NSObjectRACSignalForSelector(id self, SEL _cmd, SEL selector) {
-	// ???: Should self's class be synchronized?
+static void RACSignalForSelectorForwardingIMP(id self, SEL _cmd, NSInvocation *invocation) {
+	NSString *selectorName = NSStringFromSelector(invocation.selector);
+
+	NSMutableDictionary *selectorSignals = objc_getAssociatedObject(self, RACObjectSelectorSignals);
+	if (selectorSignals != nil) {
+		RACSubject *subject = selectorSignals[selectorName];
+		if (subject != nil) {
+			RACTuple *argumentsTuple = [RACTuple tupleWithObjectsFromArray:invocation.rac_allArguments];
+			[subject sendNext:argumentsTuple];
+		}
+	}
+
+	SEL reservedSelector = NSSelectorFromString([@"rac_forward_" stringByAppendingString:selectorName]);
+	if ([invocation.target respondsToSelector:reservedSelector]) {
+		invocation.selector = reservedSelector;
+		[invocation invoke];
+	}
+}
+
+static RACSignal *NSObjectRACSignalForSelector(id self, SEL selector) {
 	@synchronized(self) {
 		NSMutableDictionary *selectorSignals = objc_getAssociatedObject(self, RACObjectSelectorSignals);
 		if (selectorSignals == nil) {
@@ -33,58 +51,48 @@ static RACSignal *NSObjectRACSignalForSelector(id self, SEL _cmd, SEL selector) 
 		if (subject != nil) return subject;
 
 		subject = selectorSignals[selectorName] = [RACSubject subject];
-
-		Class class = object_getClass(self);
-		SEL reservedSelector = NSSelectorFromString([@"rac_forward_" stringByAppendingString:selectorName]);
-		if ([class instancesRespondToSelector:reservedSelector] && class_getMethodImplementation(class, selector) == _objc_msgForward) {
-			return subject;
-		}
-
-		Method method = class_getInstanceMethod(class, selector);
-		if (method != NULL) {
-			// Alias the existing method to reservedSelector.
-			class_addMethod(class, reservedSelector, method_getImplementation(method), method_getTypeEncoding(method));
-			// Redefine the selector to call -forwardInvocation:
-			method_setImplementation(method, _objc_msgForward);
-		} else {
-			// Define the selector to call -forwardInvocation:
-			class_addMethod(class, selector, _objc_msgForward, "v@:@");
-		}
-
-		IMP imp = imp_implementationWithBlock(^(id self, NSInvocation *invocation) {
-			NSMutableDictionary *selectorSignals = objc_getAssociatedObject(self, RACObjectSelectorSignals);
-			if (selectorSignals != nil) {
-				RACSubject *subject = selectorSignals[selectorName];
-				if (subject != nil) {
-					RACTuple *argumentsTuple = [RACTuple tupleWithObjectsFromArray:invocation.rac_allArguments];
-					[subject sendNext:argumentsTuple];
-				}
-			}
-
-			// ???: Consider methods that return non-void.
-			if ([invocation.target respondsToSelector:reservedSelector]) {
-				invocation.selector = reservedSelector;
-				[invocation invoke];
-			}
-		});
-
-		// TODO: Handle case where -forwardInvocation: exists.
-		class_replaceMethod(class, @selector(forwardInvocation:), imp, "v@:@");
-
 		[self rac_addDeallocDisposable:[RACDisposable disposableWithBlock:^{
 			[subject sendCompleted];
 		}]];
+
+		Class class = object_getClass(self);
+		NSString *subclassSuffix = class_isMetaClass(class) ? @"_RACClassProxy" : @"_RACObjectProxy";
+		NSString *proxySubclassName = [[class description] stringByAppendingString:subclassSuffix];
+		Class proxySubclass = NSClassFromString(proxySubclassName);
+
+		if (proxySubclass == nil) {
+			proxySubclass = objc_allocateClassPair(class, proxySubclassName.UTF8String, 0);
+			objc_registerClassPair(proxySubclass);
+		}
+
+		class_replaceMethod(proxySubclass, @selector(forwardInvocation:), (IMP)RACSignalForSelectorForwardingIMP, "v@:@");
+
+		Method method = class_getInstanceMethod(proxySubclass, selector);
+		if (method_getImplementation(method) != _objc_msgForward) {
+			SEL reservedSelector = NSSelectorFromString([@"rac_forward_" stringByAppendingString:selectorName]);
+			if (method != NULL) {
+				// Alias the existing method to reservedSelector.
+				class_addMethod(proxySubclass, reservedSelector, method_getImplementation(method), method_getTypeEncoding(method));
+				// Redefine the selector to call -forwardInvocation:
+				method_setImplementation(method, _objc_msgForward);
+			} else {
+				// Define the selector to call -forwardInvocation:
+				class_replaceMethod(proxySubclass, selector, _objc_msgForward, "v@:@");
+			}
+		}
+
+		object_setClass(self, proxySubclass);
 
 		return subject;
 	}
 }
 
 - (RACSignal *)rac_signalForSelector:(SEL)selector {
-	return NSObjectRACSignalForSelector(self, _cmd, selector);
+	return NSObjectRACSignalForSelector(self, selector);
 }
 
 + (RACSignal *)rac_signalForSelector:(SEL)selector {
-	return NSObjectRACSignalForSelector(self, _cmd, selector);
+	return NSObjectRACSignalForSelector(self, selector);
 }
 
 @end

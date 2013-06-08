25e282506ae9d1e65cdf5836f1ae15104d852e7d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-06-07 19:12:18 -0700

    Use a default name when nil for RACTargetQueueScheduler

--- ReactiveCocoaFramework/ReactiveCocoa/RACTargetQueueScheduler.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACTargetQueueScheduler.m
@@ -15,6 +15,10 @@
 - (id)initWithName:(NSString *)name targetQueue:(dispatch_queue_t)targetQueue {
 	NSCParameterAssert(targetQueue != NULL);
 
+	if (name == nil) {
+		name = [NSString stringWithFormat:@"com.ReactiveCocoa.RACTargetQueueScheduler(%s)", dispatch_queue_get_label(targetQueue)];
+	}
+
 	dispatch_queue_t queue = dispatch_queue_create(name.UTF8String, DISPATCH_QUEUE_SERIAL);
 	if (queue == NULL) return nil;
 

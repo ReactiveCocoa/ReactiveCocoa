727fe2a9d12d0fa8e1d6a9844696bd43b6875146
Henrik Hodne <me@henrikhodne.com>
2013-06-17 21:08:09 +0200

    Improve docs for UIAlertView+RACCommandSupport.

--- ReactiveCocoaFramework/ReactiveCocoa/UIAlertView+RACCommandSupport.h
+++ ReactiveCocoaFramework/ReactiveCocoa/UIAlertView+RACCommandSupport.h
@@ -12,11 +12,13 @@
 
 @interface UIAlertView (RACCommandSupport)
 
-// Sets the alert view's command. When a button on the alert view is pressed,
-// the command is executed with the index of the button that was pressed.
+// A command that's executed when a button on the alert view is pressed.
+// The command is executed with an `NSNumber` containing the index of the
+// button that was pressed (see
+// `-[UIAlertViewDelegate alertView:clickedButtonAtIndex:]`).
 //
-// This will override the alert view's delegate, so you can't use this together
-// with a custom delegate.
+// Note: This will override the alert view's delegate, so you can't use this
+// together with a custom delegate.
 @property (nonatomic, strong) RACCommand *rac_command;
 
 @end

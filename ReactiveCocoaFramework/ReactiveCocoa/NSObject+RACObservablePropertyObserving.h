88ff6a647c2e37fd47df45260153820827c2cc1c
Uri Baghin <skerfat@gmail.com>
2013-06-19 17:30:23 +0200

    Clean up docs of RACObservablePropertyObserving.

--- ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACObservablePropertyObserving.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSObject+RACObservablePropertyObserving.h
@@ -13,20 +13,26 @@
 @interface NSObject (RACObservablePropertyObserving)
 
 // Adds the given blocks as the callbacks for when the key path changes. Unlike
-// direct KVO observation this handles deallocation of intermediate objects. At
-// least one of the two callbacks must be non-nil.
+// direct KVO observation this handles deallocation of intermediate objects.
 //
-// The blocks are passed whether the change was triggered by a change or by the
-// deallocation of a value, whether the value was that of the last key path
-// component or of an intermediate key path component, and the new value of the
-// key path. The observation does not need to be explicitly removed. It will be
-// removed when the observer or the receiver deallocate. The blocks can be
-// called on different threads, but will not be called concurrently.
+// The observation does not need to be explicitly removed. It will be removed
+// when the observer or the receiver deallocate. The blocks can be called on
+// different threads, but will not be called concurrently.
 //
-// observer        - The object that requested the observation.
-// keyPath         - The key path to observe.
-// willChangeBlock - The block called before the value at the key path changes.
-// didChangeBlock  - The block called after the value at the key path changes.
+// observer          - The object that requested the observation.
+// keyPath           - The key path to observe.
+// serializationLock - The lock used to serialize calls to willChangeBlock and
+//                     didChangeBlock. If not provided, one will be created.
+// willChangeBlock   - The block called before the value at the key path
+//                     changes. It is passed whether the key path component
+//                     whose value will be changed explicitly is the last one or
+//                     an intermediate one.
+// didChangeBlock    - The block called after the value at the key path changes.
+//                     It is passed whether the key path component whose value
+//                     was changed explicitly was the last one or an
+//                     intermediate one, whether the change was caused by the
+//                     deallocation of a value, and the new value of the key
+//                     path.
 //
 // Returns a disposable that can be used to stop the observation.
 - (RACDisposable *)rac_addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath willChangeBlock:(void(^)(BOOL triggeredByLastKeyPathComponent))willChangeBlock didChangeBlock:(void(^)(BOOL triggeredByLastKeyPathComponent, BOOL triggeredByDeallocation, id value))didChangeBlock;

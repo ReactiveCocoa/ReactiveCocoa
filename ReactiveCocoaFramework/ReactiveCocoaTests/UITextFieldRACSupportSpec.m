72dca5d9c6633e64b77222672037f0386261cd28
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-06-22 00:40:19 -0700

    (Failing) tests for UITextField and UITextView support

---
+++ ReactiveCocoaFramework/ReactiveCocoaTests/UITextFieldRACSupportSpec.m
@@ -0,0 +1,34 @@
+//
+//  UITextFieldRACSupportSpec.m
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2013-06-22.
+//  Copyright (c) 2013 GitHub, Inc. All rights reserved.
+//
+
+#import "RACSignal.h"
+#import "RACSignal+Operations.h"
+#import "UITextField+RACSignalSupport.h"
+
+SpecBegin(UITextFieldRACSupport)
+
+describe(@"-rac_textSignal", ^{
+	__block UITextField *textField;
+
+	beforeEach(^{
+		textField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 100, 20)];
+		expect(textField).notTo.beNil();
+	});
+
+	it(@"should start with the initial text", ^{
+		textField.text = @"foo";
+
+		RACSignal *textSignal = textField.rac_textSignal;
+		expect([textSignal first]).to.equal(@"foo");
+
+		textField.text = @"bar";
+		expect([textSignal first]).to.equal(@"bar");
+	});
+});
+
+SpecEnd

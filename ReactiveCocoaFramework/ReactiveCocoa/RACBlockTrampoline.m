846f879896ca26d40426799e12c2d96a64ec1c1d
Kazuo Koga <obiwanko@me.com>
2013-06-22 17:50:47 +0900

    Revert "Add case of arg count = 0"
    This reverts commit c0f44328d9863ce869ddd3a5adf1df173e52d1cb.

--- ReactiveCocoaFramework/ReactiveCocoa/RACBlockTrampoline.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACBlockTrampoline.m
@@ -21,7 +21,6 @@ static SEL selectorForArgumentCount(NSUInteger count) {
 	NSCParameterAssert(count > 0);
 
 	switch (count) {
-		case 0: return NULL;
 		case 1: return @selector(performWith:);
 		case 2: return @selector(performWith::);
 		case 3: return @selector(performWith:::);

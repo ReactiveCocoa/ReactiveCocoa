893087958a8353dccf935af7aa0824ed414d5792
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-06-23 09:37:32 -0700

    Replace EXTScope cleanup with namespaced static inline
    This avoids symbol conflicts if EXTScope.m is included via two
    different projects.

--- ReactiveCocoaFramework/ReactiveCocoa/extobjc/EXTScope.h
+++ ReactiveCocoaFramework/ReactiveCocoa/extobjc/EXTScope.h
@@ -30,7 +30,7 @@
  */
 #define onExit \
     try {} @finally {} \
-    __strong ext_cleanupBlock_t metamacro_concat(ext_exitBlock_, __LINE__) __attribute__((cleanup(ext_executeCleanupBlock), unused)) = ^
+    __strong ext_cleanupBlock_t metamacro_concat(ext_exitBlock_, __LINE__) __attribute__((cleanup(rac_executeCleanupBlock), unused)) = ^
 
 /**
  * Creates \c __weak shadow variables for each of the variables provided as
@@ -90,7 +90,9 @@
 /*** implementation details follow ***/
 typedef void (^ext_cleanupBlock_t)();
 
-void ext_executeCleanupBlock (__strong ext_cleanupBlock_t *block);
+static inline void rac_executeCleanupBlock (__strong ext_cleanupBlock_t *block) {
+    (*block)();
+}
 
 #define ext_weakify_(INDEX, CONTEXT, VAR) \
     CONTEXT __typeof__(VAR) metamacro_concat(VAR, _weak_) = (VAR);

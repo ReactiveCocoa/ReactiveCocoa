0b17d44f69b6c2d5f4af82d848b5feeb72f35f83
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-07-21 03:00:15 -0700

    RACObservablePropertySubject -> RACKVOBinding

---
+++ ReactiveCocoaFramework/ReactiveCocoa/RACKVOBinding.h
@@ -0,0 +1,85 @@
+//
+//  RACKVOBinding.h
+//  ReactiveCocoa
+//
+//  Created by Uri Baghin on 27/12/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "RACBinding.h"
+#import "EXTKeyPathCoding.h"
+#import "metamacros.h"
+
+// Creates a RACKVOBinding to the given key path. When the targeted object
+// deallocates, the binding will complete.
+//
+// If RACBind() is used on the left-hand side of an assignment, there must a
+// RACBinding on the right-hand side of the assignment. The left binding's facts
+// endpoint will be bound to the right binding's rumors endpoint, so values
+// originating from the left side are considered to be rumors, and values
+// originating from the right side are considered to be facts.
+//
+// There are two different versions of this macro:
+//
+//  - RACBind(TARGET, KEYPATH, NILVALUE) will create a binding to the `KEYPATH`
+//    of `TARGET`. If the `rumorsSubscriber` is ever sent a `nil` value, the
+//    property will be set to `NILVALUE` instead. `NILVALUE` may itself be `nil`
+//    for object properties, but an NSValue should be used for primitive
+//    properties, to avoid an exception if `nil` is sent (which might occur if
+//    an intermediate object is set to `nil`).
+//  - RACBind(TARGET, KEYPATH) is the same as the above, but `NILVALUE` defaults to
+//    `nil`.
+//
+// Examples
+//
+//  RACKVOBinding *objectBinding = RACBind(self, objectProperty);
+//  RACKVOBinding *integerBinding = RACBind(self, integerProperty, @42);
+//
+//  RACBind(view, objectProperty) = RACBind(model, objectProperty);
+//  RACBind(view, integerProperty, @2) = RACBind(model, integerProperty, @10);
+#define RACBind(TARGET, ...) \
+    metamacro_if_eq(1, metamacro_argcount(__VA_ARGS__)) \
+        (RACBind_(TARGET, __VA_ARGS__, nil)) \
+        (RACBind_(TARGET, __VA_ARGS__))
+
+// Do not use this directly. Use the RACBind macro above.
+#define RACBind_(TARGET, KEYPATH, NILVALUE) \
+    [RACKVOBinding bindingWithTarget:(TARGET) keyPath:@keypath(TARGET, KEYPATH) nilValue:(NILVALUE)][@""]
+
+// A RACBinding that observes a KVO-compliant key path for changes.
+@interface RACKVOBinding : RACBinding
+
+// Initializes a binding that will observe the given object and key path.
+//
+// Whenever a KVO notification is generated for the key path, the new value is
+// assumed to be a fact, and will be sent upon the `factsSignal`. When the
+// object deallocates, the binding will complete.
+//
+// Whenever a rumor is received, it will be set at the given key path using
+// key-value coding. A KVO binding receiving an error on `rumorsSignal` is
+// considered undefined behavior.
+//
+// This is the designated initializer for this class.
+//
+// target   - The object to bind to.
+// keyPath  - The key path to observe for new facts, and set when new rumors are
+//            received.
+// nilValue - The value to set at the key path whenever a `nil` rumor is
+//            received. This may be nil when binding to object properties, but
+//            an NSValue should be used for primitive properties, to avoid an
+//            exception if `nil` is received (which might occur if an intermediate
+//            object is set to `nil`).
+- (id)initWithTarget:(NSObject *)target keyPath:(NSString *)keyPath nilValue:(id)nilValue;
+
++ (instancetype)new __attribute__((unavailable("Use -initWithTarget:keyPath:nilValue: instead")));
+- (id)init __attribute__((unavailable("Use -initWithTarget:keyPath:nilValue: instead")));
+
+@end
+
+// Methods needed for the convenience macro. Do not call explicitly.
+@interface RACKVOBinding (RACBind)
+
+- (instancetype)objectForKeyedSubscript:(NSString *)unused;
+- (void)setObject:(RACBinding *)binding forKeyedSubscript:(NSString *)unused;
+
+@end
---
+++ ReactiveCocoaFramework/ReactiveCocoa/RACKVOBinding.m
@@ -0,0 +1,230 @@
+//
+//  RACKVOBinding.m
+//  ReactiveCocoa
+//
+//  Created by Uri Baghin on 27/12/2012.
+//  Copyright (c) 2012 GitHub, Inc. All rights reserved.
+//
+
+#import "RACKVOBinding.h"
+#import "EXTScope.h"
+#import "NSObject+RACDeallocating.h"
+#import "NSObject+RACDescription.h"
+#import "NSObject+RACKVOWrapper.h"
+#import "NSObject+RACPropertySubscribing.h"
+#import "NSString+RACKeyPathUtilities.h"
+#import "RACBinding.h"
+#import "RACCompoundDisposable.h"
+#import "RACDisposable.h"
+#import "RACReplaySubject.h"
+#import "RACSignal+Operations.h"
+#import "RACSubscriber+Private.h"
+#import "RACSubject.h"
+
+// Key for the array of RACKVOBinding's additional thread local
+// data in the thread dictionary.
+static NSString * const RACKVOBindingDataDictionaryKey = @"RACKVOBindingKey";
+
+// Wrapper class for additional thread local data.
+@interface RACKVOBindingData : NSObject
+
+// The flag used to ignore updates the binding itself has triggered.
+@property (nonatomic, assign) BOOL ignoreNextUpdate;
+
+// The current -willChangeValueForKey:/-didChangeValueForKey: call stack depth.
+@property (nonatomic, assign) NSUInteger stackDepth;
+
+// A pointer to the owner of the data. Only use this for pointer comparison,
+// never as an object reference.
+@property (nonatomic, assign) void *owner;
+
++ (instancetype)dataForBinding:(RACKVOBinding *)binding;
+
+@end
+
+@interface RACKVOBinding ()
+
+// The object whose key path the binding is wrapping.
+@property (atomic, unsafe_unretained) NSObject *target;
+
+// The key path the binding is wrapping.
+@property (nonatomic, copy, readonly) NSString *keyPath;
+
+// Returns the existing thread local data container or nil if none exists.
+@property (nonatomic, strong, readonly) RACKVOBindingData *currentThreadData;
+
+// Creates the thread local data container for the binding.
+- (void)createCurrentThreadData;
+
+// Destroy the thread local data container for the binding.
+- (void)destroyCurrentThreadData;
+
+@end
+
+@implementation RACKVOBinding
+
+#pragma mark Properties
+
+- (RACKVOBindingData *)currentThreadData {
+	NSMutableArray *dataArray = NSThread.currentThread.threadDictionary[RACKVOBindingDataDictionaryKey];
+
+	for (RACKVOBindingData *data in dataArray) {
+		if (data.owner == (__bridge void *)self) return data;
+	}
+
+	return nil;
+}
+
+#pragma mark Lifecycle
+
+- (id)initWithTarget:(NSObject *)target keyPath:(NSString *)keyPath nilValue:(id)nilValue {
+	NSCParameterAssert(keyPath.rac_keyPathComponents.count > 0);
+
+	self = [super init];
+	if (self == nil) return nil;
+
+	_target = target;
+	_keyPath = [keyPath copy];
+
+	[self.rumorsEndpoint.factsSignal setNameWithFormat:@"[-initWithTarget: %@ keyPath: %@ nilValue: %@] factsSignal", target, keyPath, nilValue];
+	[self.factsEndpoint.rumorsSignal setNameWithFormat:@"[-initWithTarget: %@ keyPath: %@ nilValue: %@] rumorsSignal", target, keyPath, nilValue];
+	
+	@weakify(self);
+
+	// Observe the key path on target for changes. Update the value of stackDepth
+	// accordingly and forward the changes to updatesSubject.
+	RACDisposable *observationDisposable = [target rac_observeKeyPath:keyPath options:NSKeyValueObservingOptionPrior | NSKeyValueObservingOptionInitial observer:self block:^(id value, NSDictionary *change) {
+		@strongify(self);
+		RACKVOBindingData *data = self.currentThreadData;
+		
+		// If the change is prior we only increase the stack depth if it was
+		// triggered by the last path component, we don't do anything otherwise.
+		if ([change[NSKeyValueChangeNotificationIsPriorKey] boolValue]) {
+			if ([change[RACKeyValueChangeAffectedOnlyLastComponentKey] boolValue]) {
+				// Don't worry about the data being nil, if it is it means the binding
+				// hasn't received a value since the latest ignored one anyway.
+				++data.stackDepth;
+			}
+
+			return;
+		}
+		
+		// From here the change isn't prior.
+
+		// The binding only triggers changes to the last path component, if the
+		// change wasn't triggered by the last path component, or was triggered by
+		// a deallocation, it definitely wasn't triggered by this binding, so just
+		// forward it.
+		if (![change[RACKeyValueChangeAffectedOnlyLastComponentKey] boolValue] || [change[RACKeyValueChangeCausedByDeallocationKey] boolValue]) {
+			[self.factsEndpoint.factsSubscriber sendNext:value];
+			return;
+		}
+
+		--data.stackDepth;
+		NSCAssert(data.stackDepth != NSUIntegerMax, @"%@ received -didChangeValueForKey: without corresponding -willChangeValueForKey:", self);
+
+		// If the current stackDepth is greater than 0, then the change was
+		// triggered by a callback on -willChangeValueForKey:, and not by the
+		// binding itself. If however the stackDepth is 0, and ignoreNextUpdate is
+		// set, the changes was triggered by this binding and should not be
+		// forwarded.
+		if (data.stackDepth == 0 && data.ignoreNextUpdate) {
+			[self destroyCurrentThreadData];
+			return;
+		}
+
+		[self.factsEndpoint.factsSubscriber sendNext:value];
+	}];
+	
+	NSString *keyPathByDeletingLastKeyPathComponent = keyPath.rac_keyPathByDeletingLastKeyPathComponent;
+	NSArray *keyPathComponents = keyPath.rac_keyPathComponents;
+	NSUInteger keyPathComponentsCount = keyPathComponents.count;
+	NSString *lastKeyPathComponent = keyPathComponents.lastObject;
+
+	// Update the value of the property with the values received.
+	[[self.factsEndpoint.rumorsSignal
+		finally:^{
+			[observationDisposable dispose];
+		}]
+		subscribeNext:^(id x) {
+			@strongify(self);
+
+			// Check the value of the second to last key path component. Since the
+			// binding can only update the value of a property on an object, and not
+			// update intermediate objects, it can only update the value of the whole
+			// key path if this object is not nil.
+			NSObject *object = (keyPathComponentsCount > 1 ? [self.target valueForKeyPath:keyPathByDeletingLastKeyPathComponent] : self.target);
+			if (object == nil) return;
+
+			// Set the ignoreNextUpdate flag before setting the value so this binding
+			// ignores the value in the subsequent -didChangeValueForKey: callback.
+			[self createCurrentThreadData];
+			self.currentThreadData.ignoreNextUpdate = YES;
+
+			[object setValue:x ?: nilValue forKey:lastKeyPathComponent];
+		} error:^(NSError *error) {
+			@strongify(self);
+			NSCAssert(NO, @"Received error in %@: %@", self, error);
+			
+			// Log the error if we're running with assertions disabled.
+			NSLog(@"Received error in %@: %@", self, error);
+		}];
+	
+	[target.rac_deallocDisposable addDisposable:[RACDisposable disposableWithBlock:^{
+		@strongify(self);
+		[self.factsEndpoint.factsSubscriber sendCompleted];
+		self.target = nil;
+	}]];
+	
+	return self;
+}
+
+- (void)createCurrentThreadData {
+	NSMutableArray *dataArray = NSThread.currentThread.threadDictionary[RACKVOBindingDataDictionaryKey];
+	if (dataArray == nil) {
+		dataArray = [NSMutableArray array];
+		NSThread.currentThread.threadDictionary[RACKVOBindingDataDictionaryKey] = dataArray;
+		[dataArray addObject:[RACKVOBindingData dataForBinding:self]];
+		return;
+	}
+
+	for (RACKVOBindingData *data in dataArray) {
+		if (data.owner == (__bridge void *)self) return;
+	}
+
+	[dataArray addObject:[RACKVOBindingData dataForBinding:self]];
+}
+
+- (void)destroyCurrentThreadData {
+	NSMutableArray *dataArray = NSThread.currentThread.threadDictionary[RACKVOBindingDataDictionaryKey];
+	NSUInteger index = [dataArray indexOfObjectPassingTest:^ BOOL (RACKVOBindingData *data, NSUInteger idx, BOOL *stop) {
+		return data.owner == (__bridge void *)self;
+	}];
+
+	if (index != NSNotFound) [dataArray removeObjectAtIndex:index];
+}
+
+@end
+
+@implementation RACKVOBinding (RACBind)
+
+- (instancetype)objectForKeyedSubscript:(NSString *)unused {
+	return self;
+}
+
+- (void)setObject:(RACBinding *)binding forKeyedSubscript:(NSString *)unused {
+	[binding.rumorsEndpoint.factsSignal subscribe:self.factsEndpoint.factsSubscriber];
+	[[self.factsEndpoint.rumorsSignal skip:1] subscribe:binding.rumorsEndpoint.rumorsSubscriber];
+}
+
+@end
+
+@implementation RACKVOBindingData
+
++ (instancetype)dataForBinding:(RACKVOBinding *)binding {
+	RACKVOBindingData *data = [[self alloc] init];
+	data->_owner = (__bridge void *)binding;
+	return data;
+}
+
+@end

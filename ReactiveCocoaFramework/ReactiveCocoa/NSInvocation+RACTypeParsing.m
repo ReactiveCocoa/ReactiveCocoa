008d0b3c0e5e39267d07ec4a569abe870af40adf
Uri Baghin <skerfat@gmail.com>
2013-07-22 16:18:18 +0200

    Add vertical whitespace.

--- ReactiveCocoaFramework/ReactiveCocoa/NSInvocation+RACTypeParsing.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSInvocation+RACTypeParsing.m
@@ -57,19 +57,25 @@
 		[self setArgument:&cString atIndex:(NSInteger)index];
 		[self retainArguments];
 	} else {
+		NSCParameterAssert([[object class] isSubclassOfClass:NSValue.class]);
+
 		void *valueBytes = NULL;
 		NSUInteger valueSize = 0;
 		NSGetSizeAndAlignment([object objCType], &valueSize, NULL);
+
 #if DEBUG
 		NSUInteger argSize = 0;
 		NSGetSizeAndAlignment(argType, &argSize, NULL);
 		NSCAssert(valueSize == argSize, @"Value size does not match argument size in -rac_setArgument: %@ atIndex: %lu", object, (unsigned long)index);
 #endif
+		
 		valueBytes = malloc(valueSize);
 		[object getValue:valueBytes];
+
 		[self setArgument:valueBytes atIndex:(NSInteger)index];
 		// Retain the arguments in case this was a c array of some type
 		[self retainArguments];
+
 		free(valueBytes);
 	}
 
@@ -121,13 +127,17 @@
 	} else if (strcmp(argType, "*") == 0) {
 		WRAP_AND_RETURN(const char *);
 	} else {
-		void *valueBytes = NULL;
 		NSUInteger valueSize = 0;
 		NSGetSizeAndAlignment(argType, &valueSize, NULL);
+
+		void *valueBytes = NULL;
 		valueBytes = malloc(valueSize);
 		[self getArgument:valueBytes atIndex:(NSInteger)index];
+		
 		NSValue *value = [NSValue valueWithBytes:valueBytes objCType:argType];
+
 		free(valueBytes);
+
 		return value;
 	}
 
@@ -154,52 +164,56 @@
 		return @(val); \
 	} while (0)
 
-	const char *argType = self.methodSignature.methodReturnType;
+	const char *returnType = self.methodSignature.methodReturnType;
 	// Skip const type qualifier.
-	if (argType[0] == 'r') {
-		argType++;
+	if (returnType[0] == 'r') {
+		returnType++;
 	}
 
-	if (strcmp(argType, "@") == 0 || strcmp(argType, "#") == 0) {
+	if (strcmp(returnType, "@") == 0 || strcmp(returnType, "#") == 0) {
 		__autoreleasing id returnObj;
 		[self getReturnValue:&returnObj];
 		return returnObj;
-	} else if (strcmp(argType, "c") == 0) {
+	} else if (strcmp(returnType, "c") == 0) {
 		WRAP_AND_RETURN(char);
-	} else if (strcmp(argType, "i") == 0) {
+	} else if (strcmp(returnType, "i") == 0) {
 		WRAP_AND_RETURN(int);
-	} else if (strcmp(argType, "s") == 0) {
+	} else if (strcmp(returnType, "s") == 0) {
 		WRAP_AND_RETURN(short);
-	} else if (strcmp(argType, "l") == 0) {
+	} else if (strcmp(returnType, "l") == 0) {
 		WRAP_AND_RETURN(long);
-	} else if (strcmp(argType, "q") == 0) {
+	} else if (strcmp(returnType, "q") == 0) {
 		WRAP_AND_RETURN(long long);
-	} else if (strcmp(argType, "C") == 0) {
+	} else if (strcmp(returnType, "C") == 0) {
 		WRAP_AND_RETURN(unsigned char);
-	} else if (strcmp(argType, "I") == 0) {
+	} else if (strcmp(returnType, "I") == 0) {
 		WRAP_AND_RETURN(unsigned int);
-	} else if (strcmp(argType, "S") == 0) {
+	} else if (strcmp(returnType, "S") == 0) {
 		WRAP_AND_RETURN(unsigned short);
-	} else if (strcmp(argType, "L") == 0) {
+	} else if (strcmp(returnType, "L") == 0) {
 		WRAP_AND_RETURN(unsigned long);
-	} else if (strcmp(argType, "Q") == 0) {
+	} else if (strcmp(returnType, "Q") == 0) {
 		WRAP_AND_RETURN(unsigned long long);
-	} else if (strcmp(argType, "f") == 0) {
+	} else if (strcmp(returnType, "f") == 0) {
 		WRAP_AND_RETURN(float);
-	} else if (strcmp(argType, "d") == 0) {
+	} else if (strcmp(returnType, "d") == 0) {
 		WRAP_AND_RETURN(double);
-	} else if (strcmp(argType, "*") == 0) {
+	} else if (strcmp(returnType, "*") == 0) {
 		WRAP_AND_RETURN(const char *);
-	} else if (strcmp(argType, "v") == 0) {
+	} else if (strcmp(returnType, "v") == 0) {
 		return RACUnit.defaultUnit;
 	} else {
-		void *valueBytes = NULL;
 		NSUInteger valueSize = 0;
-		NSGetSizeAndAlignment(argType, &valueSize, NULL);
+		NSGetSizeAndAlignment(returnType, &valueSize, NULL);
+
+		void *valueBytes = NULL;
 		valueBytes = malloc(valueSize);
 		[self getReturnValue:valueBytes];
-		NSValue *value = [NSValue valueWithBytes:valueBytes objCType:argType];
+
+		NSValue *value = [NSValue valueWithBytes:valueBytes objCType:returnType];
+
 		free(valueBytes);
+		
 		return value;
 	}
 

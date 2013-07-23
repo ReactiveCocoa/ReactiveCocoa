8e8790717b5f45344096e59db46014510a18aaa5
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-07-23 00:07:39 -0700

    Argh missing imports

--- ReactiveCocoaFramework/ReactiveCocoa/UITableViewCell+RACSignalSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/UITableViewCell+RACSignalSupport.m
@@ -9,7 +9,9 @@
 #import "UITableViewCell+RACSignalSupport.h"
 #import "NSObject+RACDescription.h"
 #import "NSObject+RACSelectorSignal.h"
+#import "RACSignal+Operations.h"
 #import "RACUnit.h"
+#import <objc/runtime.h>
 
 @implementation UITableViewCell (RACSignalSupport)
 

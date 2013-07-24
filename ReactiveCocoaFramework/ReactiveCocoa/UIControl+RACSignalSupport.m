30ca726b6859b8de71aec735531f2ec1151cbd7b
Uri Baghin <skerfat@gmail.com>
2013-07-24 09:53:13 +0200

    Merge control events signal with KVO signal.

--- ReactiveCocoaFramework/ReactiveCocoa/UIControl+RACSignalSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/UIControl+RACSignalSupport.m
@@ -11,6 +11,7 @@
 #import "RACBinding+Private.h"
 #import "RACCompoundDisposable.h"
 #import "RACDisposable.h"
+#import "RACObservablePropertySubject.h"
 #import "RACSignal.h"
 #import "RACSignal+Operations.h"
 #import "RACSubscriber+Private.h"
@@ -41,56 +42,18 @@
 }
 
 - (RACBinding *)rac_bindingForControlEvents:(UIControlEvents)controlEvents key:(NSString *)key nilValue:(id)nilValue {
-	@weakify(self);
-
 	RACBinding *binding = [[RACBinding alloc] init];
 	if (binding == nil) return nil;
 
-	binding.signal = [RACSignal
-		createSignal:^(id<RACSubscriber> subscriber) {
-			@strongify(self);
-
-			[subscriber sendNext:[self valueForKey:key]];
-			return [[[self
-				rac_signalForControlEvents:controlEvents]
-				map:^(id sender) {
-					return [sender valueForKey:key];
-				}]
-				subscribe:subscriber];
+	RACBinding *KVOBinding = [RACObservablePropertySubject propertyWithTarget:self keyPath:key nilValue:nilValue].binding;
+	RACSignal *controlEventsSignal = [[self
+		rac_signalForControlEvents:controlEvents]
+		map:^(id sender) {
+			return [sender valueForKey:key];
 		}];
 
-	SEL selector = NSSelectorFromString([NSString stringWithFormat:@"set%@%@:animated:", [key substringToIndex:1].uppercaseString, [key substringFromIndex:1]]);
-	NSInvocation *invocation = nil;
-	if ([self respondsToSelector:selector]) {
-		invocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:selector]];
-		invocation.selector = selector;
-		invocation.target = self;
-	}
-
-	binding.subscriber = [RACSubscriber subscriberWithNext:^(id x) {
-		@strongify(self);
-		if (self == nil) return;
-
-		if (invocation == nil) {
-			[self setValue:x ?: nilValue forKey:key];
-			return;
-		}
-
-		id value = x ?: nilValue;
-		[invocation rac_setArgument:value atIndex:2];
-
-		BOOL animated = YES;
-		[invocation setArgument:&animated atIndex:3];
-
-		[invocation invoke];
-	} error:^(NSError *error) {
-		@strongify(self);
-
-		NSCAssert(NO, @"Received error from %@ in binding for control events: %lx key \"%@\": %@", self, (unsigned long)controlEvents, key, error);
-		// Log the error if we're running with assertions disabled.
-		NSLog( @"Received error from %@ in binding for control events: %lx key \"%@\": %@", self, (unsigned long)controlEvents, key, error);
-
-	} completed:nil];
+	binding.signal = [RACSignal merge:@[ controlEventsSignal, KVOBinding ]];
+	binding.subscriber = KVOBinding;
 
 	return binding;
 }

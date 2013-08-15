62e203995251e559bdf154f7c08082f327a6de77
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-15 00:42:22 -0700

    Declare 1.x RACCommand methods as unavailable
    It'd be nice to make them deprecated instead, and offer a migration
    path, but I don't think that's feasible given the scope of the API
    changes.

--- ReactiveCocoaFramework/ReactiveCocoa/RACCommand.h
+++ ReactiveCocoaFramework/ReactiveCocoa/RACCommand.h
@@ -103,3 +103,14 @@ extern const NSInteger RACCommandErrorNotEnabled;
 - (RACSignal *)execute:(id)input;
 
 @end
+
+@interface RACCommand (Unavailable)
+
+@property (atomic, readonly) BOOL canExecute __attribute__((unavailable("Use the 'enabled' signal instead")));
+
++ (instancetype)command __attribute__((unavailable("Use -initWithSignalBlock: instead")));
++ (instancetype)commandWithCanExecuteSignal:(RACSignal *)canExecuteSignal __attribute__((unavailable("Use -initWithEnabled:signalBlock: instead")));
+- (id)initWithCanExecuteSignal:(RACSignal *)canExecuteSignal __attribute__((unavailable("Use -initWithEnabled:signalBlock: instead")));
+- (RACSignal *)addSignalBlock:(RACSignal * (^)(id value))signalBlock __attribute__((unavailable("Pass the signalBlock to -initWithSignalBlock: instead")));
+
+@end

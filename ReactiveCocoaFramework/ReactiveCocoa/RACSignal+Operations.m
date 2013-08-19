4e4aaf900b09d26697bac74212bd82e2a14d754f
Dave Lee <davelee.com@gmail.com>
2013-08-18 23:42:36 -0600

    Reimplement -switchToLatest using existing operators

--- ReactiveCocoaFramework/ReactiveCocoa/RACSignal+Operations.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACSignal+Operations.m
@@ -716,47 +716,12 @@ static RACDisposable *subscribeForever (RACSignal *signal, void (^next)(id), voi
 }
 
 - (RACSignal *)switchToLatest {
-	return [[RACSignal createSignal:^(id<RACSubscriber> subscriber) {
-		RACSerialDisposable *innerDisposable = [[RACSerialDisposable alloc] init];
+	return [[self flattenMap:^(id x) {
+		if (x == nil) return RACSignal.empty;
 
-		__block volatile uint32_t latestChildSignalHasCompleted = 0;
-		__block volatile int32_t partialCompletionCount = 0;
-		
-		RACDisposable *selfDisposable = [self subscribeNext:^(id x) {
-			NSCAssert([x isKindOfClass:RACSignal.class] || x == nil, @"-switchToLatest requires that the source signal (%@) send signals. Instead we got: %@", self, x);
-			
-			[innerDisposable.disposable dispose];
-			
-			int32_t previousChildSignalHadCompleted = OSAtomicAnd32OrigBarrier(0, &latestChildSignalHasCompleted);
-			if (previousChildSignalHadCompleted == 1) {
-				OSAtomicDecrement32Barrier(&partialCompletionCount);
-			}
-			
-			innerDisposable.disposable = [x subscribeNext:^(id x) {
-				[subscriber sendNext:x];
-			} error:^(NSError *error) {
-				[subscriber sendError:error];
-			} completed:^{
-				OSAtomicOr32Barrier(1, &latestChildSignalHasCompleted);
-				
-				int32_t currentPartialCompletionCount = OSAtomicIncrement32Barrier(&partialCompletionCount);
-				if (currentPartialCompletionCount == 2) {
-					[subscriber sendCompleted];
-				}
-			}];
-		} error:^(NSError *error) {
-			[subscriber sendError:error];
-		} completed:^{
-			int32_t currentPartialCompletionCount = OSAtomicAdd32Barrier(1, &partialCompletionCount);
-			if (currentPartialCompletionCount == 2) {
-				[subscriber sendCompleted];
-			}
-		}];
-		
-		return [RACDisposable disposableWithBlock:^{
-			[innerDisposable dispose];
-			[selfDisposable dispose];
-		}];
+		NSCAssert([x isKindOfClass:RACSignal.class], @"-switchToLatest requires that the source signal (%@) send signals. Instead we got: %@", self, x);
+
+		return [x takeUntil:[self concat:RACSignal.never]];
 	}] setNameWithFormat:@"[%@] -switchToLatest", self.name];
 }
 

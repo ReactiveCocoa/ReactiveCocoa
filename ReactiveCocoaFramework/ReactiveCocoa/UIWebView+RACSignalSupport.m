3874d24123260a03fdaeaf8d9826648ead7eab7b
Jackson Harper <jacksonh@gmail.com>
2013-09-23 22:31:58 -0400

    Fix some style issues, extra includes, setting the signal name.

--- ReactiveCocoaFramework/ReactiveCocoa/UIWebView+RACSignalSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/UIWebView+RACSignalSupport.m
@@ -7,16 +7,13 @@
 //
 
 #import "UIWebView+RACSignalSupport.h"
-#import "EXTScope.h"
 #import "NSObject+RACDeallocating.h"
 #import "RACDelegateProxy.h"
 #import "RACSignal+Operations.h"
-#import "RACTuple.h"
 #import "NSObject+RACDescription.h"
 #import <objc/runtime.h>
 
 @implementation UIWebView (RACSignalSupport)
-
 
 static void RACUseDelegateProxy(UIWebView *self) {
     if (self.delegate == self.rac_delegateProxy) return;
@@ -36,18 +33,18 @@ static void RACUseDelegateProxy(UIWebView *self) {
 }
 
 - (RACSignal *)rac_loadedSignal {
-
 	RACSignal *loaded = [[self.rac_delegateProxy
 		signalForSelector:@selector(webViewDidFinishLoad:)]
 		reduceEach:^(UIWebView *webview){
-			return [webview request];
+			return webview.request;
 		}];
 
 	RACSignal *failed = [[[[self.rac_delegateProxy
 		signalForSelector:@selector(webView:didFailLoadWithError:)]
 		reduceEach:^(UIWebView *webview, NSError *error) {
 			return error;
-		}] filter:^BOOL(NSError *error) {
+		}]
+		filter:^ BOOL (NSError *error) {
 			// This error comes up almost every time you load html
 			NSURL *failingURL = [error.userInfo objectForKey:@"NSErrorFailingURLKey"];
 			return ![failingURL.absoluteString isEqualToString:@"about:blank"];
@@ -58,9 +55,10 @@ static void RACUseDelegateProxy(UIWebView *self) {
 
 	RACUseDelegateProxy(self);
 
-	return [[[RACSignal merge:@[ loaded , failed ]]
+	return [[[RACSignal
+		merge:@[ loaded , failed ]]
 		takeUntil:self.rac_willDeallocSignal]
-		setNameWithFormat:@"%@ -rac_loadedSignal", self];
+		setNameWithFormat:@"%@ -rac_loadedSignal", [self rac_description]];
 }
 
 @end

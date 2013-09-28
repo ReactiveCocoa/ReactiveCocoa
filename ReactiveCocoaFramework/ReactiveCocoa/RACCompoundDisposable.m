b5e9033bd46d0def498e43f5676e93953b547a81
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-09-27 23:27:46 -0700

    Inline 2 disposables into RACCompoundDisposables
    Empirically determined. There are few compound disposables with 3 or
    more inner items.

--- ReactiveCocoaFramework/ReactiveCocoa/RACCompoundDisposable.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACCompoundDisposable.m
@@ -10,9 +10,37 @@
 #import "RACCompoundDisposableProvider.h"
 #import <libkern/OSAtomic.h>
 
+// The number of child disposables for which space will be reserved directly in
+// `RACCompoundDisposable`.
+//
+// This number has been empirically determined to provide a good tradeoff
+// between performance, memory usage, and `RACCompoundDisposable` instance size
+// in a moderately complex GUI application.
+//
+// Profile any change!
+#define RACCompoundDisposableInlineCount 2
+
+static CFMutableArrayRef RACCreateDisposablesArray(void) {
+	// Compare values using only pointer equality.
+	CFArrayCallBacks callbacks = kCFTypeArrayCallBacks;
+	callbacks.equal = NULL;
+
+	return CFArrayCreateMutable(NULL, 0, &callbacks);
+}
+
 @interface RACCompoundDisposable () {
 	// Used for synchronization.
 	OSSpinLock _spinLock;
+
+	#if RACCompoundDisposableInlineCount
+	// A fast array to the first N of the receiver's disposables.
+	//
+	// Once this is full, `_disposables` will be created and used for additional
+	// disposables.
+	//
+	// This array should only be manipulated while _spinLock is held.
+	RACDisposable *_inlineDisposables[RACCompoundDisposableInlineCount];
+	#endif
 
 	// Contains the receiver's disposables.
 	//
@@ -40,7 +68,7 @@
 	return disposed;
 }
 
-#pragma mark Initializers
+#pragma mark Lifecycle
 
 + (instancetype)compoundDisposable {
 	return [[self alloc] initWithDisposables:nil];
@@ -50,25 +78,22 @@
 	return [[self alloc] initWithDisposables:disposables];
 }
 
-- (id)init {
-	self = [super init];
-	if (self == nil) return nil;
-
-	// Use a CFArray for speed, and compare values using only pointer equality.
-	CFArrayCallBacks callbacks = kCFTypeArrayCallBacks;
-	callbacks.equal = NULL;
-
-	_disposables = CFArrayCreateMutable(NULL, 0, &callbacks);
-
-	return self;
-}
-
 - (id)initWithDisposables:(NSArray *)otherDisposables {
 	self = [self init];
 	if (self == nil) return nil;
 
-	if (otherDisposables != nil) {
-		CFArrayAppendArray(_disposables, (__bridge CFArrayRef)otherDisposables, CFRangeMake(0, (CFIndex)otherDisposables.count));
+	#if RACCompoundDisposableInlineCount
+	[otherDisposables enumerateObjectsUsingBlock:^(RACDisposable *disposable, NSUInteger index, BOOL *stop) {
+		if (index == RACCompoundDisposableInlineCount) *stop = YES;
+		_inlineDisposables[index] = disposable;
+	}];
+	#endif
+
+	if (otherDisposables.count > RACCompoundDisposableInlineCount) {
+		_disposables = RACCreateDisposablesArray();
+
+		CFRange range = CFRangeMake(RACCompoundDisposableInlineCount, (CFIndex)otherDisposables.count - RACCompoundDisposableInlineCount);
+		CFArrayAppendArray(_disposables, (__bridge CFArrayRef)otherDisposables, range);
 	}
 
 	return self;
@@ -80,13 +105,19 @@
 }
 
 - (void)dealloc {
+	#if RACCompoundDisposableInlineCount
+	for (unsigned i = 0; i < RACCompoundDisposableInlineCount; i++) {
+		_inlineDisposables[i] = nil;
+	}
+	#endif
+
 	if (_disposables != NULL) {
 		CFRelease(_disposables);
 		_disposables = NULL;
 	}
 }
 
-#pragma mark Compound
+#pragma mark Addition and Removal
 
 - (void)addDisposable:(RACDisposable *)disposable {
 	NSCParameterAssert(disposable != nil);
@@ -99,11 +130,25 @@
 		if (_disposed) {
 			shouldDispose = YES;
 		} else {
+			#if RACCompoundDisposableInlineCount
+			for (unsigned i = 0; i < RACCompoundDisposableInlineCount; i++) {
+				if (_inlineDisposables[i] == nil) {
+					_inlineDisposables[i] = disposable;
+					goto foundSlot;
+				}
+			}
+			#endif
+
+			if (_disposables == NULL) _disposables = RACCreateDisposablesArray();
 			CFArrayAppendValue(_disposables, (__bridge void *)disposable);
 
 			if (RACCOMPOUNDDISPOSABLE_ADDED_ENABLED()) {
-				RACCOMPOUNDDISPOSABLE_ADDED(self.description.UTF8String, disposable.description.UTF8String, CFArrayGetCount(_disposables));
+				RACCOMPOUNDDISPOSABLE_ADDED(self.description.UTF8String, disposable.description.UTF8String, CFArrayGetCount(_disposables) + RACCompoundDisposableInlineCount);
 			}
+
+		#if RACCompoundDisposableInlineCount
+		foundSlot:;
+		#endif
 		}
 	}
 	OSSpinLockUnlock(&_spinLock);
@@ -119,16 +164,24 @@
 	OSSpinLockLock(&_spinLock);
 	{
 		if (!_disposed) {
-			CFIndex count = CFArrayGetCount(_disposables);
-			for (CFIndex i = count - 1; i >= 0; i--) {
-				const void *item = CFArrayGetValueAtIndex(_disposables, i);
-				if (item == (__bridge void *)disposable) {
-					CFArrayRemoveValueAtIndex(_disposables, i);
-				}
+			#if RACCompoundDisposableInlineCount
+			for (unsigned i = 0; i < RACCompoundDisposableInlineCount; i++) {
+				if (_inlineDisposables[i] == disposable) _inlineDisposables[i] = nil;
 			}
+			#endif
 
-			if (RACCOMPOUNDDISPOSABLE_REMOVED_ENABLED()) {
-				RACCOMPOUNDDISPOSABLE_REMOVED(self.description.UTF8String, disposable.description.UTF8String, CFArrayGetCount(_disposables));
+			if (_disposables != NULL) {
+				CFIndex count = CFArrayGetCount(_disposables);
+				for (CFIndex i = count - 1; i >= 0; i--) {
+					const void *item = CFArrayGetValueAtIndex(_disposables, i);
+					if (item == (__bridge void *)disposable) {
+						CFArrayRemoveValueAtIndex(_disposables, i);
+					}
+				}
+
+				if (RACCOMPOUNDDISPOSABLE_REMOVED_ENABLED()) {
+					RACCOMPOUNDDISPOSABLE_REMOVED(self.description.UTF8String, disposable.description.UTF8String, CFArrayGetCount(_disposables) + RACCompoundDisposableInlineCount);
+				}
 			}
 		}
 	}
@@ -143,24 +196,41 @@ static void disposeEach(const void *value, void *context) {
 }
 
 - (void)dispose {
-	CFArrayRef allDisposables = NULL;
+	#if RACCompoundDisposableInlineCount
+	RACDisposable *inlineCopy[RACCompoundDisposableInlineCount];
+	#endif
+
+	CFArrayRef remainingDisposables = NULL;
 
 	OSSpinLockLock(&_spinLock);
 	{
 		_disposed = YES;
 
-		allDisposables = _disposables;
+		#if RACCompoundDisposableInlineCount
+		for (unsigned i = 0; i < RACCompoundDisposableInlineCount; i++) {
+			inlineCopy[i] = _inlineDisposables[i];
+			_inlineDisposables[i] = nil;
+		}
+		#endif
+
+		remainingDisposables = _disposables;
 		_disposables = NULL;
 	}
 	OSSpinLockUnlock(&_spinLock);
 
-	if (allDisposables == NULL) return;
-
-	// Performed outside of the lock in case the compound disposable is used
+	#if RACCompoundDisposableInlineCount
+	// Dispose outside of the lock in case the compound disposable is used
 	// recursively.
-	CFIndex count = CFArrayGetCount(allDisposables);
-	CFArrayApplyFunction(allDisposables, CFRangeMake(0, count), &disposeEach, NULL);
-	CFRelease(allDisposables);
+	for (unsigned i = 0; i < RACCompoundDisposableInlineCount; i++) {
+		[inlineCopy[i] dispose];
+	}
+	#endif
+
+	if (remainingDisposables == NULL) return;
+
+	CFIndex count = CFArrayGetCount(remainingDisposables);
+	CFArrayApplyFunction(remainingDisposables, CFRangeMake(0, count), &disposeEach, NULL);
+	CFRelease(remainingDisposables);
 }
 
 @end

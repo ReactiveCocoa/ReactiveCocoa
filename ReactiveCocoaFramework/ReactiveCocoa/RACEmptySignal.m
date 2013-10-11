6e7380f17e2020987d67f564eac99dd4996d1a60
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-11 11:17:50 -0700

    Turn RACEmptySignal into a singleton in Release builds

--- ReactiveCocoaFramework/ReactiveCocoa/RACEmptySignal.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACEmptySignal.m
@@ -12,10 +12,41 @@
 
 @implementation RACEmptySignal
 
+#pragma mark Properties
+
+// Only allow this signal's name to be customized in DEBUG, since it's
+// a singleton in release builds (see +empty).
+- (void)setName:(NSString *)name {
+#ifdef DEBUG
+	[super setName:name];
+#endif
+}
+
+- (NSString *)name {
+#ifdef DEBUG
+	return super.name;
+#else
+	return @"+empty";
+#endif
+}
+
 #pragma mark Lifecycle
 
 + (RACSignal *)empty {
+#ifdef DEBUG
+	// Create multiple instances of this class in DEBUG so users can set custom
+	// names on each.
 	return [[[self alloc] init] setNameWithFormat:@"+empty"];
+#else
+	static id singleton;
+	static dispatch_once_t pred;
+
+	dispatch_once(&pred, ^{
+		singleton = [[self alloc] init];
+	});
+
+	return singleton;
+#endif
 }
 
 #pragma mark Subscription

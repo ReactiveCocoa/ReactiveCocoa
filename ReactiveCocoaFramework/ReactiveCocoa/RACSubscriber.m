57a260003b6c075032ae8f2e70189bb300a739bf
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-14 16:50:26 -0700

    Implement RACSubscriber readiness with a GCD queue
    This works, but doesn't indicate readiness quickly enough. By the time
    the subscriber is marked as ready, the work will happen off the
    subscriptionScheduler, which is Bad™.

--- ReactiveCocoaFramework/ReactiveCocoa/RACSubscriber.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACSubscriber.m
@@ -10,8 +10,18 @@
 #import "RACSubscriber+Private.h"
 #import "EXTScope.h"
 #import "RACCompoundDisposable.h"
+#import <libkern/OSAtomic.h>
 
 @interface RACSubscriber ()
+
+// A private serial queue used to implement backpressure against any signals
+// that the receiver is subscribed to.
+//
+// While the receiver is processing events, the queue will be suspended. To wait
+// for the receiver to be ready, enqueue a block here.
+//
+// This queue is retained.
+@property (nonatomic, readonly) dispatch_queue_t readyQueue;
 
 // These callbacks should only be accessed while synchronized on self.
 @property (nonatomic, copy) void (^next)(id value);
@@ -40,6 +50,9 @@
 	self = [super init];
 	if (self == nil) return nil;
 
+	_readyQueue = dispatch_queue_create("com.github.ReactiveCocoa.RACSubscriber.readyQueue", DISPATCH_QUEUE_SERIAL);
+	dispatch_set_target_queue(_readyQueue, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0));
+
 	@weakify(self);
 
 	RACDisposable *selfDisposable = [RACDisposable disposableWithBlock:^{
@@ -61,11 +74,21 @@
 
 - (void)dealloc {
 	[self.disposable dispose];
+
+	if (_readyQueue != NULL) {
+		dispatch_release(_readyQueue);
+		_readyQueue = NULL;
+	}
 }
 
 #pragma mark RACSubscriber
 
 - (void)sendNext:(id)value {
+	dispatch_suspend(self.readyQueue);
+	@onExit {
+		dispatch_resume(self.readyQueue);
+	};
+
 	@synchronized (self) {
 		void (^nextBlock)(id) = [self.next copy];
 		if (nextBlock == nil) return;
@@ -75,6 +98,11 @@
 }
 
 - (void)sendError:(NSError *)e {
+	dispatch_suspend(self.readyQueue);
+	@onExit {
+		dispatch_resume(self.readyQueue);
+	};
+
 	@synchronized (self) {
 		void (^errorBlock)(NSError *) = [self.error copy];
 		[self.disposable dispose];
@@ -85,6 +113,11 @@
 }
 
 - (void)sendCompleted {
+	dispatch_suspend(self.readyQueue);
+	@onExit {
+		dispatch_resume(self.readyQueue);
+	};
+
 	@synchronized (self) {
 		void (^completedBlock)(void) = [self.completed copy];
 		[self.disposable dispose];
@@ -96,6 +129,20 @@
 
 - (void)didSubscribeWithDisposable:(RACDisposable *)d {
 	if (d != nil) [self.disposable addDisposable:d];
+}
+
+- (RACDisposable *)invokeWhenReady:(void (^)(id<RACSubscriber>))block {
+	RACDisposable *blockDisposable = [[RACDisposable alloc] init];
+	[self.disposable addDisposable:blockDisposable];
+
+	dispatch_async(self.readyQueue, ^{
+		if (blockDisposable.disposed) return;
+		[self.disposable removeDisposable:blockDisposable];
+
+		block(self);
+	});
+
+	return blockDisposable;
 }
 
 @end

721824306c8a8f16952cdbd1644b6d73fc869fb9
Dave Lee <davelee.com@gmail.com>
2013-10-17 16:08:38 -0600

    Add -rac_activeLocationUpdatesSignal

--- ReactiveCocoaFramework/ReactiveCocoa/CLLocationManager+RACSignalSupport.h
+++ ReactiveCocoaFramework/ReactiveCocoa/CLLocationManager+RACSignalSupport.h
@@ -17,4 +17,10 @@
 /// methods in this category are used.
 @property (nonatomic, strong, readonly) RACDelegateProxy *rac_delegateProxy;
 
+/// A signal of location updates.
+///
+/// Returns a signal which will send location updates. The signal will complete
+/// itself when the receiver is deallocated.
+- (RACSignal *)rac_activeLocationUpdatesSignal;
+
 @end
--- ReactiveCocoaFramework/ReactiveCocoa/CLLocationManager+RACSignalSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/CLLocationManager+RACSignalSupport.m
@@ -7,7 +7,15 @@
 //
 
 #import "CLLocationManager+RACSignalSupport.h"
+#import "EXTScope.h"
+#import "NSObject+RACDeallocating.h"
+#import "NSObject+RACDescription.h"
 #import "RACDelegateProxy.h"
+#import "RACDisposable.h"
+#import "RACSignal.h"
+#import "RACSignal+Operations.h"
+#import "RACSubscriber.h"
+#import <libkern/OSAtomic.h>
 #import <objc/runtime.h>
 
 @implementation CLLocationManager (RACSignalSupport)
@@ -26,6 +34,67 @@ static void RACUseDelegateProxy(CLLocationManager *self) {
 	proxy = [[RACDelegateProxy alloc] initWithProtocol:@protocol(CLLocationManagerDelegate)];
 	objc_setAssociatedObject(self, _cmd, proxy, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 	return proxy;
+}
+
+- (RACSignal *)rac_activeLocationUpdatesSignal {
+	static volatile int32_t subscriberCount = 0;
+
+	@weakify(self);
+	return [[[RACSignal
+		createSignal:^(id<RACSubscriber> subscriber) {
+			@strongify(self);
+
+			// Subscribe to location updates.
+
+			// The preferred delegate method for OS X 10.9+ and iOS 6.0+.
+			SEL preferredSelector = NSSelectorFromString(@"locationManager:didUpdateLocations:");
+			struct objc_method_description preferredMethod = protocol_getMethodDescription(@protocol(CLLocationManagerDelegate), preferredSelector, NO, YES);
+
+			if (preferredMethod.name != NULL) {
+				[[[self.rac_delegateProxy
+					signalForSelector:preferredSelector]
+					reduceEach:^(CLLocationManager *manager, NSArray *locations) {
+						return [locations lastObject];
+					}]
+					subscribe:subscriber];
+			} else {
+				// Fallback for OS X [10.7, 10.9) and iOS 5.
+				[[[self.rac_delegateProxy
+					signalForSelector:@selector(locationManager:didUpdateToLocation:fromLocation:)]
+					reduceEach:^(CLLocationManager *manager, CLLocation* newLocation, CLLocation *oldLocation) {
+						return newLocation;
+					}]
+					subscribe:subscriber];
+			}
+
+			// Subscribe to location errors.
+
+			[[[[self.rac_delegateProxy
+				signalForSelector:@selector(locationManager:didFailWithError:)]
+				reduceEach:^(CLLocationManager *manager, NSError *error) {
+					return [RACSignal error:error];
+				}]
+				flatten]
+				subscribe:subscriber];
+
+			// Flip the switches.
+
+			RACUseDelegateProxy(self);
+
+			if (OSAtomicIncrement32Barrier(&subscriberCount) == 1) {
+				[self startUpdatingLocation];
+			} else {
+				// Replay most recent location to later subscribers.
+				[subscriber sendNext:self.location];
+			}
+
+			return [RACDisposable disposableWithBlock:^{
+				@strongify(self);
+				if (OSAtomicDecrement32Barrier(&subscriberCount) == 0) [self stopUpdatingLocation];
+			}];
+		}]
+		takeUntil:self.rac_willDeallocSignal]
+		setNameWithFormat:@"%@ -rac_activeLocationUpdatesSignal", [self rac_description]];
 }
 
 @end

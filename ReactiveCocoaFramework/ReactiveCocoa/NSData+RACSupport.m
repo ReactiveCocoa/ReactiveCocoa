a41c3871a2ad54c826151772941971f420e4dba9
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-18 14:48:49 -0700

    Refactor NSData extension with RACPromise

--- ReactiveCocoaFramework/ReactiveCocoa/NSData+RACSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSData+RACSupport.m
@@ -7,29 +7,28 @@
 //
 
 #import "NSData+RACSupport.h"
-#import "RACReplaySubject.h"
-#import "RACScheduler.h"
+#import "RACPromise.h"
+#import "RACSignal.h"
+#import "RACSubscriber.h"
 
 @implementation NSData (RACSupport)
 
 + (RACSignal *)rac_readContentsOfURL:(NSURL *)URL options:(NSDataReadingOptions)options scheduler:(RACScheduler *)scheduler {
 	NSCParameterAssert(scheduler != nil);
-	
-	RACReplaySubject *subject = [RACReplaySubject subject];
-	[subject setNameWithFormat:@"+rac_readContentsOfURL: %@ options: %lu scheduler: %@", URL, (unsigned long)options, scheduler];
-	
-	[scheduler schedule:^{
-		NSError *error = nil;
-		NSData *data = [[NSData alloc] initWithContentsOfURL:URL options:options error:&error];
-		if(data == nil) {
-			[subject sendError:error];
-		} else {
-			[subject sendNext:data];
-			[subject sendCompleted];
-		}
-	}];
-	
-	return subject;
+
+	return [[[RACPromise
+		promiseWithScheduler:scheduler block:^(id<RACSubscriber> subscriber) {
+			NSError *error = nil;
+			NSData *data = [[NSData alloc] initWithContentsOfURL:URL options:options error:&error];
+			if (data == nil) {
+				[subscriber sendError:error];
+			} else {
+				[subscriber sendNext:data];
+				[subscriber sendCompleted];
+			}
+		}]
+		start]
+		setNameWithFormat:@"+rac_readContentsOfURL: %@ options: %lu scheduler: %@", URL, (unsigned long)options, scheduler];
 }
 
 @end

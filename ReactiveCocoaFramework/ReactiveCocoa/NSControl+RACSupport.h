49d155e7c6b88167cbe723e7b97dde91259aa3de
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-31 08:05:56 -0700

    Refactor NSControl.rac_command as an example

--- ReactiveCocoaFramework/ReactiveCocoa/NSControl+RACSupport.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSControl+RACSupport.h
@@ -7,18 +7,19 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "RACDeprecated.h"
 
+@class RACAction;
 @class RACCommand;
 @class RACSignal;
 
 @interface NSControl (RACSupport)
 
-/// Sets the control's command. When the control is clicked, the command is
-/// executed with the sender of the event. The control's enabledness is bound
-/// to the command's `canExecute`.
-///
-/// Note: this will reset the control's target and action.
-@property (nonatomic, strong) RACCommand *rac_command;
+/// Sets the control's target and action using a `RACAction`.
+@property (nonatomic, strong) RACAction *rac_action;
+
+/// Enables/disables the receiver based on the BOOLs sent from a signal.
+@property (nonatomic, strong) RACSignal *rac_enabled;
 
 /// Observes a text-based control for changes.
 ///
@@ -28,5 +29,11 @@
 /// Returns a signal which sends the current string value of the receiver, then
 /// the new value any time it changes.
 - (RACSignal *)rac_textSignal;
+
+@end
+
+@interface NSControl (RACSupportDeprecated)
+
+@property (nonatomic, strong) RACCommand *rac_command RACDeprecated("Use -rac_action and -rac_enabled instead");
 
 @end
--- ReactiveCocoaFramework/ReactiveCocoa/NSControl+RACSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSControl+RACSupport.m
@@ -16,10 +16,71 @@
 #import "RACSubscriber.h"
 #import <objc/runtime.h>
 
-static void *NSControlRACCommandKey = &NSControlRACCommandKey;
 static void *NSControlEnabledDisposableKey = &NSControlEnabledDisposableKey;
 
 @implementation NSControl (RACSupport)
+
+- (RACAction *)rac_action {
+	return objc_getAssociatedObject(self, @selector(rac_action));
+}
+
+- (void)setRac_action:(RACAction *)action {
+	objc_setAssociatedObject(self, @selector(rac_action), action, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+	if (action != nil) {
+		self.target = action;
+		self.action = @selector(execute:);
+	}
+}
+
+- (RACSignal *)rac_enabled {
+	return objc_getAssociatedObject(self, @selector(rac_enabled));
+}
+
+- (void)setRac_enabled:(RACSignal *)enabled {
+	// Tear down any previous binding before setting up our new one, or else we
+	// might get assertion failures.
+	[objc_getAssociatedObject(self, NSControlEnabledDisposableKey) dispose];
+	objc_setAssociatedObject(self, @selector(rac_enabled), enabled, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+
+	if (enabled == nil) {
+		self.enabled = YES;
+		return;
+	}
+
+	RACDisposable *disposable = [enabled setKeyPath:@"enabled" onObject:self];
+	objc_setAssociatedObject(self, NSControlEnabledDisposableKey, disposable, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (RACSignal *)rac_textSignal {
+	@weakify(self);
+	return [[[[RACSignal
+		createSignal:^(id<RACSubscriber> subscriber) {
+			@strongify(self);
+			id observer = [NSNotificationCenter.defaultCenter addObserverForName:NSControlTextDidChangeNotification object:self queue:nil usingBlock:^(NSNotification *note) {
+				[subscriber sendNext:note.object];
+			}];
+
+			return [RACDisposable disposableWithBlock:^{
+				[NSNotificationCenter.defaultCenter removeObserver:observer];
+			}];
+		}]
+		map:^(NSControl *control) {
+			return [control.stringValue copy];
+		}]
+		startWith:[self.stringValue copy]]
+		setNameWithFormat:@"%@ -rac_textSignal", [self rac_description]];
+}
+
+@end
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#pragma clang diagnostic ignored "-Wdeprecated-implementations"
+
+static void *NSControlRACCommandKey = &NSControlRACCommandKey;
+
+@implementation NSControl (RACSupportDeprecated)
 
 - (RACCommand *)rac_command {
 	return objc_getAssociatedObject(self, NSControlRACCommandKey);
@@ -58,24 +119,6 @@ static void *NSControlEnabledDisposableKey = &NSControlEnabledDisposableKey;
 	[self.rac_command execute:sender];
 }
 
-- (RACSignal *)rac_textSignal {
-	@weakify(self);
-	return [[[[RACSignal
-		createSignal:^(id<RACSubscriber> subscriber) {
-			@strongify(self);
-			id observer = [NSNotificationCenter.defaultCenter addObserverForName:NSControlTextDidChangeNotification object:self queue:nil usingBlock:^(NSNotification *note) {
-				[subscriber sendNext:note.object];
-			}];
-
-			return [RACDisposable disposableWithBlock:^{
-				[NSNotificationCenter.defaultCenter removeObserver:observer];
-			}];
-		}]
-		map:^(NSControl *control) {
-			return [control.stringValue copy];
-		}]
-		startWith:[self.stringValue copy]]
-		setNameWithFormat:@"%@ -rac_textSignal", [self rac_description]];
-}
-
 @end
+
+#pragma clang diagnostic pop

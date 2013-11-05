bee6a4484714f194678a21c69c5abf04672053af
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-11-05 14:51:35 -0800

    Triple slashes for dem comments

--- ReactiveCocoaFramework/ReactiveCocoa/NSControl+RACSupport.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSControl+RACSupport.h
@@ -23,9 +23,9 @@
 
 /// Whether the receiver is enabled.
 ///
-// This property is mostly for the convenience of bindings (because -isEnabled
-// does not work in a key path), and may have `RAC()` applied to it, but it is
-// not KVO-compliant.
+/// This property is mostly for the convenience of bindings (because -isEnabled
+/// does not work in a key path), and may have `RAC()` applied to it, but it is
+/// not KVO-compliant.
 @property (nonatomic, assign, getter = rac_isEnabled) BOOL rac_enabled;
 
 /// Observes a text-based control for changes.

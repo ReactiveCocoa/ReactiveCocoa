80786f15bc8ea68a9f432f974005da6228f57369
Derrick J. Hathaway <derrick@instructure.com>
2013-11-18 11:57:37 -0700

    Promotes UICollectionViewCell category to UICollectionReusableView
    This allows use of rac_prepareForReuseSignal on supplementary views.

--- ReactiveCocoaFramework/ReactiveCocoa/UICollectionReusableView+RACSignalSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/UICollectionReusableView+RACSignalSupport.m
@@ -1,19 +1,19 @@
 //
-//  UICollectionViewCell+RACSignalSupport.m
+//  UICollectionReusableView+RACSignalSupport.m
 //  ReactiveCocoa
 //
 //  Created by Kent Wong on 2013-10-04.
 //  Copyright (c) 2013 GitHub, Inc. All rights reserved.
 //
 
-#import "UICollectionViewCell+RACSignalSupport.h"
+#import "UICollectionReusableView+RACSignalSupport.h"
 #import "NSObject+RACDescription.h"
 #import "NSObject+RACSelectorSignal.h"
 #import "RACSignal+Operations.h"
 #import "RACUnit.h"
 #import <objc/runtime.h>
 
-@implementation UICollectionViewCell (RACSignalSupport)
+@implementation UICollectionReusableView (RACSignalSupport)
 
 - (RACSignal *)rac_prepareForReuseSignal {
 	RACSignal *signal = objc_getAssociatedObject(self, _cmd);

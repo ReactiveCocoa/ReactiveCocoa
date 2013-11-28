986b91a21f99d88e4d4fb9e135ad9661132c5c5d
Nikolay Kasyanov <corrmage@gmail.com>
2013-11-28 12:10:55 +0400

    Code style

--- ReactiveCocoaFramework/ReactiveCocoa/RACSignal+Operations.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACSignal+Operations.m
@@ -1259,8 +1259,7 @@ static RACDisposable *subscribeForever (RACSignal *signal, void (^next)(id), voi
 	}] setNameWithFormat:@"[%@] -or", self.name];
 }
 
-- (RACSignal *)tupleNth:(NSUInteger)index
-{
+- (RACSignal *)tupleNth:(NSUInteger)index {
 	return [[self map:^id(RACTuple *tuple) {
 		NSCAssert(tuple == nil || [tuple isKindOfClass:RACTuple.class], @"Signal of RACTuple expected, %@ received", tuple.description);
 		NSCAssert(tuple == nil || index < tuple.count, @"Not enought elements in tuple (%lu), required index %lu", (unsigned long)tuple.count, (unsigned long)index);

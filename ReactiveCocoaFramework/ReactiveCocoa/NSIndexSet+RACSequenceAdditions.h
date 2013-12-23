b9f725b70ff054f0af9f5a5a34a3357a81b2912b
Sergey Gavrilyuk <sergey.gavrilyuk@gmail.com>
2013-12-23 12:02:15 -0500

    Documentation added. Public header added to common headers list.

--- ReactiveCocoaFramework/ReactiveCocoa/NSIndexSet+RACSequenceAdditions.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSIndexSet+RACSequenceAdditions.h
@@ -10,5 +10,10 @@
 #import "RACSequence.h"
 
 @interface NSIndexSet (RACSequenceAdditions)
+
+/// Creates and returns a sequence of indexes corresponding to the receiver.
+/// NSUinteger indexes are wrapped into NSNumbers when passing along.
+///
+/// Mutating the receiver will not affect the sequence after it's been created.
 @property (nonatomic, copy, readonly) RACSequence *rac_sequence;
 @end

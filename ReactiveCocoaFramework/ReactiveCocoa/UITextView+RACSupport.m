611213848e106be1339d129f911de7f5163a3821
Adlai Holler <guy.on.internet@icloud.com>
2014-01-08 14:52:29 -0800

    Use tabs, omit block return type, align exactly, use dot notation.

--- ReactiveCocoaFramework/ReactiveCocoa/UITextView+RACSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/UITextView+RACSupport.m
@@ -1,5 +1,5 @@
 //
-//  UITextView+RACSupport.m
+//  UITextView+RACSignalSupport.m
 //  ReactiveCocoa
 //
 //  Created by Cody Krieger on 5/18/12.
@@ -7,33 +7,30 @@
 //
 
 #import "UITextView+RACSupport.h"
-#import "EXTScope.h"
+#import "RACEXTScope.h"
 #import "NSObject+RACDeallocating.h"
 #import "RACSignal+Operations.h"
 #import "RACTuple.h"
 #import "NSObject+RACDescription.h"
 #import "NSNotificationCenter+RACSupport.h"
+#import "RACDelegateProxy.h"
 #import <objc/runtime.h>
 
 @implementation UITextView (RACSupport)
-
 - (RACSignal *)rac_textSignal {
 	@weakify(self);
-    RACSignal *noteSignal = [[[NSNotificationCenter defaultCenter] rac_addObserverForName:UITextViewTextDidChangeNotification object:self] map:^id(NSNotification *note) {
-        UITextView *tv = note.object;
-        return tv.text;
-    }];
+	RACSignal *noteSignal = [[NSNotificationCenter.defaultCenter rac_addObserverForName:UITextViewTextDidChangeNotification object:self] map:^(NSNotification *note) {
+		UITextView *tv = note.object;
+		return tv.text;
+	}];
 	RACSignal *signal = [[[[RACSignal
-                            defer:^{
-                                @strongify(self);
-                                return [RACSignal return:self.text];
-                            }]
-                           concat:noteSignal]
-                          takeUntil:self.rac_willDeallocSignal]
-                         setNameWithFormat:@"%@ -rac_textSignal", [self rac_description]];
-    
-    
+							defer:^{
+								@strongify(self);
+								return [RACSignal return:self.text];
+							}]
+							concat:noteSignal]
+							takeUntil:self.rac_willDeallocSignal]
+							setNameWithFormat:@"%@ -rac_textSignal", [self rac_description]];
 	return signal;
 }
-
 @end

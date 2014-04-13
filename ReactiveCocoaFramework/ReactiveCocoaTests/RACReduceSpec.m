ceef36396603d499db0962b4a3af6d0317c6a51a
Dave Lee <kastiglione@fb.com>
2014-04-12 20:17:09 -0700

    Emphasize intended use of RACReduce in tests

--- ReactiveCocoaFramework/ReactiveCocoaTests/RACReduceSpec.m
+++ ReactiveCocoaFramework/ReactiveCocoaTests/RACReduceSpec.m
@@ -13,43 +13,43 @@ SpecBegin(RACReduce)
 it(@"should invoke the block with the given arguments", ^{
 	__block NSString *stringArg;
 	__block NSNumber *numberArg;
-	id (^block)(NSString *, NSNumber *) = ^ id (NSString *string, NSNumber *number) {
+	id (^block)(RACTuple *) = RACReduce(^ id (NSString *string, NSNumber *number) {
 		stringArg = string;
 		numberArg = number;
 		return nil;
-	};
+	});
 
-	RACReduce(block)(RACTuplePack(@"hi", @1));
+	block(RACTuplePack(@"hi", @1));
 	expect(stringArg).to.equal(@"hi");
 	expect(numberArg).to.equal(@1);
 });
 
 it(@"should return the result of the block invocation", ^{
-	NSString * (^block)(NSString *) = ^(NSString *string) {
+	NSString * (^block)(RACTuple *) = RACReduce(^(NSString *string) {
 		return string.uppercaseString;
-	};
+	});
 
-	NSString *result = RACReduce(block)(RACTuplePack(@"hi"));
+	NSString *result = block(RACTuplePack(@"hi"));
 	expect(result).to.equal(@"HI");
 });
 
 it(@"should return the BOOL result of the block invocation", ^{
-	BOOL (^block)(NSString *) = ^ BOOL (NSString *string) {
+	BOOL (^block)(RACTuple *) = RACReduce(^ BOOL (NSString *string) {
 		return string.length == 0;
-	};
+	});
 
-	BOOL result = RACReduce(block)(RACTuplePack(@"hi"));
+	BOOL result = block(RACTuplePack(@"hi"));
 	expect(result).to.equal(NO);
 });
 
 it(@"should pass RACTupleNils as nil", ^{
 	__block id arg;
-	id (^block)(id) = ^ id (id obj) {
+	id (^block)(RACTuple *) = RACReduce(^ id (id obj) {
 		arg = obj;
 		return nil;
-	};
+	});
 
-	RACReduce(block)(RACTuplePack(nil));
+	block(RACTuplePack(nil));
 	expect(arg).to.beNil();
 });
 

eaf9975e09a1bf17a8a7fac9bc8dfa1b77f45a75
Timur Kuchkarov <timurbk91@gmail.com>
2014-04-16 16:56:31 +0700

    Added RAC support for UIImagePickerController.

---
+++ ReactiveCocoaFramework/ReactiveCocoa/UIImagePickerController+RACSignalSupport.m
@@ -0,0 +1,54 @@
+//
+//  UIImagePickerController+RACSignalSupport.m
+//  Real Estate
+//
+//  Created by Timur Kuchkarov on 28.03.14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import "UIImagePickerController+RACSignalSupport.h"
+#import "RACDelegateProxy.h"
+#import "RACSignal+Operations.h"
+#import "NSObject+RACDeallocating.h"
+#import "NSObject+RACDescription.h"
+#import "RACTuple.h"
+#import <objc/runtime.h>
+
+@implementation UIImagePickerController (RACSignalSupport)
+
+static void RACUseDelegateProxy(UIImagePickerController *self) {
+	if (self.delegate == self.rac_delegateProxy) return;
+    
+	self.rac_delegateProxy.rac_proxiedDelegate = self.delegate;
+	self.delegate = (id)self.rac_delegateProxy;
+}
+
+- (RACDelegateProxy *)rac_delegateProxy
+{
+	RACDelegateProxy *proxy = objc_getAssociatedObject(self, _cmd);
+	if (proxy == nil) {
+		proxy = [[RACDelegateProxy alloc] initWithProtocol:@protocol(UIImagePickerControllerDelegate)];
+		objc_setAssociatedObject(self, _cmd, proxy, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+	}
+    
+	return proxy;
+}
+
+- (RACSignal *)rac_imageSelectedSignal
+{
+    @weakify(self);
+	RACSignal *signal = [[[[self.rac_delegateProxy
+                            signalForSelector:@selector(imagePickerController:didFinishPickingMediaWithInfo:)]
+                           reduceEach:^(UIImagePickerController *pickerController, NSDictionary *userInfo) {
+                               @strongify(self);
+                               [self dismissViewControllerAnimated:YES completion:nil];
+                               return userInfo;
+                           }]
+                          takeUntil:self.rac_willDeallocSignal]
+                         setNameWithFormat:@"%@ -rac_imageSelectedSignal", [self rac_description]];
+    
+	RACUseDelegateProxy(self);
+    
+	return signal;
+}
+@end

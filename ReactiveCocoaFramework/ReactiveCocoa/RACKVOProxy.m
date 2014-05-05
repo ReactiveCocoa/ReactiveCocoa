57623dc079381c0830260a4f8ed2055dd7be0bd4
Richard Speyer <richards@talko.com>
2014-05-04 21:04:32 -0400

    Add RACMapTable to wrap NSMapTable and use NSMutableDictionary when not available (iOS 5.0 and earlier)

--- ReactiveCocoaFramework/ReactiveCocoa/RACKVOProxy.m
+++ ReactiveCocoaFramework/ReactiveCocoa/RACKVOProxy.m
@@ -8,8 +8,83 @@
 
 #import "RACKVOProxy.h"
 
+@interface WeakObjectHolder : NSObject
+@property(weak, nonatomic) id object;
+@end
+
+@implementation WeakObjectHolder
++ (WeakObjectHolder *)holderWithObject:(id)object {
+	WeakObjectHolder *holder = [[[self class] alloc] init];
+	holder.object = object;
+	return holder;
+}
+
+- (NSString *)description {
+	return [NSString stringWithFormat:@"<%@:%p object: %@>",
+			NSStringFromClass([self class]),
+			(__bridge void *) self,
+			self.object];
+}
+@end
+
+@interface RACMapTable : NSObject
+- (id)objectForKey:(id)aKey;
+
+- (void)removeObjectForKey:(id)aKey;
+- (void)setObject:(id)anObject forKey:(id)aKey;
+@end
+
+@implementation RACMapTable {
+	NSMapTable *m_mapTable;
+	NSMutableDictionary *m_dictionary;
+}
+
+- (instancetype)init {
+	self = [super init];
+	if (self == nil) return nil;
+	if ([NSMapTable class]) {
+		m_mapTable = [NSMapTable strongToStrongObjectsMapTable];
+	}
+	else {
+		m_dictionary = [NSMutableDictionary dictionary];
+	}
+	
+	return self;
+}
+
+- (id)objectForKey:(id)aKey {
+	if (m_mapTable) {
+		return [m_mapTable objectForKey:aKey];
+	}
+	else {
+		WeakObjectHolder *holder = [m_dictionary objectForKey:aKey];
+		return holder.object;
+	}
+}
+
+- (void)removeObjectForKey:(id)aKey {
+	if (m_mapTable) {
+		[m_mapTable removeObjectForKey:aKey];
+	}
+	else {
+		[m_dictionary removeObjectForKey:aKey];
+	}
+}
+
+- (void)setObject:(id)anObject forKey:(id)aKey {
+	if (m_mapTable) {
+		[m_mapTable setObject:anObject forKey:aKey];
+	}
+	else {
+		WeakObjectHolder *holder = [WeakObjectHolder holderWithObject:anObject];
+		[m_dictionary setObject:holder forKey:aKey];
+	}
+}
+
+@end
+
 @interface RACKVOProxy()
-@property(strong, nonatomic, readonly) NSMapTable *trampolines;
+@property(strong, nonatomic, readonly) RACMapTable *trampolines;
 @end
 
 @implementation RACKVOProxy
@@ -27,7 +102,7 @@
 - (instancetype)init {
     self = [super init];
     if (self == nil) return nil;
-    _trampolines = [NSMapTable strongToWeakObjectsMapTable];
+    _trampolines = [[RACMapTable alloc] init];
     return self;
 }
 

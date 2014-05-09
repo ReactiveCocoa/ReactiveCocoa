e8452f1026bd4fad1b986f8f212af3fda71a7f1a
Justin Spahr-Summers <justin.spahrsummers@gmail.com>
2014-05-09 00:20:15 -0700

    Refactor NSUserDefaults channel into observation

--- ReactiveCocoaFramework/ReactiveCocoa/NSUserDefaults+RACSupport.h
+++ ReactiveCocoaFramework/ReactiveCocoa/NSUserDefaults+RACSupport.h
@@ -6,22 +6,25 @@
 //  Copyright (c) 2013 GitHub, Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "RACDeprecated.h"
 
 @class RACChannelTerminal;
+@class RACSignal;
 
 @interface NSUserDefaults (RACSupport)
 
-/// Creates and returns a terminal for binding the user defaults key.
+/// Observes the given user defaults key for changes.
 ///
-/// **Note:** The value in the user defaults is *asynchronously* updated with
-/// values sent to the channel.
+/// key - The user defaults key to watch for changes. Must not be nil.
 ///
-/// key - The user defaults key to create the channel terminal for.
-///
-/// Returns a channel terminal that sends the value of the user defaults key
-/// upon subscription, sends an updated value whenever the default changes, and
-/// updates the default asynchronously with values it receives.
-- (RACChannelTerminal *)rac_channelTerminalForKey:(NSString *)key;
+/// Returns a signal that sends the current value of the given key upon
+/// subscription, then sends an updated value whenever the default changes.
+- (RACSignal *)rac_objectsForKey:(NSString *)key;
+
+@end
+
+@interface NSUserDefaults (RACSupportDeprecated)
+
+- (RACChannelTerminal *)rac_channelTerminalForKey:(NSString *)key RACDeprecated("Use -rac_objectsForKey: and -setObject:forKey: instead");
 
 @end
--- ReactiveCocoaFramework/ReactiveCocoa/NSUserDefaults+RACSupport.m
+++ ReactiveCocoaFramework/ReactiveCocoa/NSUserDefaults+RACSupport.m
@@ -7,14 +7,37 @@
 //
 
 #import "NSUserDefaults+RACSupport.h"
+
 #import "EXTScope.h"
 #import "NSNotificationCenter+RACSupport.h"
 #import "NSObject+RACDeallocating.h"
+#import "NSObject+RACDescription.h"
+#import "NSObject+RACLifting.h"
 #import "RACChannel.h"
 #import "RACScheduler.h"
 #import "RACSignal+Operations.h"
 
 @implementation NSUserDefaults (RACSupport)
+
+- (RACSignal *)rac_objectsForKey:(NSString *)key {
+	NSCParameterAssert(key != nil);
+
+	RACSignal *keySampler = [[NSNotificationCenter.defaultCenter
+		rac_addObserverForName:NSUserDefaultsDidChangeNotification object:self]
+		mapReplace:key];
+	
+	return [[[self
+		rac_liftSelector:@selector(objectForKey:) withSignals:keySampler, nil]
+		distinctUntilChanged]
+		setNameWithFormat:@"%@ -rac_objectsForKey: %@", self.rac_description, key];
+}
+
+@end
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-implementations"
+
+@implementation NSUserDefaults (RACSupportDeprecated)
 
 - (RACChannelTerminal *)rac_channelTerminalForKey:(NSString *)key {
 	RACChannel *channel = [RACChannel new];
@@ -54,3 +77,5 @@
 }
 
 @end
+
+#pragma clang diagnostic pop

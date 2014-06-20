251e102ad3443caccfbeb17d42dc8d7dd6e21d17
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-19 23:11:07 -0700

    Remove MutableBox, make Box final
    ++immutability

--- ReactiveCocoaFramework/ReactiveCocoa/Box.swift
+++ ReactiveCocoaFramework/ReactiveCocoa/Box.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 /// An immutable wrapper that can turn any value into an object.
-class Box<T> {
+@final class Box<T> {
 	let _closure: () -> T
 
 	/// The underlying value.
@@ -27,27 +27,5 @@ class Box<T> {
 	@conversion
 	func __conversion() -> T {
 		return value
-	}
-}
-
-/// A mutable box, that supports replacing its inner value.
-class MutableBox<T>: Box<T> {
-	var _mutableClosure: () -> T
-
-	/// The underlying value.
-	override var value: T {
-		get {
-			return _mutableClosure()
-		}
-	
-		set(newValue) {
-			_mutableClosure = { newValue }
-		}
-	}
-	
-	/// Initializes the box to wrap the given value.
-	init(_ value: T) {
-		_mutableClosure = { value }
-		super.init(value)
 	}
 }

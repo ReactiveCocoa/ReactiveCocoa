bbdadf5bd08332132ea14bbc7209cf50868814a6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-07-03 01:04:11 -0700

    Add NSURLSession extension as proof-of-concept

---
+++ ReactiveCocoaFramework/ReactiveCocoa/NSURLSessionExtensions.swift
@@ -0,0 +1,33 @@
+//
+//  NSURLSessionExtensions.swift
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2014-07-03.
+//  Copyright (c) 2014 GitHub, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension NSURLSession {
+	/// Returns a Producer that will fetch data for each Consumer using the
+	/// given request.
+	func rac_dataProducerWithRequest(request: NSURLRequest) -> Producer<(NSData, NSURLResponse)> {
+		return Producer { consumer in
+			let task = self.dataTaskWithRequest(request) { (data, response, error) in
+				if data == nil || response == nil {
+					consumer.put(.Error(error))
+				} else {
+					let value = (data!, response!)
+					consumer.put(.Next(Box(value)))
+					consumer.put(.Completed)
+				}
+			}
+
+			consumer.disposable.addDisposable {
+				task.cancel()
+			}
+
+			task.resume()
+		}
+	}
+}

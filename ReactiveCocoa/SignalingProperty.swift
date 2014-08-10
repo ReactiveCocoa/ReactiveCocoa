fc495ec9cb8a58082bb57e7f4d3a0a06891757f1
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2014-08-10 03:17:59 -0300

    Swift beta 5 compatibility

--- ReactiveCocoa/SignalingProperty.swift
+++ ReactiveCocoa/SignalingProperty.swift
@@ -7,7 +7,7 @@
 //
 
 /// Represents a mutable property of type T along with the changes to its value.
-public final class SignalingProperty<T>: Sink {
+public final class SignalingProperty<T>: SinkType {
 	public typealias Element = T
 
 	private let sink: SinkOf<T>

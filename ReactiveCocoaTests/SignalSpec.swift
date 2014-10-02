f474a2493827e7a79c8fac486c5ad6d62ab51585
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-01 22:28:50 -0700

    Test pipeWithInitialValue

--- ReactiveCocoaTests/SignalSpec.swift
+++ ReactiveCocoaTests/SignalSpec.swift
@@ -13,36 +13,55 @@ import ReactiveCocoa
 
 class SignalSpec: QuickSpec {
 	override func spec() {
-		it("should instantiate with a constant value") {
-			let signal = Signal.constant(42)
-			expect(signal.current).to(equal(42))
+		describe("instantiation") {
+			it("should instantiate with a constant value") {
+				let signal = Signal.constant(42)
+				expect(signal.current).to(equal(42))
 
-			var observedValue: Int? = nil
-			signal.observe { observedValue = $0 }
-			expect(observedValue).to(equal(42))
-		}
-
-		it("should instantiate with a custom generator") {
-			let queue = dispatch_queue_create("com.github.ReactiveCocoa.SignalSpec", DISPATCH_QUEUE_SERIAL)
-			dispatch_suspend(queue)
-
-			let signal = Signal(initialValue: 0) { sink in
-				dispatch_async(queue) {
-					for i in 1...3 {
-						sink.put(i)
-					}
-				}
+				var observedValue: Int? = nil
+				signal.observe { observedValue = $0 }
+				expect(observedValue).to(equal(42))
 			}
 
-			expect(signal.current).to(equal(0))
+			it("should instantiate with a custom generator") {
+				let queue = dispatch_queue_create("com.github.ReactiveCocoa.SignalSpec", DISPATCH_QUEUE_SERIAL)
+				dispatch_suspend(queue)
 
-			var observedValues: [Int] = []
-			signal.observe { observedValues.append($0) }
-			expect(observedValues).to(equal([ 0 ]))
+				let signal = Signal(initialValue: 0) { sink in
+					dispatch_async(queue) {
+						for i in 1...3 {
+							sink.put(i)
+						}
+					}
+				}
 
-			dispatch_resume(queue)
-			dispatch_sync(queue) {}
-			expect(observedValues).to(equal([ 0, 1, 2, 3 ]))
+				expect(signal.current).to(equal(0))
+
+				var observedValues: [Int] = []
+				signal.observe { observedValues.append($0) }
+				expect(observedValues).to(equal([ 0 ]))
+
+				dispatch_resume(queue)
+				dispatch_sync(queue) {}
+				expect(observedValues).to(equal([ 0, 1, 2, 3 ]))
+			}
+
+			it("should instantiate a pipe") {
+				let (signal, sink) = Signal.pipeWithInitialValue(0)
+				expect(signal.current).to(equal(0))
+
+				var observedValues: [Int] = []
+				signal.observe { observedValues.append($0) }
+				expect(observedValues).to(equal([ 0 ]))
+
+				sink.put(1)
+				expect(signal.current).to(equal(1))
+				expect(observedValues).to(equal([ 0, 1 ]))
+
+				sink.put(2)
+				expect(signal.current).to(equal(2))
+				expect(observedValues).to(equal([ 0, 1, 2 ]))
+			}
 		}
 
 		describe("interval") {

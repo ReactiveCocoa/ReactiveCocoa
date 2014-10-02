6c30e61f0c5bbc42121ad77c9eaf4f365332c773
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-01 22:12:51 -0700

    Basic tests for instantiating/observing a Signal

---
+++ ReactiveCocoaTests/SignalSpec.swift
@@ -0,0 +1,48 @@
+//
+//  SignalSpec.swift
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2014-10-01.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+import Foundation
+import Nimble
+import Quick
+import ReactiveCocoa
+
+class SignalSpec: QuickSpec {
+	override func spec() {
+		it("should instantiate with a constant value") {
+			let signal = Signal.constant(42)
+			expect(signal.current).to(equal(42))
+
+			var observedValue: Int? = nil
+			signal.observe { observedValue = $0 }
+			expect(observedValue).to(equal(42))
+		}
+
+		it("should instantiate with a custom generator") {
+			let queue = dispatch_queue_create("com.github.ReactiveCocoa.SignalSpec", DISPATCH_QUEUE_SERIAL)
+			dispatch_suspend(queue)
+
+			let signal = Signal(initialValue: 0) { sink in
+				dispatch_async(queue) {
+					for i in 1...3 {
+						sink.put(i)
+					}
+				}
+			}
+
+			expect(signal.current).to(equal(0))
+
+			var observedValues: [Int] = []
+			signal.observe { observedValues.append($0) }
+			expect(observedValues).to(equal([ 0 ]))
+
+			dispatch_resume(queue)
+			dispatch_sync(queue) {}
+			expect(observedValues).to(equal([ 0, 1, 2, 3 ]))
+		}
+	}
+}

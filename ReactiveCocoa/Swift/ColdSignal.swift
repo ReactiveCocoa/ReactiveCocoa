79ddcb2cd09003fd0a19ce6aa998fdaf4f41f472
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-18 13:42:20 -0700

    Move Event type into ColdSignal.swift
    It’s not used for other types.

--- ReactiveCocoa/Swift/ColdSignal.swift
+++ ReactiveCocoa/Swift/ColdSignal.swift
@@ -12,6 +12,61 @@ func doNothing<T>(value: T) {}
 func doNothing(error: NSError) {}
 func doNothing() {}
 
+/// Represents a stream event.
+///
+/// Streams must conform to the grammar:
+/// `Next* (Error | Completed)?`
+public enum Event<T> {
+	/// A value provided by the stream.
+	case Next(Box<T>)
+
+	/// The stream terminated because of an error.
+	case Error(NSError)
+
+	/// The stream successfully terminated.
+	case Completed
+
+	/// Whether this event indicates stream termination (from success or
+	/// failure).
+	public var isTerminating: Bool {
+		switch self {
+		case let .Next:
+			return false
+
+		default:
+			return true
+		}
+	}
+	
+	/// Lifts the given function over the event's value.
+	public func map<U>(f: T -> U) -> Event<U> {
+		switch self {
+		case let .Next(box):
+			return .Next(Box(f(box.unbox)))
+
+		case let .Error(error):
+			return .Error(error)
+
+		case let .Completed:
+			return .Completed
+		}
+	}
+
+	/// Case analysis on the receiver.
+	public func event<U>(#ifNext: T -> U, ifError: NSError -> U, ifCompleted: @autoclosure () -> U) -> U {
+		switch self {
+		case let .Next(box):
+			return ifNext(box.unbox)
+
+		case let .Error(err):
+			return ifError(err)
+
+		case let .Completed:
+			return ifCompleted()
+		}
+	}
+}
+
 /// A stream that will begin generating Events when a Subscriber is attached,
 /// possibly performing some side effects in the process. Events are pushed to
 /// the subscriber as they are generated.

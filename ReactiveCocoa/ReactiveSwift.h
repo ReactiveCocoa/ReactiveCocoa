040be4b97694be28f818273e48c22e65e4cf86a0
Alan Rogers <alanjrogers@me.com>
2014-11-06 22:32:23 +1100

    Add a swift only RAC framework.
    This plays nicely with Playgrounds.

---
+++ ReactiveCocoa/ReactiveSwift.h
@@ -0,0 +1,15 @@
+//
+//  ReactiveSwift.h
+//  ReactiveCocoa
+//
+//  Created by Alan Rogers on 6/11/2014.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactiveCocoa.
+FOUNDATION_EXPORT double ReactiveSwiftVersionNumber;
+
+//! Project version string for ReactiveCocoa.
+FOUNDATION_EXPORT const unsigned char ReactiveSwiftVersionString[];

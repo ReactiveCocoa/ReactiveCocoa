0c5cc55c8f6c6abb865e178257896cceb4ea76d0
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-11-21 13:51:59 -0800

    Test HotSignal.skip()

--- ReactiveCocoaTests/Swift/HotSignalSpec.swift
+++ ReactiveCocoaTests/Swift/HotSignalSpec.swift
@@ -126,6 +126,40 @@ class HotSignalSpec: QuickSpec {
 			}
 		}
 
+		describe("skip") {
+			it("should skip initial values") {
+				let (signal, sink) = HotSignal<Int>.pipe()
+				let newSignal = signal.skip(2)
+
+				var latestValue: Int?
+				newSignal.observe { latestValue = $0 }
+
+				expect(latestValue).to(beNil())
+
+				sink.put(1)
+				expect(latestValue).to(beNil())
+
+				sink.put(2)
+				expect(latestValue).to(beNil())
+
+				sink.put(3)
+				expect(latestValue).to(equal(3))
+			}
+
+			it("should not skip any values when 0") {
+				let (signal, sink) = HotSignal<Int>.pipe()
+				let newSignal = signal.skip(0)
+
+				var latestValue: Int?
+				newSignal.observe { latestValue = $0 }
+
+				expect(latestValue).to(beNil())
+
+				sink.put(1)
+				expect(latestValue).to(equal(1))
+			}
+		}
+
 		describe("replay") {
 			var signal: HotSignal<Int>!
 			var sink: SinkOf<Int>!

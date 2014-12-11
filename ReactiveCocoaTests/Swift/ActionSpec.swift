a903b4f844e23c842a784334984abff8546c7dff
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-12-11 14:42:54 -0800

    Stub ActionSpec

---
+++ ReactiveCocoaTests/Swift/ActionSpec.swift
@@ -0,0 +1,17 @@
+//
+//  ActionSpec.swift
+//  ReactiveCocoa
+//
+//  Created by Justin Spahr-Summers on 2014-12-11.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+import LlamaKit
+import Nimble
+import Quick
+import ReactiveCocoa
+
+class ActionSpec: QuickSpec {
+	override func spec() {
+	}
+}

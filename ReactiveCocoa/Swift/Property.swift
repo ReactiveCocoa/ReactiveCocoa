31f7deb3b6f734f2b5fd3742b14ca64276e11310
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-12-30 13:14:05 -0800

    Rename Plan<T> to SignalProducer<T>

--- ReactiveCocoa/Swift/Property.swift
+++ ReactiveCocoa/Swift/Property.swift
@@ -5,13 +5,13 @@ public final class Property<T> {
 	/// The current value of the property.
 	///
 	/// Setting this to a new value will notify all observers of any Signals
-	/// created from the `values` plan.
+	/// created from the `values` producer.
 	public var value: T { get set }
 
-	/// A plan for a signal that will send the property's current value,
+	/// A producer for a signal that will send the property's current value,
 	/// followed by all changes over time, then complete when the property has
 	/// deinitialized.
-	public let valuesPlan: Plan<T>
+	public let valuesProducer: SignalProducer<T>
 
 	/// Initializes a property with the given value to start.
 	public init(_ initialValue: T)
@@ -31,7 +31,7 @@ infix operator <~ {
 /// or when the signal sends a `Completed` event.
 public func <~ <T>(property: Property<T>, signal: Signal<T>)
 
-/// Creates a signal from the given plan, which will be immediately bound to
+/// Creates a signal from the given producer, which will be immediately bound to
 /// the given property, updating the property's value to the latest value sent
 /// by the signal.
 ///
@@ -40,4 +40,4 @@ public func <~ <T>(property: Property<T>, signal: Signal<T>)
 ///
 /// The binding will automatically terminate when the property is deinitialized,
 /// or when the created signal sends a `Completed` event.
-public func <~ <T>(property: Property<T>, plan: Plan<T>)
+public func <~ <T>(property: Property<T>, producer: SignalProducer<T>)
--- ReactiveCocoa/Swift/Signal.swift
+++ ReactiveCocoa/Swift/Signal.swift
@@ -6,11 +6,11 @@
 ///
 /// Signals are generally used to represent event streams that are already “in
 /// progress,” like notifications, user input, etc. To represent streams that
-/// must first be _started_, see the Plan type.
+/// must first be _started_, see the SignalProducer type.
 ///
 /// Signals do not need to be retained. A Signal will be automatically kept
 /// alive until the event stream has terminated, or until the operation which
-/// yielded the Signal (e.g., Plan.start) has been cancelled.
+/// yielded the Signal (e.g., SignalProducer.start) has been cancelled.
 public final class Signal<T> {
 	/// Initializes a Signal that will immediately invoke the given generator,
 	/// then forward events put into the given sink.

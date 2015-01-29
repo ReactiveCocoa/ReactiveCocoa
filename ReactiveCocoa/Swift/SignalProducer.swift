919fd790bebb09c95cde87fe2ee3b6d41bdb8ba9
Dave Lee <kastiglione@fb.com>
2015-01-29 00:56:57 -0800

    Compose wait() on top of last()

--- ReactiveCocoa/Swift/SignalProducer.swift
+++ ReactiveCocoa/Swift/SignalProducer.swift
@@ -618,19 +618,8 @@ public func last<T, E>(producer: SignalProducer<T, E>) -> Result<T, E>? {
 
 /// Starts the producer, then blocks, waiting for completion.
 public func wait<T, E>(producer: SignalProducer<T, E>) -> Result<(), E> {
-	let semaphore = dispatch_semaphore_create(0)
-	var result: Result<(), E>!
-
-	producer.start(error: { error in
-		result = failure(error)
-		dispatch_semaphore_signal(semaphore)
-	}, completed: {
-		result = success(())
-		dispatch_semaphore_signal(semaphore)
-	})
-
-	dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
-	return result
+	let result = producer |> map { _ in () } |> last
+	return result ?? success(())
 }
 
 /// SignalProducer.startWithSignal() as a free function, for easier use with |>.

b7a5cc78b780ae35be57aacaafc6c3ebda9f515d
Rob Rix <rob.rix@github.com>
2015-02-26 11:26:25 -0500

    Add a dynamic property superclass in ObjC.

---
+++ ReactiveCocoa/Objective-C/RACDynamicPropertySuperclass.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2015 GitHub. All rights reserved.
+
+@import Foundation;
+
+/// This superclass is an implementation detail of DynamicProperty. Do
+/// not use it.
+@interface RACDynamicPropertySuperclass : NSObject
+@end
--- ReactiveCocoa/ReactiveCocoa.h
+++ ReactiveCocoa/ReactiveCocoa.h
@@ -39,6 +39,7 @@ FOUNDATION_EXPORT const unsigned char ReactiveCocoaVersionString[];
 #import <ReactiveCocoa/RACCommand.h>
 #import <ReactiveCocoa/RACCompoundDisposable.h>
 #import <ReactiveCocoa/RACDisposable.h>
+#import <ReactiveCocoa/RACDynamicPropertySuperclass.h>
 #import <ReactiveCocoa/RACEvent.h>
 #import <ReactiveCocoa/RACGroupedSignal.h>
 #import <ReactiveCocoa/RACKVOChannel.h>

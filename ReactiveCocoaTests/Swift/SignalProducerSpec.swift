c1a9129e0d9a5edf976b514141384b7bf88e2e93
Neil Pankey <npankey@gmail.com>
2015-03-11 12:42:48 -0700

    Note the broken SignalProducer.start test

--- ReactiveCocoaTests/Swift/SignalProducerSpec.swift
+++ ReactiveCocoaTests/Swift/SignalProducerSpec.swift
@@ -469,6 +469,10 @@ class SignalProducerSpec: QuickSpec {
 				test()
 				expect(testSink).toNot(beNil())
 
+				// FIXME: Disposing _should_ result in the test sink being released here. Implementing
+				// an identical `start` method as a producer extension in the test module and using that
+				// works as expected. Calling the normal `start` method on `SignalProducer` leaks the
+				// sink a yet to be determined reason.
 				disposable.dispose()
 				expect(testSink).to(beNil())
 			}

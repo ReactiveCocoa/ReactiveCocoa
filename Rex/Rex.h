205af2854f36c489b4cfc441981e7b5e67d3ec3f
Neil Pankey <npankey@gmail.com>
2015-05-09 15:39:40 -0700

    Xcode project

---
+++ Rex/Rex.h
@@ -0,0 +1,19 @@
+//
+//  Rex.h
+//  Rex
+//
+//  Created by Neil Pankey on 5/9/15.
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Rex.
+FOUNDATION_EXPORT double RexVersionNumber;
+
+//! Project version string for Rex.
+FOUNDATION_EXPORT const unsigned char RexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rex/PublicHeader.h>
+
+

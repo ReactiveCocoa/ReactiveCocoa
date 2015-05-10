a5c573dd8be8cf60ef078c684048aaae54864b6f
Neil Pankey <npankey@gmail.com>
2015-05-10 14:19:32 -0700

    Overload ignoreError since curried defaults don't work

--- Source/Signal.swift
+++ Source/Signal.swift
@@ -26,9 +26,13 @@ public func filterMap<T, U, E>(transform: T -> U?)(signal: Signal<T, E>) -> Sign
     }
 }
 
-/// Returns a signal that drops any error events. A `replacement` terminal event
-/// can be provided, the default value is `Completed`.
-public func ignoreError<T, E>(replacement: Event<T, NoError> = .Completed)(signal: Signal<T, E>) -> Signal<T, NoError> {
+/// Returns a signal that drops `Error` events, replacing them with `Completed`.
+public func ignoreError<T, E>(signal: Signal<T, E>) -> Signal<T, NoError> {
+    return signal |> ignoreError(replacement: .Completed)
+}
+
+/// Returns a signal that drops `Error` sending `replacement` terminal event instead.
+public func ignoreError<T, E>(#replacement: Event<T, NoError>)(signal: Signal<T, E>) -> Signal<T, NoError> {
     precondition(replacement.isTerminating)
 
     return Signal { observer in

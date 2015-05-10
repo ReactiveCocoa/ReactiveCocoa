b5fccaad8bfec968fa35ec48cd8565480ed6e36d
Neil Pankey <npankey@gmail.com>
2015-05-09 17:10:27 -0700

    Test filterMap

--- Tests/SignalTests.swift
+++ Tests/SignalTests.swift
@@ -7,11 +7,31 @@
 //
 
 import Rex
+import ReactiveCocoa
 import XCTest
 
 final class SignalTests: XCTestCase {
 
     func testFilterMap() {
+        let (signal, sink) = Signal<Int, NoError>.pipe()
+        var values: [String] = []
 
+        signal
+            |> filterMap {
+                return $0 % 2 == 0 ? toString($0) : nil
+            }
+            |> observe(next: values.append)
+
+        sendNext(sink, 1)
+        XCTAssert(values == [])
+
+        sendNext(sink, 2)
+        XCTAssert(values == ["2"])
+
+        sendNext(sink, 3)
+        XCTAssert(values == ["2"])
+
+        sendNext(sink, 6)
+        XCTAssert(values == ["2", "6"])
     }
 }

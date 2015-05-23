1af4c9f6aa46b0716d3d2246ce81da21b50b4400
Neil Pankey <npankey@gmail.com>
2015-05-22 18:37:42 -0700

    Tweak rac_notifications test

--- ReactiveCocoaTests/Swift/FoundationExtensionsSpec.swift
+++ ReactiveCocoaTests/Swift/FoundationExtensionsSpec.swift
@@ -15,12 +15,15 @@ class FoundationExtensionsSpec: QuickSpec {
 	override func spec() {
 		describe("NSNotificationCenter.rac_notifications") {
 
-			it("should forward notifications on the producer") {
+			it("should send notifications on the producer") {
 				let center = NSNotificationCenter.defaultCenter()
 				let producer = center.rac_notifications(name: "rac_notifications_test")
 
 				var notif: NSNotification? = nil
 				let disposable = producer.start(next: { notif = $0 })
+
+				center.postNotificationName("some_other_notification", object: nil)
+				expect(notif).to(beNil())
 
 				center.postNotificationName("rac_notifications_test", object: nil)
 				expect(notif?.name).to(equal("rac_notifications_test"))

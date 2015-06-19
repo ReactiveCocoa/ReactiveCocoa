62137f865c10c18f2e015804a975ac5bfa0ccd8b
Neil Pankey <npankey@gmail.com>
2015-06-19 13:24:46 -0700

    UIControl.rex_enabled

---
+++ Source/UIKit/UIControl.swift
@@ -0,0 +1,16 @@
+//
+//  UIView.swift
+//  Rex
+//
+//  Created by Neil Pankey on 6/19/15.
+//  Copyright (c) 2015 Neil Pankey. All rights reserved.
+//
+
+import UIKit
+
+extension UIControl {
+    /// Wraps a control's `enabled` state in a bindable property.
+    public var rex_enabled: MutableProperty<Bool> {
+        return associatedProperty(self, &Keys.enabled, { self.enabled }, { self.enabled = $0 })
+    }
+}

f2ba5075bf45bae302bd8f57e60da1b0b142b589
Neil Pankey <npankey@gmail.com>
2015-07-04 13:08:13 -0700

    rex_controlEvents wrapper for rac_signalForControlEvents

--- Source/UIKit/UIControl.swift
+++ Source/UIKit/UIControl.swift
@@ -10,6 +10,14 @@ import ReactiveCocoa
 import UIKit
 
 extension UIControl {
+    /// Creates a producer for the sender whenever a specified control event is triggered.
+    public func rex_controlEvents(events: UIControlEvents) -> SignalProducer<UIControl?, NoError> {
+        return rac_signalForControlEvents(events)
+            .toSignalProducer()
+            |> map { $0 as? UIControl }
+            |> catch { _ in SignalProducer(value: nil) }
+    }
+
     /// Wraps a control's `enabled` state in a bindable property.
     public var rex_enabled: MutableProperty<Bool> {
         return rex_valueProperty(&enabled, { self.enabled }, { self.enabled = $0 })

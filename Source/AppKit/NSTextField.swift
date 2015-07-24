cc12082d38a5a9e0a4a9d13d61db4189f2488439
skyylex <yury.lapitsky@gmail.com>
2015-07-24 18:49:32 +0300

    rex_textSignal implementation

--- Source/AppKit/NSTextField.swift
+++ Source/AppKit/NSTextField.swift
@@ -11,31 +11,12 @@ import ReactiveCocoa
 import AppKit
 
 extension NSTextField {
-    /// Producer will forward all text changes related to current NSTextField
-    public var rex_textChanged : SignalProducer <String, NoError> {
-        return SignalProducer(values: [rex_textChangedOnSelector, rex_textChangedOnNotification]) |> flatten(FlattenStrategy.Merge)
-    }
-    
-    private var rex_textChangedOnSelector : SignalProducer<String, NoError> {
-        let signalProducer : SignalProducer = self.rac_signalForSelector("setStringValue:").toSignalProducer()
-        return signalProducer
-            |> map { next in
-                if let tuple = next as? RACTuple, text = tuple.first as? String {
-                    return text
-                } else {
-                    return ""
-                }
-            }
-            |> ignoreError
-    }
-    
-    private var rex_textChangedOnNotification: SignalProducer<String, NoError> {
-        return self.rac_textSignal().toSignalProducer()
-            |> map { next in
-                precondition(next == nil || next is String)
-                
-                return next as? String ?? ""
-            }
-            |> ignoreError
+    /// only changes from UI will be produced here
+    public var rex_textSignal: SignalProducer<String, NoError> {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        let notificationsProducer = notificationCenter.rac_notifications(name: NSControlTextDidChangeNotification, object: self)
+        return notificationsProducer
+            |> filter { notification in (notification.object as? NSTextField) == self }
+            |> map { notification in (notification.object as! NSTextField).stringValue }
     }
 }

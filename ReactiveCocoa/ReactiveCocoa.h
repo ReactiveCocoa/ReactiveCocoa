4a970053281a4f8f26bdcd847ccb53d7540556ce
Syo Ikeda <suicaicoca@gmail.com>
2015-07-27 17:29:23 +0900

    Using `#elif TARGET_OS_IPHONE` after evaluating `#if TARGET_OS_WATCH` seems fine

--- ReactiveCocoa/ReactiveCocoa.h
+++ ReactiveCocoa/ReactiveCocoa.h
@@ -63,7 +63,7 @@ FOUNDATION_EXPORT const unsigned char ReactiveCocoaVersionString[];
 #import <ReactiveCocoa/RACUnit.h>
 
 #if TARGET_OS_WATCH
-#elif defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+#elif TARGET_OS_IPHONE
 	#import <ReactiveCocoa/MKAnnotationView+RACSignalSupport.h>
 	#import <ReactiveCocoa/UIActionSheet+RACSignalSupport.h>
 	#import <ReactiveCocoa/UIAlertView+RACSignalSupport.h>

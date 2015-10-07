224124d1491576184c4fb704ee214309b0b7700b
NachoSoto <ignaciosoto90@gmail.com>
2015-10-07 15:09:15 -0700

    Fixed infinite recursion in RACScheduler.schedule

--- ReactiveCocoa/Swift/ObjectiveCBridging.swift
+++ ReactiveCocoa/Swift/ObjectiveCBridging.swift
@@ -15,7 +15,7 @@ extension RACScheduler: DateSchedulerType {
 	}
 
 	public func schedule(action: () -> ()) -> Disposable? {
-		return self.schedule(action)
+		return (self.schedule(action) as RACDisposable) as Disposable?
 	}
 
 	public func scheduleAfter(date: NSDate, action: () -> ()) -> Disposable? {

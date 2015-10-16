dd12633093db5ffc80466d220256788c5c8c9dd9
Paul Young <paulyoungonline@gmail.com>
2015-10-15 23:17:04 -0700

    Add missing overload for signal.

--- ReactiveCocoa/Swift/SignalProducer.swift
+++ ReactiveCocoa/Swift/SignalProducer.swift
@@ -534,6 +534,17 @@ extension SignalProducerType {
 	public func sampleOn(sampler: SignalProducer<(), NoError>) -> SignalProducer<[Value], Error> {
 		return lift(Signal.sampleOn)(sampler)
 	}
+	
+	/// Forwards an array of values sent from `self` since the last time
+	/// `sampler` sent a Next event.
+	///
+	/// Returns a producer that will yield an array of values from `self`,
+	/// sampled by `sampler`, then complete once both input signals have
+	/// completed, or interrupt if either input signal is interrupted.
+	@warn_unused_result(message="Did you forget to call `start` on the producer?")
+	public func sampleOn(sampler: Signal<(), NoError>) -> SignalProducer<[Value], Error> {
+		return lift(Signal.sampleOn)(sampler)
+	}
 
 	/// Forwards events from `self` until `trigger` sends a Next or Completed
 	/// event, at which point the returned producer will complete.

9af72d3e961d9f7a7d5e43cd8d10e58880091e9c
Nate Stedman <natesm@gmail.com>
2015-12-04 02:30:55 -0500

    Move DynamicProperty to a separate file.
    This needs to be done to support the Swift Package
    Manager as it currently exists, because there’s no
    support for Objective-C. Therefore, the superclass
    for DynamicProperty isn’t available.
    
    It’s in the Tests directory again, because
    otherwise the Swift will try to build it.

--- Sources/ReactiveCocoa/Swift/Property.swift
+++ Sources/ReactiveCocoa/Swift/Property.swift
@@ -123,62 +123,6 @@ public final class MutableProperty<Value>: MutablePropertyType {
 	}
 }
 
-/// Wraps a `dynamic` property, or one defined in Objective-C, using Key-Value
-/// Coding and Key-Value Observing.
-///
-/// Use this class only as a last resort! `MutableProperty` is generally better
-/// unless KVC/KVO is required by the API you're using (for example,
-/// `NSOperation`).
-@objc public final class DynamicProperty: RACDynamicPropertySuperclass, MutablePropertyType {
-	public typealias Value = AnyObject?
-
-	private weak var object: NSObject?
-	private let keyPath: String
-
-	/// The current value of the property, as read and written using Key-Value
-	/// Coding.
-	public var value: AnyObject? {
-		@objc(rac_value) get {
-			return object?.valueForKeyPath(keyPath)
-		}
-
-		@objc(setRac_value:) set(newValue) {
-			object?.setValue(newValue, forKeyPath: keyPath)
-		}
-	}
-
-	/// A producer that will create a Key-Value Observer for the given object,
-	/// send its initial value then all changes over time, and then complete
-	/// when the observed object has deallocated.
-	///
-	/// By definition, this only works if the object given to init() is
-	/// KVO-compliant. Most UI controls are not!
-	public var producer: SignalProducer<AnyObject?, NoError> {
-		if let object = object {
-			return object.rac_valuesForKeyPath(keyPath, observer: nil).toSignalProducer()
-				// Errors aren't possible, but the compiler doesn't know that.
-				.flatMapError { error in
-					0 // suppresses implicit return error on fatalError
-					fatalError("Received unexpected error from KVO signal: \(error)")
-				}
-		} else {
-			return .empty
-		}
-	}
-
-	/// Initializes a property that will observe and set the given key path of
-	/// the given object. `object` must support weak references!
-	public init(object: NSObject?, keyPath: String) {
-		self.object = object
-		self.keyPath = keyPath
-		
-		/// DynamicProperty stay alive as long as object is alive.
-		/// This is made possible by strong reference cycles.
-		super.init()
-		object?.rac_willDeallocSignal()?.toSignalProducer().startWithCompleted { self }
-	}
-}
-
 infix operator <~ {
 	associativity right
 

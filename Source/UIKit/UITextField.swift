2e4d4a8daf9fbf10381d684b7d613e2b6f8c7d2d
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-17 03:20:44 +0000

    Added the rex_textSignal to UITextField

---
+++ Source/UIKit/UITextField.swift
@@ -0,0 +1,24 @@
+//
+//  UITextField.swift
+//  Rex
+//
+//  Created by Rui Peres on 17/01/2016.
+//  Copyright © 2016 Neil Pankey. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+import UIKit
+
+extension UITextField {
+    
+    /// Sends the field's string value whenever it changes.
+    public var rex_textSignal: SignalProducer<String, NoError> {
+        return NSNotificationCenter.defaultCenter()
+            .rac_notifications(UITextFieldTextDidChangeNotification, object: self)
+            .filterMap { notification in
+                guard let textField = notification.object as? UITextField else { return nil}
+                return textField.text
+        }
+    }
+}

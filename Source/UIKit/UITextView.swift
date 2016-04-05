b529c3405e1215a2aafd9217ded2dcf148f4f6af
Rui Peres <rui.peres@mailonline.co.uk>
2016-04-05 11:28:14 +0100

    Added UITextView text's producer

---
+++ Source/UIKit/UITextView.swift
@@ -0,0 +1,24 @@
+//
+//  UITextView.swift
+//  Rex
+//
+//  Created by Rui Peres on 05/04/2016.
+//  Copyright © 2016 Neil Pankey. All rights reserved.
+//
+
+import ReactiveCocoa
+import UIKit
+import enum Result.NoError
+
+extension UITextView {
+    
+    /// Sends the textView's string value whenever it changes.
+    public var rex_textSignal: SignalProducer<String, NoError> {
+        return NSNotificationCenter.defaultCenter()
+            .rac_notifications(UITextViewTextDidChangeNotification, object: self)
+            .filterMap { notification in
+                guard let textView = notification.object as? UITextView else { return nil}
+                return textView.text
+        }
+    }
+}
---
+++ Tests/UIKit/UITextViewTests.swift
@@ -0,0 +1,29 @@
+//
+//  UITextViewTests.swift
+//  Rex
+//
+//  Created by Rui Peres on 05/04/2016.
+//  Copyright © 2016 Neil Pankey. All rights reserved.
+//
+
+import ReactiveCocoa
+import UIKit
+import XCTest
+
+class UITextViewTests: XCTestCase {
+    
+    func testTextProperty() {
+        let expectation = self.expectationWithDescription("Expected textSignal's value to equal to the textViews's text")
+        defer { self.waitForExpectationsWithTimeout(2, handler: nil) }
+        
+        let textView = UITextView(frame: CGRectZero)
+        textView.text = "Test"
+        
+        textView.rex_textSignal.startWithNext { text in
+            XCTAssertEqual(text, textView.text)
+            expectation.fulfill()
+        }
+        
+        NSNotificationCenter.defaultCenter().postNotificationName(UITextViewTextDidChangeNotification, object: textView)
+    }
+}

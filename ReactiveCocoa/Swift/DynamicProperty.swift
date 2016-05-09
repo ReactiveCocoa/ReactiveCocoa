ReactiveCocoa/ReactiveCocoa

commit 0059830a20e29956f0d6d5dc1a7d08920e1b8b6e

Allow DynamicProperty to support regular reference types
This change removes the `_ObjectiveCBridgeable` constraint on
`DynamicProperty`, replacing it with two convenience initialisers that
are only defined for types that can be represented in Objective-C. So
while `DynamicProperty` appears to be unconstrained, it is in practice
only constructable for particular generic types.

ReactiveCocoa/Swift/DynamicProperty.swift
@@ -1,29 +1,36 @@
 import Foundation
 import enum Result.NoError
 
+/// Models types that can be represented in Objective-C (i.e., reference
+/// types, including generic types when boxed via `AnyObject`).
+private protocol ObjectiveCRepresenting {
+	associatedtype Value
+	func extractValue(fromRepresentation representation: AnyObject) -> Value
+	func represent(value value: Value) -> AnyObject
+}
+
 /// Wraps a `dynamic` property, or one defined in Objective-C, using Key-Value
 /// Coding and Key-Value Observing.
 ///
 /// Use this class only as a last resort! `MutableProperty` is generally better
 /// unless KVC/KVO is required by the API you're using (for example,
 /// `NSOperation`).
-public final class DynamicProperty<SwiftValue: _ObjectiveCBridgeable>: MutablePropertyType {
-	public typealias Value = SwiftValue?
-
+public final class DynamicProperty<Value>: MutablePropertyType {
 	private weak var object: NSObject?
 	private let keyPath: String
+	private let representation: AnyRepresentation<Value>
 
-	private var property: MutableProperty<SwiftValue?>?
+	private var property: MutableProperty<Value?>?
 
 	/// The current value of the property, as read and written using Key-Value
 	/// Coding.
-	public var value: SwiftValue? {
+	public var value: Value? {
 		get {
-			return object?.valueForKeyPath(keyPath).map { $0 as! SwiftValue }
+			return object?.valueForKeyPath(keyPath).map(representation.extractValue(fromRepresentation:))
 		}
 
 		set(newValue) {
-			object?.setValue(newValue?._bridgeToObjectiveC(), forKeyPath: keyPath)
+			object?.setValue(newValue.map(representation.represent(value:)), forKeyPath: keyPath)
 		}
 	}
 
@@ -33,20 +40,21 @@ public final class DynamicProperty<SwiftValue: _ObjectiveCBridgeable>: MutablePr
 	///
 	/// By definition, this only works if the object given to init() is
 	/// KVO-compliant. Most UI controls are not!
-	public var producer: SignalProducer<SwiftValue?, NoError> {
+	public var producer: SignalProducer<Value?, NoError> {
 		return property?.producer ?? .empty
 	}
 
-	public var signal: Signal<SwiftValue?, NoError> {
+	public var signal: Signal<Value?, NoError> {
 		return property?.signal ?? .empty
 	}
 
 	/// Initializes a property that will observe and set the given key path of
 	/// the given object. `object` must support weak references!
-	public init(object: NSObject?, keyPath: String) {
+	private init<Representation: ObjectiveCRepresenting where Representation.Value == Value>(object: NSObject?, keyPath: String, representation: Representation) {
 		self.object = object
 		self.keyPath = keyPath
 		self.property = MutableProperty(nil)
+		self.representation = AnyRepresentation(representation)
 
 		/// DynamicProperty stay alive as long as object is alive.
 		/// This is made possible by strong reference cycles.
@@ -56,12 +64,78 @@ public final class DynamicProperty<SwiftValue: _ObjectiveCBridgeable>: MutablePr
 			.start { event in
 				switch event {
 				case let .Next(newValue):
-					self.property?.value = newValue.map { $0 as! SwiftValue }
+					self.property?.value = newValue.map(representation.extractValue(fromRepresentation:))
 				case let .Failed(error):
 					fatalError("Received unexpected error from KVO signal: \(error)")
 				case .Interrupted, .Completed:
 					self.property = nil
 				}
 			}
+	}
+}
+
+extension DynamicProperty where Value: _ObjectiveCBridgeable {
+	/// Initializes a property that will observe and set the given key path of
+	/// the given object, where `Value` is a value type that is bridgeable
+	/// to Objective-C.
+	///
+	/// `object` must support weak references!
+	public convenience init(object: NSObject, keyPath: String) {
+		self.init(object: object, keyPath: keyPath, representation: BridgeableRepresentation())
+	}
+}
+
+extension DynamicProperty where Value: AnyObject {
+	/// Initializes a property that will observe and set the given key path of
+	/// the given object, where `Value` is a reference type that can be
+	/// represented directly in Objective-C via `AnyObject`.
+	///
+	/// `object` must support weak references!
+	public convenience init(object: NSObject, keyPath: String) {
+		self.init(object: object, keyPath: keyPath, representation: DirectRepresentation())
+	}
+}
+
+/// Represents values in Objective-C directly, via `AnyObject`.
+private struct DirectRepresentation<Value: AnyObject>: ObjectiveCRepresenting {
+	func extractValue(fromRepresentation representation: AnyObject) -> Value {
+		return representation as! Value
+	}
+
+	func represent(value value: Value) -> AnyObject {
+		return value
+	}
+}
+
+/// Represents values in Objective-C indirectly, via bridging.
+private struct BridgeableRepresentation<Value: _ObjectiveCBridgeable>: ObjectiveCRepresenting {
+	func extractValue(fromRepresentation representation: AnyObject) -> Value {
+		let object = representation as! Value._ObjectiveCType
+		var result: Value?
+		Value._forceBridgeFromObjectiveC(object, result: &result)
+		return result!
+	}
+
+	func represent(value value: Value) -> AnyObject {
+		return value._bridgeToObjectiveC()
+	}
+}
+
+/// A type-erased wrapper for generic types representable in Objective-C.
+private struct AnyRepresentation<Value>: ObjectiveCRepresenting {
+	private let extract: AnyObject -> Value
+	private let represent: Value -> AnyObject
+
+	init<Base: ObjectiveCRepresenting where Base.Value == Value>(_ base: Base) {
+		self.extract = { base.extractValue(fromRepresentation: $0) }
+		self.represent = { base.represent(value: $0) }
+	}
+
+	func extractValue(fromRepresentation representation: AnyObject) -> Value {
+		return extract(representation)
+	}
+
+	func represent(value value: Value) -> AnyObject {
+		return represent(value)
 	}
 }

ReactiveCocoaTests/Swift/PropertySpec.swift
@@ -518,6 +518,12 @@ class PropertySpec: QuickSpec {
 				object = nil
 				expect(dynamicProperty).to(beNil())
 			}
+
+			it("should support un-bridged reference types") {
+				let dynamicProperty = DynamicProperty<UnbridgedObject>(object: object, keyPath: "rac_reference")
+				dynamicProperty.value = UnbridgedObject("foo")
+				expect(object.rac_reference.value) == "foo"
+			}
 		}
 
 		describe("binding") {
@@ -713,4 +719,12 @@ class PropertySpec: QuickSpec {
 
 private class ObservableObject: NSObject {
 	dynamic var rac_value: Int = 0
+	dynamic var rac_reference: UnbridgedObject = UnbridgedObject("")
+}
+
+private class UnbridgedObject: NSObject {
+	let value: String
+	init(_ value: String) {
+		self.value = value
+	}
 }

commit 1c86b1bedd175cb94ae62215343ba5464c08622f

Allow DynamicProperty to be initialized with an optional object
This regression was introduced in
0059830a20e29956f0d6d5dc1a7d08920e1b8b6e.

ReactiveCocoa/Swift/DynamicProperty.swift
@@ -83,7 +83,7 @@ extension DynamicProperty where Value: _ObjectiveCBridgeable {
 	/// to Objective-C.
 	///
 	/// `object` must support weak references!
-	public convenience init(object: NSObject, keyPath: String) {
+	public convenience init(object: NSObject?, keyPath: String) {
 		self.init(object: object, keyPath: keyPath, representable: BridgeableRepresentation.self)
 	}
 }
@@ -94,7 +94,7 @@ extension DynamicProperty where Value: AnyObject {
 	/// represented directly in Objective-C via `AnyObject`.
 	///
 	/// `object` must support weak references!
-	public convenience init(object: NSObject, keyPath: String) {
+	public convenience init(object: NSObject?, keyPath: String) {
 		self.init(object: object, keyPath: keyPath, representable: DirectRepresentation.self)
 	}
 }
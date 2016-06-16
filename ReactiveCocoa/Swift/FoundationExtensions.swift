9efc5afe39cc81f1978c9626e8955526d8daaf61
Eimantas Vaiciunas <eimantas@walkingsmarts.com>
2016-06-16 07:42:37 +0300

    Add markup to `FoundationExtensions.swift`

--- ReactiveCocoa/Swift/FoundationExtensions.swift
+++ ReactiveCocoa/Swift/FoundationExtensions.swift
@@ -10,11 +10,19 @@ import Foundation
 import enum Result.NoError
 
 extension NSNotificationCenter {
-	/// Returns a producer of notifications posted that match the given criteria.
-	/// If the `object` is deallocated before starting the producer, it will 
-	/// terminate immediatelly with an Interrupted event. Otherwise, the producer
-	/// will not terminate naturally, so it must be explicitly disposed to avoid
-	/// leaks.
+	/// A function to observe posted notifications through returned signal
+	/// producer.
+	///
+	/// - parameters:
+	///   - name: notification name to observe
+	///   - object: an instance that sends the notifications
+	/// - returns: A producer of notifications posted that match the given 
+	///            criteria.
+	///
+	/// - note: If the `object` is deallocated before starting the producer, it
+	///         will terminate immediatelly with an Interrupted event.
+	///         Otherwise, the producer will not terminate naturally, so it must
+	///         be explicitly disposed to avoid leaks.
 	public func rac_notifications(name: String? = nil, object: AnyObject? = nil) -> SignalProducer<NSNotification, NoError> {
 		// We're weakly capturing an optional reference here, which makes destructuring awkward.
 		let objectWasNil = (object == nil)
@@ -38,8 +46,12 @@ extension NSNotificationCenter {
 private let defaultSessionError = NSError(domain: "org.reactivecocoa.ReactiveCocoa.rac_dataWithRequest", code: 1, userInfo: nil)
 
 extension NSURLSession {
-	/// Returns a producer that will execute the given request once for each
-	/// invocation of start().
+	/// - parameter request: A request that will be performed when producer is
+	///                      started
+	/// - returns: A producer that will execute the given request once for each
+	///            invocation of `start()`.
+	/// - note: This method will not send error event upon server-side error
+	///         (i.e. when response with code other than 200...299 is received).
 	public func rac_dataWithRequest(request: NSURLRequest) -> SignalProducer<(NSData, NSURLResponse), NSError> {
 		return SignalProducer { observer, disposable in
 			let task = self.dataTaskWithRequest(request) { data, response, error in

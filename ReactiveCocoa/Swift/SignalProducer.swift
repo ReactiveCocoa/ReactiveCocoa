5fe503518f10c17c3c7859d507f6f2c955562514
NachoSoto <ignaciosoto90@gmail.com>
2016-06-28 10:35:07 -0700

    Deprecated SignalProducer.startWithNext (in favor of the more specific one for Error == NoError)

--- ReactiveCocoa/Swift/SignalProducer.swift
+++ ReactiveCocoa/Swift/SignalProducer.swift
@@ -361,6 +361,11 @@ extension SignalProducerType {
 		return start(Observer(observerAction))
 	}
 
+	@available(*, unavailable, message="This SignalProducer may emit errors which must be handled explicitly, or observed using startWithResult:")
+	public func startWithNext(next: Value -> Void) -> Disposable {
+		fatalError("Unavailable method")
+	}
+
 	/// Creates a Signal from the producer, then adds an observer to
 	/// the Signal, which will invoke the given callback when `next` or `failed` 
 	/// events are received.

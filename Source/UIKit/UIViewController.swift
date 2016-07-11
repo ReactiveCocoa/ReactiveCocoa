356d839a7c6bd4ed908ef858d4c22d0610e4cea1
Anders <hello@andersio.co>
2016-07-11 10:44:56 +0800

    Update `UIViewController`.

--- Source/UIKit/UIViewController.swift
+++ Source/UIKit/UIViewController.swift
@@ -14,36 +14,36 @@ extension UIViewController {
     /// Returns a `Signal`, that will be triggered
     /// when `self`'s `viewDidDisappear` is called
     public var rex_viewDidDisappear: Signal<(), NoError> {
-        return triggerForSelector(#selector(UIViewController.viewDidDisappear(_:)))
+        return trigger(for: #selector(UIViewController.viewDidDisappear(_:)))
     }
-    
+
     /// Returns a `Signal`, that will be triggered
     /// when `self`'s `viewWillDisappear` is called
     public var rex_viewWillDisappear: Signal<(), NoError> {
-        return triggerForSelector(#selector(UIViewController.viewWillDisappear(_:)))
+        return trigger(for: #selector(UIViewController.viewWillDisappear(_:)))
     }
-    
+
     /// Returns a `Signal`, that will be triggered
     /// when `self`'s `viewDidAppear` is called
     public var rex_viewDidAppear: Signal<(), NoError> {
-        return triggerForSelector(#selector(UIViewController.viewDidAppear(_:)))
+        return trigger(for: #selector(UIViewController.viewDidAppear(_:)))
     }
-    
+
     /// Returns a `Signal`, that will be triggered
     /// when `self`'s `viewWillAppear` is called
     public var rex_viewWillAppear: Signal<(), NoError> {
-        return triggerForSelector(#selector(UIViewController.viewWillAppear(_:)))
+        return trigger(for: #selector(UIViewController.viewWillAppear(_:)))
     }
-    
+
     private func trigger(for selector: Selector) -> Signal<(), NoError>  {
         return self
             .rac_signal(for: selector)
             .rex_toTriggerSignal()
     }
-    
+
     public typealias DismissingCompletion = ((Void) -> Void)?
     public typealias DismissingInformation = (animated: Bool, completion: DismissingCompletion)?
-    
+
     /// Wraps a viewController's `dismissViewControllerAnimated` function in a bindable property.
     /// It mimics the same input as `dismissViewControllerAnimated`: a `Bool` flag for the animation
     /// and a `(Void -> Void)?` closure for `completion`.
@@ -55,21 +55,21 @@ extension UIViewController {
     /// The dismissal observation can be made either with binding (example above)
     /// or `viewController.dismissViewControllerAnimated(true, completion: nil)`
     public var rex_dismissAnimated: MutableProperty<DismissingInformation> {
-        
+
         let initial: (UIViewController) -> DismissingInformation = { _ in nil }
         let setter: (UIViewController, DismissingInformation) -> Void = { host, dismissingInfo in
-            
+
             guard let unwrapped = dismissingInfo else { return }
             host.dismiss(animated: unwrapped.animated, completion: unwrapped.completion)
         }
-        
+
         let property = associatedProperty(self, key: &dismissModally, initial: initial, setter: setter) { property in
             property <~ self.rac_signal(for: #selector(UIViewController.dismiss))
                 .take { _ in property.value != nil }
                 .rex_toTriggerSignal()
                 .map { _ in return nil }
         }
-        
+
         return property
     }
 }

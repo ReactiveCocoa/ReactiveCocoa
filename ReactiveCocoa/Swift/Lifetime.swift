3b167c968a588a4287b73a993c4cdaae60be2b69
Anders <hello@andersio.co>
2016-07-12 22:30:31 +0800

    Provides a default implementation of `lifetimeProducer`.

--- ReactiveCocoa/Swift/Lifetime.swift
+++ ReactiveCocoa/Swift/Lifetime.swift
@@ -16,6 +16,16 @@ public protocol LifetimeProviding: class {
 	var lifetimeProducer: SignalProducer<(), NoError> { get }
 }
 
+extension LifetimeProviding {
+	/// An interruptible observation to the lifetime of `self`.
+	///
+	/// The signal emits `completed` when the object completes, or
+	/// `interrupted` after the object is completed.
+	public var lifetimeProducer: SignalProducer<(), NoError> {
+		return SignalProducer(signal: lifetime)
+	}
+}
+
 internal final class DeallocationToken {
 	let (deallocSignal, observer) = Signal<(), NoError>.pipe()
 

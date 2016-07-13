aacf7be927b735c2d349174dc3a47d3a09489197
Anders <hello@andersio.co>
2016-07-13 13:47:47 +0800

    Update the documentation of `LifetimeProviding`.

--- ReactiveCocoa/Swift/Lifetime.swift
+++ ReactiveCocoa/Swift/Lifetime.swift
@@ -1,26 +1,23 @@
 import enum Result.NoError
 
 /// Describes the ability of an object to notify others of its deinitialization.
-/// Proxy objects may pass through the lifetime of its underlying object.
+///
+/// Proxy objects are allowed to pass through the lifetime of its underlying object.
 public protocol LifetimeProviding: class {
-	/// A signal representing the lifetime of `self`.
-	///
-	/// The signal emits `completed` when the object completes, or
-	/// `interrupted` after the object is completed.
+	/// The signal emits `completed` when the object deinitializes, or
+	/// `interrupted` after the object has deinitialized.
 	var lifetime: Signal<(), NoError> { get }
 
-	/// An interruptible observation to the lifetime of `self`.
-	///
-	/// The signal emits `completed` when the object completes, or
-	/// `interrupted` after the object is completed.
+	/// A producer of signals that would emit `completed` when the
+	/// object deinitializes, or `interrupted` after the object has
+	/// deinitialized.
 	var lifetimeProducer: SignalProducer<(), NoError> { get }
 }
 
 extension LifetimeProviding {
-	/// An interruptible observation to the lifetime of `self`.
-	///
-	/// The signal emits `completed` when the object completes, or
-	/// `interrupted` after the object is completed.
+	/// A producer of signals that would emit `completed` when the
+	/// object deinitializes, or `interrupted` after the object has
+	/// deinitialized.
 	public var lifetimeProducer: SignalProducer<(), NoError> {
 		return SignalProducer(signal: lifetime)
 	}

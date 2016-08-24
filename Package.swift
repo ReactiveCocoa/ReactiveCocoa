d5acfb76f4141b1427973f9e1adb0c488be6c509
Matt Diephouse <matt@diephouse.com>
2016-08-23 20:07:59 -0400

    Use newer version of Result

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "ReactiveSwift",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", "3.0.0-alpha.2")
+        .Package(url: "https://github.com/antitypical/Result.git", "3.0.0-alpha.3")
     ],
     exclude: [
         "Sources/Deprecations+Removals.swift",

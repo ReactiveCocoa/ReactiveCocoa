b99eb0cf0df5667e116167f5ae38470a4d952bb9
Matt Diephouse <matt@diephouse.com>
2016-09-17 17:24:09 -0400

    Remove ReactiveObjC and ReactiveObjCBridge

--- ReactiveObjC/RACCompoundDisposableProvider.d
+++
@@ -1,4 +0,0 @@
-provider RACCompoundDisposable {
-    probe added(char *compoundDisposable, char *disposable, long newTotal);
-    probe removed(char *compoundDisposable, char *disposable, long newTotal);
-};

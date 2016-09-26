0c5a4da8247636554d0af6b3a6cec60144df7223
Anders <hello@andersio.co>
2016-09-27 01:13:38 +0200

    Nullability annotation for the private header.

--- ReactiveCocoa/RACObjCRuntimeUtilities.h
+++ ReactiveCocoa/RACObjCRuntimeUtilities.h
@@ -1,8 +1,10 @@
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
 typedef void (^rac_receiver_t)(void);
 
 /// Register a block which would be triggered when `selector` is called.
 ///
 /// Warning: The callee is responsible for synchronization.
-BOOL RACRegisterBlockForSelector(NSObject *self, SEL selector, Protocol *protocol, rac_receiver_t receiver);
+BOOL RACRegisterBlockForSelector(NSObject *self, SEL selector, Protocol * _Nullable protocol, rac_receiver_t receiver);
+NS_ASSUME_NONNULL_END

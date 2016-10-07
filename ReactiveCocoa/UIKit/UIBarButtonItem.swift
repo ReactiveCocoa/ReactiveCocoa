ec69f254865db3563d024264291318864596c424
Anders <hello@andersio.co>
2016-10-07 17:58:06 +0200

    Enabled state binding in `Reactive<UIBarButtonItem>.pressed`.

--- ReactiveCocoa/UIKit/UIBarButtonItem.swift
+++ ReactiveCocoa/UIKit/UIBarButtonItem.swift
@@ -10,19 +10,25 @@ import ReactiveSwift
 import UIKit
 
 extension Reactive where Base: UIBarButtonItem {
-	private var associatedAction: Atomic<CocoaAction<Base>?> {
+	private var associatedAction: Atomic<(action: CocoaAction<Base>, disposable: Disposable)?> {
 		return associatedValue { _ in Atomic(nil) }
 	}
 
 	public var pressed: CocoaAction<Base>? {
 		get {
-			return associatedAction.value
+			return associatedAction.value?.action
 		}
 
 		nonmutating set {
-			associatedAction.value = newValue
 			base.target = newValue
 			base.action = newValue.map { _ in CocoaAction<Base>.selector }
+
+			associatedAction
+				.swap(newValue.map { action in
+						let disposable = isEnabled <~ action.isEnabled
+						return (action, disposable)
+				})?
+				.disposable.dispose()
 		}
 	}
 }

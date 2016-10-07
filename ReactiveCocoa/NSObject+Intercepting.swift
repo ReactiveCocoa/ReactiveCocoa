0093b650427485566323bd036a47fd2ebb510cd1
Anders <hello@andersio.co>
2016-10-07 16:50:54 +0200

    Reused `associatedObject(_:key:initial:) in `signal(for:setup:)`.

--- ReactiveCocoa/NSObject+Intercepting.swift
+++ ReactiveCocoa/NSObject+Intercepting.swift
@@ -14,15 +14,7 @@ extension Reactive where Base: NSObject {
 		objc_sync_enter(self)
 		defer { objc_sync_exit(self) }
 
-		let map: NSMutableDictionary = {
-			if let map = objc_getAssociatedObject(self, &mapKey) as? NSMutableDictionary {
-				return map
-			} else {
-				let map = NSMutableDictionary()
-				objc_setAssociatedObject(self, &mapKey, map, .OBJC_ASSOCIATION_RETAIN)
-				return map
-			}
-		}()
+		let map = associatedObject(base, key: &mapKey) { _ in NSMutableDictionary() }
 
 		let selectorName = String(describing: selector) as NSString
 		if let signal = map.object(forKey: selectorName) as? Signal<U, NoError> {

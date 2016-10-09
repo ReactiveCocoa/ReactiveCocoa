b75b3d6cf6e6a700ecc40576bfc90a0426a98651
Anders <hello@andersio.co>
2016-10-09 14:02:32 +0200

    Fixed a typo in the doc of `UIProgressView.reactive`.

--- ReactiveCocoa/UIKit/UIProgressView.swift
+++ ReactiveCocoa/UIKit/UIProgressView.swift
@@ -2,7 +2,7 @@ import ReactiveSwift
 import UIKit
 
 extension Reactive where Base: UIProgressView {
-	/// Sets the reative progress to be reflected by the progress view.
+	/// Sets the relative progress to be reflected by the progress view.
 	public var progress: BindingTarget<Float> {
 		return makeBindingTarget { $0.progress = $1 }
 	}

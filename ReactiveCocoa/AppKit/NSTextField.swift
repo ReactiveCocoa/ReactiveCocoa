24cd6230d200b70bddcb049cd21bb5296a1a5f01
Anders <anders@andersha.com>
2016-10-15 02:50:32 +0200

    Added `NSTextField.reactive.continuousAttributedStringValues`.

--- ReactiveCocoa/AppKit/NSTextField.swift
+++ ReactiveCocoa/AppKit/NSTextField.swift
@@ -16,4 +16,19 @@ extension Reactive where Base: NSTextField {
 
 		return signal
 	}
+
+	/// A signal of values in `NSAttributedString` from the text field upon any
+	/// changes.
+	public var continuousAttributedStringValues: Signal<NSAttributedString, NoError> {
+		var signal: Signal<NSAttributedString, NoError>!
+
+		NotificationCenter.default
+			.reactive
+			.notifications(forName: .NSControlTextDidChange, object: base)
+			.take(during: lifetime)
+			.map { ($0.object as! NSTextField).attributedStringValue }
+			.startWithSignal { innerSignal, _ in signal = innerSignal }
+
+		return signal
+	}
 }

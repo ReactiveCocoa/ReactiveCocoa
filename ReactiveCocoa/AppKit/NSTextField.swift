1c3a3a0dd0ace8123fded783db269d96ec60c74b
Anders <anders@andersha.com>
2016-10-19 00:58:33 +0200

    ReactiveSwift 1.0.0-alpha.3

--- ReactiveCocoa/AppKit/NSTextField.swift
+++ ReactiveCocoa/AppKit/NSTextField.swift
@@ -5,30 +5,20 @@ import enum Result.NoError
 extension Reactive where Base: NSTextField {
 	/// A signal of values in `String` from the text field upon any changes.
 	public var continuousStringValues: Signal<String, NoError> {
-		var signal: Signal<String, NoError>!
-
-		NotificationCenter.default
+		return NotificationCenter.default
 			.reactive
 			.notifications(forName: .NSControlTextDidChange, object: base)
 			.take(during: lifetime)
 			.map { ($0.object as! NSTextField).stringValue }
-			.startWithSignal { innerSignal, _ in signal = innerSignal }
-
-		return signal
 	}
 
 	/// A signal of values in `NSAttributedString` from the text field upon any
 	/// changes.
 	public var continuousAttributedStringValues: Signal<NSAttributedString, NoError> {
-		var signal: Signal<NSAttributedString, NoError>!
-
-		NotificationCenter.default
+		return NotificationCenter.default
 			.reactive
 			.notifications(forName: .NSControlTextDidChange, object: base)
 			.take(during: lifetime)
 			.map { ($0.object as! NSTextField).attributedStringValue }
-			.startWithSignal { innerSignal, _ in signal = innerSignal }
-
-		return signal
 	}
 }
--- ReactiveCocoa/UIKit/UITextView.swift
+++ ReactiveCocoa/UIKit/UITextView.swift
@@ -10,29 +10,19 @@ extension Reactive where Base: UITextView {
 
 	/// A signal of text values emitted by the text view upon end of editing.
 	public var textValues: Signal<String, NoError> {
-		var signal: Signal<String, NoError>!
-
-		NotificationCenter.default
+		return NotificationCenter.default
 			.reactive
 			.notifications(forName: .UITextViewTextDidEndEditing, object: base)
 			.take(during: lifetime)
 			.map { ($0.object as! UITextView).text! }
-			.startWithSignal { innerSignal, _ in signal = innerSignal }
-
-		return signal
 	}
 
 	/// A signal of text values emitted by the text view upon any changes.
 	public var continuousTextValues: Signal<String, NoError> {
-		var signal: Signal<String, NoError>!
-
-		NotificationCenter.default
+		return NotificationCenter.default
 			.reactive
 			.notifications(forName: .UITextViewTextDidChange, object: base)
 			.take(during: lifetime)
 			.map { ($0.object as! UITextView).text! }
-			.startWithSignal { innerSignal, _ in signal = innerSignal }
-
-		return signal
 	}
 }

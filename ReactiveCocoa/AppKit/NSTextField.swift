84ba3bfb3713f5bb3eca8e3e561ffed6d5ca3e9b
Christopher Liscio <chris@supermegaultragroovy.com>
2016-10-20 16:34:49 -0400

    Add some extra control bindings

--- ReactiveCocoa/AppKit/NSTextField.swift
+++ ReactiveCocoa/AppKit/NSTextField.swift
@@ -22,4 +22,10 @@ extension Reactive where Base: NSTextField {
 		return notifications
 			.map { ($0.object as! NSTextField).attributedStringValue }
 	}
+
+	/// Wraps the `stringValue` binding target from NSControl for
+	/// cross-platform compatibility
+	public var text: BindingTarget<String> {
+		return stringValue
+	}
 }

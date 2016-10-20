a3c3dc214251b48c732de2a85c32680b509e5063
Anders <anders@andersha.com>
2016-10-20 15:53:09 +0200

    Adjusted the `prepareForReuse` extensions to account for platform API differences.

--- ReactiveCocoa/UIKit/ReusableComponents.swift
+++ ReactiveCocoa/UIKit/ReusableComponents.swift
@@ -1,4 +1,16 @@
 import UIKit
+import ReactiveSwift
+import enum Result.NoError
+
+@objc public protocol Reusable: class {
+	func prepareForReuse()
+}
+
+extension Reactive where Base: NSObject, Base: Reusable {
+	public var prepareForReuse: Signal<(), NoError> {
+		return trigger(for: #selector(base.prepareForReuse))
+	}
+}
 
 extension UITableViewCell: Reusable {}
 extension UITableViewHeaderFooterView: Reusable {}
--- ReactiveCocoaTests/AppKit/ReusableComponentsSpec.swift
+++ ReactiveCocoaTests/AppKit/ReusableComponentsSpec.swift
@@ -21,18 +21,26 @@ class ReusableComponentsSpec: QuickSpec {
 			expect(isTriggered) == true
 		}
 
-		describe("NSCollectionViewItem") {
-			let cell = NSCollectionViewItem()
+		if #available(macOS 10.11, *) {
+			describe("NSCollectionViewItem") {
+				let item = TestViewItem()
 
-			var isTriggered = false
-			cell.reactive.prepareForReuse.observeValues {
-				isTriggered = true
+				var isTriggered = false
+				item.reactive.prepareForReuse.observeValues {
+					isTriggered = true
+				}
+
+				expect(isTriggered) == false
+
+				item.prepareForReuse()
+				expect(isTriggered) == true
 			}
-
-			expect(isTriggered) == false
-
-			cell.prepareForReuse()
-			expect(isTriggered) == true
 		}
+	}
+}
+
+private class TestViewItem: NSCollectionViewItem {
+	override func loadView() {
+		view = NSView()
 	}
 }

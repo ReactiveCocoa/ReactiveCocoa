e68475d9ddf771b26e286f3e9d5cfea3fc37a153
Anders <anders@andersha.com>
2016-10-20 15:05:32 +0200

    Removed the false workaround. Updated the project settings.

--- ReactiveCocoa/AppKit/ReusableComponents.swift
+++ ReactiveCocoa/AppKit/ReusableComponents.swift
@@ -1,12 +1,4 @@
 import AppKit
 
 extension NSTableCellView: Reusable {}
-extension NSCollectionViewItem: Reusable {
-	@nonobjc public func prepareForReuse() {
-		// Workaround: The compiler complains about `prepareForReuse` not being
-		//             available for "OSXApplicationExtension 10.9", regardless of
-		//             any `@available` annotation to the extension and to the
-		//             protocol.
-		(self as AnyObject).prepareForReuse()
-	}
-}
+extension NSCollectionViewItem: Reusable {}

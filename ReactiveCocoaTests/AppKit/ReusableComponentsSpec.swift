743d8314832fd86a130829278bb053a48b866731
Anders <anders@andersha.com>
2016-10-22 01:58:37 +0200

    Fixed the ReusableComponentsSpecs.

--- ReactiveCocoaTests/AppKit/ReusableComponentsSpec.swift
+++ ReactiveCocoaTests/AppKit/ReusableComponentsSpec.swift
@@ -8,32 +8,36 @@ import AppKit
 class ReusableComponentsSpec: QuickSpec {
 	override func spec() {
 		describe("NSTableCellView") {
-			let cell = NSTableCellView()
-
-			var isTriggered = false
-			cell.reactive.prepareForReuse.observeValues {
-				isTriggered = true
-			}
-
-			expect(isTriggered) == false
-
-			cell.prepareForReuse()
-			expect(isTriggered) == true
-		}
-
-		if #available(macOS 10.11, *) {
-			describe("NSCollectionViewItem") {
-				let item = TestViewItem()
+			it("should send a `value` event when `prepareForReuse` is triggered") {
+				let cell = NSTableCellView()
 
 				var isTriggered = false
-				item.reactive.prepareForReuse.observeValues {
+				cell.reactive.prepareForReuse.observeValues {
 					isTriggered = true
 				}
 
 				expect(isTriggered) == false
 
-				item.prepareForReuse()
+				cell.prepareForReuse()
 				expect(isTriggered) == true
+			}
+		}
+
+		if #available(macOS 10.11, *) {
+			describe("NSCollectionViewItem") {
+				it("should send a `value` event when `prepareForReuse` is triggered") {
+					let item = TestViewItem()
+
+					var isTriggered = false
+					item.reactive.prepareForReuse.observeValues {
+						isTriggered = true
+					}
+
+					expect(isTriggered) == false
+
+					item.prepareForReuse()
+					expect(isTriggered) == true
+				}
 			}
 		}
 	}

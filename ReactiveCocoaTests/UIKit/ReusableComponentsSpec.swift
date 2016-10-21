43431a20cc83e9e41f31c3f9e886bc8bd6319860
Anders <anders@andersha.com>
2016-10-22 01:58:37 +0200

    Fixed the ReusableComponentsSpecs.

--- ReactiveCocoaTests/UIKit/ReusableComponentsSpec.swift
+++ ReactiveCocoaTests/UIKit/ReusableComponentsSpec.swift
@@ -8,45 +8,51 @@ import UIKit
 class ReusableComponentsSpec: QuickSpec {
 	override func spec() {
 		describe("UITableViewCell") {
-			let cell = UITableViewCell()
+			it("should send a `value` event when `prepareForReuse` is triggered") {
+				let cell = UITableViewCell()
 
-			var isTriggered = false
-			cell.reactive.prepareForReuse.observeValues {
-				isTriggered = true
+				var isTriggered = false
+				cell.reactive.prepareForReuse.observeValues {
+					isTriggered = true
+				}
+
+				expect(isTriggered) == false
+
+				cell.prepareForReuse()
+				expect(isTriggered) == true
 			}
-
-			expect(isTriggered) == false
-
-			cell.prepareForReuse()
-			expect(isTriggered) == true
 		}
 
 		describe("UITableViewHeaderFooterView") {
-			let cell = UITableViewHeaderFooterView()
+			it("should send a `value` event when `prepareForReuse` is triggered") {
+				let cell = UITableViewHeaderFooterView()
 
-			var isTriggered = false
-			cell.reactive.prepareForReuse.observeValues {
-				isTriggered = true
+				var isTriggered = false
+				cell.reactive.prepareForReuse.observeValues {
+					isTriggered = true
+				}
+
+				expect(isTriggered) == false
+
+				cell.prepareForReuse()
+				expect(isTriggered) == true
 			}
-
-			expect(isTriggered) == false
-
-			cell.prepareForReuse()
-			expect(isTriggered) == true
 		}
 
 		describe("UICollectionReusableView") {
-			let cell = UICollectionReusableView()
+			it("should send a `value` event when `prepareForReuse` is triggered") {
+				let cell = UICollectionReusableView()
 
-			var isTriggered = false
-			cell.reactive.prepareForReuse.observeValues {
-				isTriggered = true
+				var isTriggered = false
+				cell.reactive.prepareForReuse.observeValues {
+					isTriggered = true
+				}
+
+				expect(isTriggered) == false
+
+				cell.prepareForReuse()
+				expect(isTriggered) == true
 			}
-
-			expect(isTriggered) == false
-
-			cell.prepareForReuse()
-			expect(isTriggered) == true
 		}
 	}
 }

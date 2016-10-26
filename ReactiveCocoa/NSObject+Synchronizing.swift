7c1c5d033dd365d2c6412283034b165fd6c1167a
Anders <anders@andersha.com>
2016-10-26 13:19:41 +0200

    Marked `NSObject.synchronized` as final and non-ObjC. Renamed the containing file.

--- ReactiveCocoa/NSObject+Synchronizing.swift
+++ ReactiveCocoa/NSObject+Synchronizing.swift
@@ -1,5 +1,5 @@
 extension NSObject {
-	internal func synchronized<Result>(execute: () throws -> Result) rethrows -> Result {
+	@nonobjc internal final func synchronized<Result>(execute: () throws -> Result) rethrows -> Result {
 		objc_sync_enter(self)
 		defer { objc_sync_exit(self) }
 		return try execute()

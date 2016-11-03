79c05a4caa83ec9ec70102e5f0e749346819b25f
Eimantas Vaiciunas <eimantas@walkingsmarts.com>
2016-11-03 15:50:46 +0200

    Add reactive `image` and `title` to `UIBarItem`

--- ReactiveCocoa/UIKit/UIBarItem.swift
+++ ReactiveCocoa/UIKit/UIBarItem.swift
@@ -6,4 +6,14 @@ extension Reactive where Base: UIBarItem {
 	public var isEnabled: BindingTarget<Bool> {
 		return makeBindingTarget { $0.isEnabled = $1 }
 	}
+
+	/// Sets image of bar item.
+	public var image: BindingTarget<UIImage?> {
+		return makeBindingTarget { $0.image = $1 }
+	}
+
+	/// Sets the title of bar item.
+	public var title: BindingTarget<String?> {
+		return makeBindingTarget { $0.title = $1 }
+	}
 }

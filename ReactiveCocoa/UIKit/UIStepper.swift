413eecb03398058ebdfa9472a7a665d7c4bd7ba4
Eimantas Vaiciunas <eimantas@walkingsmarts.com>
2016-11-14 08:44:47 +0200

    Make more `UIStepper` properties reactive
    - add `minimumValue`;
    - add `maximumValue`;
    - create `values` signal;
    - update podspec file;

--- ReactiveCocoa/UIKit/UIStepper.swift
+++ ReactiveCocoa/UIKit/UIStepper.swift
@@ -1,10 +1,28 @@
 import UIKit
 import ReactiveSwift
+import enum Result.NoError
 
 extension Reactive where Base: UIStepper {
 
 	/// Sets the stepper's value.
 	public var value: BindingTarget<Double> {
 		return makeBindingTarget { $0.value = $1 }
+	}
+
+	/// Sets stepper's minimum value.
+	public var minimumValue: BindingTarget<Double> {
+		return makeBindingTarget { $0.minimumValue = $1 }
+	}
+
+	/// Sets stepper's maximum value.
+	public var maximumValue: BindingTarget<Double> {
+		return makeBindingTarget { $0.maximumValue = $1 }
+	}
+
+	/// A signal of double values emitted by the stepper upon each user's
+	/// interaction.
+	public var values: Signal<Double, NoError> {
+		return trigger(for: .valueChanged)
+			.map { [unowned base = self.base] in base.value }
 	}
 }
--- ReactiveCocoaTests/UIKit/UIStepperSpec.swift
+++ ReactiveCocoaTests/UIKit/UIStepperSpec.swift
@@ -30,5 +30,40 @@ class UIStepperSpec: QuickSpec {
 			observer.send(value: 0.5)
 			expect(stepper.value) ≈ 0.5
 		}
+
+		it("should accept changes from bindings to its minimum value") {
+			expect(stepper.minimumValue) == 0.0
+
+			let (pipeSignal, observer) = Signal<Double, NoError>.pipe()
+
+			stepper.reactive.minimumValue <~ pipeSignal
+
+			observer.send(value: 0.3)
+			expect(stepper.minimumValue) ≈ 0.3
+		}
+
+		it("should accept changes from bindings to its maximum value") {
+			expect(stepper.maximumValue) == 100.0
+
+			let (pipeSignal, observer) = Signal<Double, NoError>.pipe()
+
+			stepper.reactive.maximumValue <~ pipeSignal
+
+			observer.send(value: 33.0)
+			expect(stepper.maximumValue) ≈ 33.0
+		}
+
+		it("should emit user's changes for its value") {
+			stepper.value = 0.25
+
+			var updatedValue: Double?
+			stepper.reactive.values.observeValues { value in
+				updatedValue = value
+			}
+
+			expect(updatedValue).to(beNil())
+			stepper.sendActions(for: .valueChanged)
+			expect(updatedValue) ≈ 0.25
+		}
 	}
 }

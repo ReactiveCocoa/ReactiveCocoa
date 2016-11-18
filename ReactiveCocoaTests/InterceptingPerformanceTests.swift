0b88ed17d5605bc08d795eeb6e1bac6e5fdbf28e
Anders <anders@andersha.com>
2016-11-18 18:17:04 +0100

    Reduced the message forwarding overhead related to runtime selectors.

---
+++ ReactiveCocoaTests/InterceptingPerformanceTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import ReactiveCocoa
+import ReactiveSwift
+
+private final class Receiver: NSObject {
+	dynamic func message1() {}
+	dynamic func message2() {}
+}
+
+class InterceptingTests: XCTestCase {
+	fileprivate var receiver: Receiver!
+
+	override func setUp() {
+		receiver = Receiver()
+	}
+
+	func testDirectMessage() {
+		measure {
+			for _ in 0 ..< 50000 {
+				self.receiver.message1()
+			}
+		}
+	}
+
+	func testInterceptedMessage() {
+		_ = receiver.reactive.trigger(for: #selector(receiver.message2))
+		measure {
+			for _ in 0 ..< 50000 {
+				self.receiver.message2()
+			}
+		}
+	}
+}

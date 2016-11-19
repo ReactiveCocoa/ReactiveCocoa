e0cbaaaf5b2c445654cf46faef45a889474aadcf
Anders <anders@andersha.com>
2016-11-19 17:48:43 +0100

    Abolished global atomic properties.

--- ReactiveCocoa/ObjC+RuntimeSubclassing.swift
+++ ReactiveCocoa/ObjC+RuntimeSubclassing.swift
@@ -1,6 +1,7 @@
 import ReactiveSwift
 
-private let swizzledExternalClasses = Atomic<Set<ObjectIdentifier>>([])
+private var isSwizzledKey = 0
+private var test = 0
 
 /// ISA-swizzle the class of the supplied instance.
 ///
@@ -24,25 +25,34 @@ internal func swizzleClass(_ instance: NSObject) -> AnyClass {
 
 	if perceivedClass != realClass {
 		// If the class is already lying about what it is, it's probably a KVO
-		// dynamic subclass or something else that we shouldn't subclass
-		// ourselves.
-		//
-		// Use this runtime subclass directly.
-		swizzledExternalClasses.modify { classes in
-			if !classes.contains(ObjectIdentifier(realClass)) {
-				classes.insert(ObjectIdentifier(realClass))
+		// dynamic subclass or something else that we shouldn't subclass at runtime.
+		synchronized(realClass) {
+			let isSwizzled = objc_getAssociatedObject(realClass, &isSwizzledKey) as! Bool? ?? false
+
+			if !isSwizzled {
 				replaceGetClass(in: realClass, decoy: perceivedClass)
+				objc_setAssociatedObject(realClass, &isSwizzledKey, true, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+
+				let token = Lifetime.Token()
+				objc_setAssociatedObject(realClass, &test, token, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+
+				Lifetime(token).ended.observeCompleted {
+
+					let a = 1
+					_ = a
+
+				}
 			}
 		}
 
 		return realClass
 	} else {
 		let name = subclassName(of: perceivedClass)
-		let subclass: AnyClass = name.withCString { name in
-			if let existingClass = objc_getClass(name) as! AnyClass? {
+		let subclass: AnyClass = name.withCString { cString in
+			if let existingClass = objc_getClass(cString) as! AnyClass? {
 				return existingClass
 			} else {
-				let subclass: AnyClass = objc_allocateClassPair(perceivedClass, name, 0)!
+				let subclass: AnyClass = objc_allocateClassPair(perceivedClass, cString, 0)!
 				replaceGetClass(in: subclass, decoy: perceivedClass)
 				objc_registerClassPair(subclass)
 				return subclass

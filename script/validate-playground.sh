c1c253fe5cc39132460b0b0ad7d3d1494a302878
Anders Ha <anders@andersha.com>
2016-11-22 16:49:57 +0100

    Get the SDK root and pass it to the Swift compiler.

--- script/validate-playground.sh
+++ script/validate-playground.sh
@@ -28,11 +28,12 @@ else
 	BUILD_DIR_PATH=Intermediates/CodeCoverage/Products/${CONFIGURATION}-${XCODE_SDK}
 fi
 
+SDK_ROOT=$(xcrun --sdk ${XCODE_SDK} --show-sdk-path)
 PAGES_PATH=${BUILD_DIRECTORY}/Build/${BUILD_DIR_PATH}/all-playground-pages.swift
 
 cat ${PLAYGROUND}/Sources/*.swift ${PLAYGROUND}/Pages/**/*.swift > ${PAGES_PATH}
 
-swift -v -target ${XCODE_PLAYGROUND_TARGET} -D NOT_IN_PLAYGROUND -F ${BUILD_DIRECTORY}/Build/${BUILD_DIR_PATH} ${PAGES_PATH} > /dev/null
+swift -v -target ${XCODE_PLAYGROUND_TARGET} -sdk ${SDK_ROOT} -D NOT_IN_PLAYGROUND -F ${BUILD_DIRECTORY}/Build/${BUILD_DIR_PATH} ${PAGES_PATH} > /dev/null
 result=$?
 
 # Cleanup

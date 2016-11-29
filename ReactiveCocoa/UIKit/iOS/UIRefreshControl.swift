4135229f6274e05764077ef64b166b5721aa77ae
Caleb Davenport <caleb@calebd.me>
2016-11-29 15:32:09 -0500

    Add comments.

--- ReactiveCocoa/UIKit/iOS/UIRefreshControl.swift
+++ ReactiveCocoa/UIKit/iOS/UIRefreshControl.swift
@@ -3,10 +3,12 @@ import enum Result.NoError
 import UIKit
 
 extension Reactive where Base: UIRefreshControl {
+	/// Sets whether the activity indicator should be refreshing.
 	public var isRefreshing: BindingTarget<Bool> {
 		return makeBindingTarget { $1 ? $0.beginRefreshing() : $0.endRefreshing() }
 	}
 
+	/// A trigger that sends `next` when the user performs a refresh.
 	public var refresh: Signal<Void, NoError> {
 		return trigger(for: .valueChanged)
 	}

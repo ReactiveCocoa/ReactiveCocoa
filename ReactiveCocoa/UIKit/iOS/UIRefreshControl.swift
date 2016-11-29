3cd43b1fe85b8c3542f367269383f378df93de33
Caleb Davenport <caleb@calebd.me>
2016-11-29 15:49:24 -0500

    Add attributed title binding.

--- ReactiveCocoa/UIKit/iOS/UIRefreshControl.swift
+++ ReactiveCocoa/UIKit/iOS/UIRefreshControl.swift
@@ -3,9 +3,14 @@ import enum Result.NoError
 import UIKit
 
 extension Reactive where Base: UIRefreshControl {
-	/// Sets whether the activity indicator should be refreshing.
+	/// Sets whether the refresh control should be refreshing.
 	public var isRefreshing: BindingTarget<Bool> {
 		return makeBindingTarget { $1 ? $0.beginRefreshing() : $0.endRefreshing() }
+	}
+
+	/// Sets the attributed title of the refresh control.
+	public var attributedTitle: BindingTarget<NSAttributedString?> {
+		return makeBindingTarget { $0.attributedTitle = $1 }
 	}
 
 	/// A trigger that sends `next` when the user performs a refresh.
--- ReactiveCocoaTests/UIKit/UIRefreshControlSpec.swift
+++ ReactiveCocoaTests/UIKit/UIRefreshControlSpec.swift
@@ -30,6 +30,22 @@ class UIRefreshControlSpec: QuickSpec {
 			expect(refreshControl.isRefreshing) == false
 		}
 
+		it("should accept changes from bindings to its attributed title state") {
+			let (pipeSignal, observer) = Signal<NSAttributedString?, NoError>.pipe()
+			refreshControl.reactive.attributedTitle <~ SignalProducer(signal: pipeSignal)
+
+			let string = NSAttributedString(string: "test")
+
+			observer.send(value: nil)
+			expect(refreshControl.attributedTitle).to(beNil())
+
+			observer.send(value: string)
+			expect(refreshControl.attributedTitle) == string
+
+			observer.send(value: nil)
+			expect(refreshControl.attributedTitle).to(beNil())
+		}
+
 		it("should emit user's changes for its value") {
 			var count = 0
 

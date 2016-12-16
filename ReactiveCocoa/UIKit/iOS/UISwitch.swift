cf69baf58fa24ac98ba3d4066373259fb547e4e3
Cristian Ames <cristian.ames@wolox.com.ar>
2016-12-16 17:05:18 -0300

    Will add a Property value for the isOn value

--- ReactiveCocoa/UIKit/iOS/UISwitch.swift
+++ ReactiveCocoa/UIKit/iOS/UISwitch.swift
@@ -12,4 +12,19 @@ extension Reactive where Base: UISwitch {
 	public var isOnValues: Signal<Bool, NoError> {
 		return controlEvents(.valueChanged).map { $0.isOn }
 	}
+	
+	/// A signal of the enable state in `Bool` emitted by the switch.
+	public var isEnableValues: Signal<Bool, NoError> {
+		return controlEvents(.valueChanged).map { $0.isEnabled }
+	}
+	
+	/// A property of on-off states in `Bool` emmited by the switch
+	var isOnProperty: Property<Bool> {
+		return Property(initial: base.isOn, then: self.isOnValues)
+	}
+	
+	/// A property of the enable state in `Bool` emmited by the switch
+	var isEnableProperty: Property<Bool> {
+		return Property(initial: base.isEnabled, then: self.isEnableValues)
+	}
 }

69d4ca9fde4350cdbc22761fa10c5bea7876b5b2
Anders Ha <anders@andersha.com>
2016-12-16 04:34:48 +0100

    Public `NSObject.makeBindingTarget`.

--- ReactiveCocoa/NSObject+BindingTarget.swift
+++ ReactiveCocoa/NSObject+BindingTarget.swift
@@ -13,7 +13,7 @@ extension Reactive where Base: NSObject {
 	///
 	/// - returns:
 	///   A binding target that holds no strong references to the object.
-	internal func makeBindingTarget<U>(_ key: StaticString = #function, action: @escaping (Base, U) -> Void) -> BindingTarget<U> {
+	public func makeBindingTarget<U>(_ key: StaticString = #function, action: @escaping (Base, U) -> Void) -> BindingTarget<U> {
 		return BindingTarget(on: UIScheduler(), lifetime: lifetime) { [weak base = self.base] value in
 			if let base = base {
 				action(base, value)

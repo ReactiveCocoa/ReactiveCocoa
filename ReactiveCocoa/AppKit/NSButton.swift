0c1ac132e228db029d363a2064a1816307d7f020
David Ohayon <ohayon.1@gmail.com>
2017-01-11 13:25:02 -0500

    Use CocoaAction

--- ReactiveCocoa/AppKit/NSButton.swift
+++ ReactiveCocoa/AppKit/NSButton.swift
@@ -4,8 +4,24 @@ import AppKit
 
 extension Reactive where Base: NSButton {
 
-	public var pressed: Signal<Base, NoError> {
-		return trigger.map { [unowned base = self.base] in base }
+	internal var associatedAction: Atomic<(action: CocoaAction<Base>, disposable: Disposable?)?> {
+		return associatedValue { _ in Atomic(nil) }
 	}
 
+	public var pressed: CocoaAction<Base>? {
+		get {
+			return associatedAction.value?.action
+		}
+
+		nonmutating set {
+			base.target = newValue
+			base.action = newValue.map { _ in CocoaAction<Base>.selector }
+			associatedAction
+				.swap(newValue.map { action in
+					let disposable = isEnabled <~ action.isEnabled
+					return (action, disposable)
+				})?
+				.disposable?.dispose()
+		}
+	}
 }

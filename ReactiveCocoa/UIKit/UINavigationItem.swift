62c1740ecc71a39725b21361da0a3620a13a5178
Marco Cancellieri <marco@cancellieri.de>
2017-04-05 12:03:39 +0200

    Add Extensions for UINavigationItem & UITabBarItem
    UINavigationItem
    - title
    - prompt
    
    UITabBarItem
    - badgeValue
    - badgeColor

---
+++ ReactiveCocoa/UIKit/UINavigationItem.swift
@@ -0,0 +1,14 @@
+import ReactiveSwift
+import UIKit
+
+extension Reactive where Base: UINavigationItem {
+	/// Sets the title of the navigation item.
+	public var title: BindingTarget<String?> {
+		return makeBindingTarget { $0.title = $1 }
+	}
+	
+	/// Sets the prompt of the navigation item.
+	public var prompt: BindingTarget<String?> {
+		return makeBindingTarget { $0.prompt = $1 }
+	}
+}
---
+++ ReactiveCocoaTests/UIKit/UINavigationItemSpec.swift
@@ -0,0 +1,61 @@
+import ReactiveSwift
+import ReactiveCocoa
+import UIKit
+import Quick
+import Nimble
+import enum Result.NoError
+
+class UINavigationItemSpec: QuickSpec {
+	override func spec() {
+		var navigationItem: UINavigationItem!
+		weak var _navigationItem: UINavigationItem?
+		
+		beforeEach {
+			navigationItem = UINavigationItem(title: "initial")
+			_navigationItem = navigationItem
+		}
+		
+		afterEach {
+			navigationItem = nil
+			expect(_navigationItem).to(beNil())
+		}
+		
+		it("should accept changes from bindings to its title value") {
+			let firstChange = "first"
+			let secondChange = "second"
+			
+			navigationItem.title = ""
+			
+			let (pipeSignal, observer) = Signal<String?, NoError>.pipe()
+			navigationItem.reactive.title <~ SignalProducer(pipeSignal)
+			
+			observer.send(value: firstChange)
+			expect(navigationItem.title) == firstChange
+			
+			observer.send(value: secondChange)
+			expect(navigationItem.title) == secondChange
+			
+			observer.send(value: nil)
+			expect(navigationItem.title).to(beNil())
+		}
+		
+		it("should accept changes from bindings to its prompt value") {
+			let firstChange = "first"
+			let secondChange = "second"
+			
+			navigationItem.prompt = ""
+			
+			let (pipeSignal, observer) = Signal<String?, NoError>.pipe()
+			navigationItem.reactive.prompt <~ SignalProducer(pipeSignal)
+			
+			observer.send(value: firstChange)
+			expect(navigationItem.prompt) == firstChange
+			
+			observer.send(value: secondChange)
+			expect(navigationItem.prompt) == secondChange
+			
+			observer.send(value: nil)
+			expect(navigationItem.prompt).to(beNil())
+		}
+	}
+}

7edcfd5f0451e2768be9aaa34c486e3b4f4a8f42
Marco Cancellieri <marco@cancellieri.de>
2017-04-05 17:19:00 +0200

    UIFeedbackGenerator: Use () instead of Void

--- ReactiveCocoa/UIKit/iOS/UIFeedbackGenerator/UIFeedbackGenerator.swift
+++ ReactiveCocoa/UIKit/iOS/UIFeedbackGenerator/UIFeedbackGenerator.swift
@@ -4,7 +4,7 @@ import UIKit
 @available(iOS 10.0, *)
 extension Reactive where Base: UIFeedbackGenerator {
 	/// Prepares the feedback generator.
-	public var prepare: BindingTarget<Void> {
+	public var prepare: BindingTarget<()> {
 		return makeBindingTarget { generator, _ in
 			generator.prepare()
 		}
--- ReactiveCocoa/UIKit/iOS/UIFeedbackGenerator/UIImpact​Feedback​Generator.swift
+++ ReactiveCocoa/UIKit/iOS/UIFeedbackGenerator/UIImpact​Feedback​Generator.swift
@@ -4,7 +4,7 @@ import UIKit
 @available(iOS 10.0, *)
 extension Reactive where Base: UIImpactFeedbackGenerator {
 	/// Triggers the feedback.
-	public var impactOccurred: BindingTarget<Void> {
+	public var impactOccurred: BindingTarget<()> {
 		return makeBindingTarget { generator, _ in
 			generator.impactOccurred()
 		}

b2c54aec9bc4b47d8e6f838492ec9ae521583a24
Anders Ha <anders@andersha.com>
2017-04-27 13:06:37 +0800

    Safe, read-only `DynamicProperty` as `Property`.

--- ReactiveCocoa/DynamicProperty.swift
+++ ReactiveCocoa/DynamicProperty.swift
@@ -5,28 +5,23 @@ import enum Result.NoError
 /// A typed mutable property view to a certain key path of an Objective-C object using
 /// Key-Value Coding and Key-Value Observing.
 ///
-/// - warning: A `DynamicProperty` holds an unowned reference to the Objective-C object.
-///            Any access of any property in a `DynamicProperty` after the deallocation
-///            of the object would trap at runtime.
+/// Bindings towards a `DynamicProperty` would be directed to the underlying Objective-C
+/// object, and would not be affected by the deinitialization of the `DynamicProperty`.
 public final class DynamicProperty<Value>: MutablePropertyProtocol {
-	private unowned let object: NSObject
+	private weak var object: NSObject?
 	private let keyPath: String
+	private let cache: Property<Value>
 
 	/// The current value of the property, as read and written using Key-Value
 	/// Coding.
 	public var value: Value {
-		get {
-			return object.value(forKeyPath: keyPath) as! Value
-		}
-
-		set(newValue) {
-			object.setValue(newValue, forKeyPath: keyPath)
-		}
+		get { return cache.value }
+		set { object?.setValue(newValue, forKeyPath: keyPath) }
 	}
 
 	/// The lifetime of the property.
 	public var lifetime: Lifetime {
-		return object.reactive.lifetime
+		return object?.reactive.lifetime ?? .empty
 	}
 
 	/// The binding target of the property.
@@ -43,11 +38,11 @@ public final class DynamicProperty<Value>: MutablePropertyProtocol {
 	/// - important: This only works if the object given to init() is KVO-compliant.
 	///              Most UI controls are not!
 	public var producer: SignalProducer<Value, NoError> {
-		return object.reactive.producer(forKeyPath: keyPath).map { $0 as! Value }
+		return cache.producer
 	}
 
 	public var signal: Signal<Value, NoError> {
-		return object.reactive.signal(forKeyPath: keyPath).map { $0 as! Value }
+		return cache.signal
 	}
 
 	/// Create a typed mutable view to the given key path of the given Objective-C object.	
@@ -60,5 +55,6 @@ public final class DynamicProperty<Value>: MutablePropertyProtocol {
 	public init(object: NSObject, keyPath: String) {
 		self.object = object
 		self.keyPath = keyPath
+		self.cache = Property(object: object, keyPath: keyPath)
 	}
 }

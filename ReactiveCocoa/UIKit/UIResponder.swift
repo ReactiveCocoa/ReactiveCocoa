570dad3cfda69e446ed5b61b62c6005857c56bd1
Marco Cancellieri <marco@cancellieri.de>
2018-04-10 13:42:14 +0200

    Add UIResponder extension

---
+++ ReactiveCocoa/UIKit/UIResponder.swift
@@ -0,0 +1,14 @@
+import ReactiveSwift
+import UIKit
+
+extension Reactive where Base: UIResponder {
+	/// Asks UIKit to make this object the first responder in its window.
+	public var becomeFirstResponder: BindingTarget<()> {
+		return makeBindingTarget { base, _ in base.becomeFirstResponder() }
+	}
+	
+	/// Notifies this object that it has been asked to relinquish its status as first responder in its window.
+	public var resignFirstResponder: BindingTarget<()> {
+		return makeBindingTarget { base, _ in base.resignFirstResponder() }
+	}
+}

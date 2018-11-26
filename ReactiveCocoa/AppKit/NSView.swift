4976da53482e4b4c47eb6e44dbb4c3cb88e0e1ca
Eimantas Vaičiūnas <eimantas@stork.software>
2018-11-26 20:30:07 +0200

    Add a little documentation for `NSView`'s bindings

--- ReactiveCocoa/AppKit/NSView.swift
+++ ReactiveCocoa/AppKit/NSView.swift
@@ -3,6 +3,7 @@ import AppKit
 import enum Result.NoError
 
 extension Reactive where Base: NSView {
+	/// Sets the visibility of the view.
 	public var isHidden: BindingTarget<Bool> {
 		return makeBindingTarget { $0.isHidden = $1 }
 	}

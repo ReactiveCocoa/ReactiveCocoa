fb458d215742c39801c4d9f9115cb520fc49ae7f
Rehat Kathuria <rehat@kathuria.co>
2019-06-15 07:00:50 +0100

    Add a binding target for the barTintColor of UINavigationBar

---
+++ ReactiveCocoa/UIKit/UINavigationBar.swift
@@ -0,0 +1,9 @@
+import ReactiveSwift
+import UIKit
+
+extension Reactive where Base: UINavigationBar {
+	/// Sets the barTintColor of the navigation bar.
+	public var barTintColor: BindingTarget<UIColor?> {
+		return makeBindingTarget { $0.barTintColor = $1 }
+	}
+}

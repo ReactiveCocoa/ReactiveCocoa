edc7cb28b86cfc160e72ddb3981fdf9e60d89609
Simon Jarbrant <simon.jarbrant@isotop.se>
2019-11-05 15:57:07 +0100

    Add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "ReactiveCocoa",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "ReactiveCocoa", targets: ["ReactiveCocoa"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", from: "6.1.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ReactiveCocoaObjC",
+            dependencies: [],
+            path: "ReactiveCocoaObjC"),
+
+        .target(
+            name: "ReactiveCocoa",
+            dependencies: ["ReactiveSwift", "ReactiveCocoaObjC"],
+            path: "ReactiveCocoa"),
+
+        .target(
+            name: "ReactiveCocoaObjCTestSupport",
+            path: "ReactiveCocoaObjCTestSupport"),
+
+        .testTarget(
+            name: "ReactiveCocoaTests",
+            dependencies: [
+                "ReactiveCocoa",
+                "ReactiveCocoaObjCTestSupport",
+                "Quick",
+                "Nimble"
+            ],
+            path: "ReactiveCocoaTests"),
+    ]
+)

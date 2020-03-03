74ee06e89c2ffd8208c7abf46cd1910a18f429fb
Wang Kai <kawang@microstrategy.com>
2020-03-03 16:06:15 +0800

    add test pr

---
+++ ReactiveCocoa/Useless.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  ReactiveCocoa-iOS
+//
+//  Created by Wang, Kai on 2020/3/3.
+//  Copyright © 2020 GitHub. All rights reserved.
+//
+
+import Foundation
+
+
+class Useless {
+	func echo() {
+		NSLog("useless commit")
+	}
+}

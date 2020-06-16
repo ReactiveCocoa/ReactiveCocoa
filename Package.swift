c37d0f40db2622cd35bfcb48b384166281dfb5e2
Anders Ha <anders@andersha.com>
2020-06-16 14:44:42 +0100

    Requires Swift 5.2.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(

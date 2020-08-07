6d1b7f953604038707eca7204bd43c97bf102397
Michael Brown <me@michael-brown.net>
2020-08-07 16:48:15 +0100

    Bump SPM swift-tools-version to 5.2 (#3710)
    * Add ReactiveCocoaObjC as a product
    
    * Bump swift-tools-version to 5.2
    
    For the same reason as this was done in ReactiveSwift: so clients aren't forced to pull in test dependencies.
    
    * Add swiftLanguageVersions
    
    * Update .swift-version to 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -39,5 +39,6 @@ let package = Package(
                 "Nimble"
             ],
             path: "ReactiveCocoaTests"),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]    
 )

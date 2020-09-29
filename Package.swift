0f5b94cce4ae4209f4b754f72f31f4d41b6cc141
Petr Pavlik <petrpavlik@me.com>
2020-09-29 20:03:16 +0200

    bump package target to iOS 9 and update testing packages (#3712)
    * Update swift package quick dependency to 3.0.0+
    
    * bump min deployment target to iOS 9 and update Nimble

--- Package.swift
+++ Package.swift
@@ -5,14 +5,14 @@ import PackageDescription
 let package = Package(
     name: "ReactiveCocoa",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
     products: [
         .library(name: "ReactiveCocoa", targets: ["ReactiveCocoa"])
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift", from: "6.2.0"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
     ],
     targets: [

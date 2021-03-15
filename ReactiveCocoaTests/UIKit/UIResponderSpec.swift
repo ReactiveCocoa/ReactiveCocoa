2b5398f49c1c9ee235ea6d5d0ea7486a2cfe67e3
Anders Ha <anders@andersha.com>
2021-03-15 15:21:01 +0000

    Disable first responder test cases for Mac Catalyst.

--- ReactiveCocoaTests/UIKit/UIResponderSpec.swift
+++ ReactiveCocoaTests/UIKit/UIResponderSpec.swift
@@ -7,17 +7,19 @@ import Nimble
 
 class UIResponderSpec: QuickSpec {
 	override func spec() {
+		#if !targetEnvironment(macCatalyst)
 		it("should become and resign first responder") {
 			let window = UIWindow(frame: .zero)
 			let textField = UITextField(frame: .zero)
 			window.addSubview(textField)
-			
+
 			expect(textField.isFirstResponder).to(beFalse())
 			textField.reactive.becomeFirstResponder <~ SignalProducer(value: ())
 			expect(textField.isFirstResponder).to(beTrue())
 			textField.reactive.resignFirstResponder <~ SignalProducer(value: ())
 			expect(textField.isFirstResponder).to(beFalse())
 		}
+		#endif
 	}
 }
 #endif
